a3a2ce80b838ec1c9b52df27aaaca33879e4a289
Jon Phipps <jphipps@madcreek.com>
2006-07-07 16:20:57 +0000

    form method should be lower case

--- apps/frontend/modules/conceptprop/templates/_search.php
+++ apps/frontend/modules/conceptprop/templates/_search.php
@@ -1,3 +1,3 @@
-<?php echo form_tag('@concept_search', 'method=GET') ?>
+<?php echo form_tag('@concept_search', 'method=get') ?>
   <?php echo input_tag('term', htmlspecialchars($searchTerm), array('style' => 'width: 150px')) ?>&nbsp;<?php echo submit_tag('Search', 'class=small') ?>
 </form>
