3b4a94b5a4772feb6b889bde60da6ec4fc85574b
Jon Phipps <jphipps@madcreek.com>
2006-09-18 13:14:18 +0000

    make admin table sort indicators an image

--- data/symfony/generator/sfPropelAdmin/default/template/templates/_list_th_tabular.php
+++ data/symfony/generator/sfPropelAdmin/default/template/templates/_list_th_tabular.php
@@ -7,7 +7,11 @@
     <?php if ($column->isReal()): ?>
       [?php if ($sf_user->getAttribute('sort', null, 'sf_admin/<?php echo $this->getSingularName() ?>/sort') == '<?php echo $column->getName() ?>'): ?]
       [?php echo link_to(__('<?php echo str_replace("'", "\\'", $this->getParameterValue('list.fields.'.$column->getName().'.name')) ?>'), '<?php echo $this->getModuleName() ?>/list?sort=<?php echo $column->getName() ?>&type='.($sf_user->getAttribute('type', 'asc', 'sf_admin/<?php echo $this->getSingularName() ?>/sort') == 'asc' ? 'desc' : 'asc')) ?]
-      ([?php echo __($sf_user->getAttribute('type', 'asc', 'sf_admin/<?php echo $this->getSingularName() ?>/sort')) ?])
+      [?php if ($sf_user->getAttribute('type', null, 'sf_admin/<?php echo $this->getSingularName() ?>/sort') == 'asc'): ?]
+      [?php echo image_tag(sfConfig::get('sf_admin_web_dir').'/images/Sort_Arrow_asc.gif', array('align' => 'absmiddle', 'alt' => __('Ascending Order'), 'title' => __('List has been sorted in ascending order'))) ?]
+      [?php elseif ($sf_user->getAttribute('type', null, 'sf_admin/<?php echo $this->getSingularName() ?>/sort') == 'desc'): ?]
+      [?php echo image_tag(sfConfig::get('sf_admin_web_dir').'/images/Sort_Arrow_desc.gif', array('align' => 'absmiddle', 'alt' => __('Descending Order'), 'title' => __('List has been sorted in descending order'))) ?]
+      [?php endif; ?]
       [?php else: ?]
       [?php echo link_to(__('<?php echo str_replace("'", "\\'", $this->getParameterValue('list.fields.'.$column->getName().'.name')) ?>'), '<?php echo $this->getModuleName() ?>/list?sort=<?php echo $column->getName() ?>&type=asc') ?]
       [?php endif; ?]
