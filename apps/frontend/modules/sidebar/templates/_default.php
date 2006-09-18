0650cbb89b140e6666118075615ed88e67ddcdd3
Jon Phipps <jphipps@madcreek.com>
2006-09-18 12:31:26 +0000

    fix duplicate div#panel_admin id

--- apps/frontend/modules/sidebar/templates/_default.php
+++ apps/frontend/modules/sidebar/templates/_default.php
@@ -1,7 +1,7 @@
 <?php use_helper('Global') ?>
 <?php echo include_partial('sidebar/administration') ?>
 <?php if ($sf_user->isAuthenticated()): ?>
-<div id="panel_admin">
+<div id="panel_default">
   <h2><?php echo __('Register New...') ?></h2>
   <ul>
     <li><?php echo link_to(__('Resource Owner'), 'agent/create') ?></li>
