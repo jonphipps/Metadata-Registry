e39008d9158849d386b5e86bd92d70660ed3c4a9
Jon Phipps <jphipps@madcreek.com>
2006-09-23 12:05:42 +0000

    Agent: set default type

--- apps/frontend/modules/agent/templates/_type.php
+++ apps/frontend/modules/agent/templates/_type.php
@@ -1,3 +1,3 @@
-<?php echo radiobutton_tag("agent[type]", "Individual", $agent->getType() == 'Individual'); ?>&nbsp;Individual&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-<?php echo radiobutton_tag("agent[type]", "Organization", $agent->getType() == 'Organization'); ?>&nbsp;Organization
-
+<?php $type = $agent->getType() ? $agent->getType() : 'Individual' ?>
+<?php echo radiobutton_tag("agent[type]", "Individual", $type == 'Individual'); ?>&nbsp;Individual&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<?php echo radiobutton_tag("agent[type]", "Organization", $type == 'Organization'); ?>&nbsp;Organization
