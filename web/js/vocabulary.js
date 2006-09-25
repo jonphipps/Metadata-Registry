822f2181a5e468f3b1501b28aacc19d3623904aa
Jon Phipps <jphipps@madcreek.com>
2006-09-25 17:34:08 +0000

    fix vocabulary javascript

--- web/js/vocabulary.js
+++ web/js/vocabulary.js
@@ -1,6 +1,15 @@
-document.getElementById('vocabulary_token');
-tokenField.onblur = function(){
-var domainField = $F('vocabulary[base_domain]');
-var tokenField = $F('vocabulary[token]');
-var uriField = $F('vocabulary[uri]');
-uriField.value = domainField.value + tokenField.value};
+function updateUri()
+{
+   var domainField = $('vocabulary_base_domain');
+   var tokenField = $('vocabulary_token');
+   var uriField = $('vocabulary_uri');
+   var updateIt = true;
+   if(uriField.value != "")
+   {
+      updateIt = confirm("Update the URI?");
+   }
+   if(updateIt)
+   {
+      uriField.value = domainField.value + tokenField.value
+   }
+};
