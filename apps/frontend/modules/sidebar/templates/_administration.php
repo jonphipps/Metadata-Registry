ada14eaa229cd9cf001ef677007e62e9f09faf79
Jon Phipps <jphipps@madcreek.com>
2006-09-27 12:50:07 +0000

    add list users to sidebar/templates/_administration.php

--- apps/frontend/modules/sidebar/templates/_administration.php
+++ apps/frontend/modules/sidebar/templates/_administration.php
@@ -3,6 +3,7 @@
   <h2><?php echo __('administration') ?></h2>
 
   <ul>
+    <li><?php echo link_to(__('All users'), 'user/list') ?> (<?php echo UserPeer::getUsersCount() ?>)</li>
     <li><?php echo link_to(__('maintainer candidates'), 'administrator/moderatorCandidates') ?> (<?php echo UserPeer::getModeratorCandidatesCount() ?>)</li>
     <li><?php echo link_to(__('maintainer list'), 'administrator/maintainers') ?></li>
     <li><?php echo link_to(__('administrator list'), 'administrator/administrators') ?></li>
