13aa06847a661b16a513feedf73efc104674bb1c
Jon Phipps <jphipps@madcreek.com>
2006-09-27 12:41:04 +0000

    Update concepts interface

--- apps/frontend/modules/concept/templates/_list_header.php
+++ apps/frontend/modules/concept/templates/_list_header.php
@@ -1,8 +1,7 @@
-<h1>
-  Concepts <br />
-  in <?php echo link_to('Vocabulary', 'vocabulary/list') ?>
+<?php $vocabulary = $sf_user->getCurrentVocabulary() ?>
+<h1><?php echo link_to('Vocabulary:', 'vocabulary/list') ?>
 <?php if ($sf_user->getCurrentVocabulary()): ?>
-  <?php echo link_to($sf_user->getCurrentVocabulary()->getName(), 'vocabulary/show?id=' . $sf_user->getCurrentVocabulary()->getId()) ?>
+  <?php echo link_to($vocabulary->getName(), 'vocabulary/show?id=' . $vocabulary->getId()) ?>
+  <br /><?php echo link_to('Concepts', '/concept/list?vocabulary_id=' . $vocabulary->getID()) ?>
  <?php endif; ?>
 </h1>
-
