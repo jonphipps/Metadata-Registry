fadafe3517754849a4716cffbe4a080d62474987
Jon Phipps <jphipps@madcreek.com>
2006-10-03 22:41:34 +0000

    add agent user admin

---
+++ apps/frontend/modules/agentuser/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * agentuser actions.
+ *
+ * @package    registry
+ * @subpackage agentuser
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 63 2006-06-14 17:31:15Z jphipps $
+ */
+class agentuserActions extends autoagentuserActions
+{
+}
---
+++ apps/frontend/modules/agentuser/templates/_user.php
@@ -0,0 +1,1 @@
+<?php $user = $agent_has_user->getUser(); echo link_to($user, 'user/show?id='.$user->getId()) ?></td>
