679dd6afdeb55e890f789c6b63293fb46411ed08
Jon Phipps <jphipps@madcreek.com>
2006-10-16 21:41:50 +0000

    update concept list header with link

--- apps/frontend/modules/concept/templates/_list_header.php
+++ apps/frontend/modules/concept/templates/_list_header.php
@@ -2,6 +2,6 @@
 <h1><?php echo link_to('Vocabulary:', 'vocabulary/list') ?>
 <?php if ($sf_user->getCurrentVocabulary()): ?>
   <?php echo link_to($vocabulary->getName(), 'vocabulary/show?id=' . $vocabulary->getId()) ?>
-  <br />Concepts
-<?php endif; ?>
+  <br /><?php echo link_to('Concepts', '/concept/list?vocabulary_id=' . $vocabulary->getID()) ?>
+ <?php endif; ?>
 </h1>
