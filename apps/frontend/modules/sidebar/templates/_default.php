509348b34ab8878339c7790e8bb0111163ade34c
Jon Phipps <jphipps@madcreek.com>
2006-10-17 13:37:25 +0000

    minor adjustments to home page and sidebar

--- apps/frontend/modules/sidebar/templates/_default.php
+++ apps/frontend/modules/sidebar/templates/_default.php
@@ -1,6 +1,11 @@
 <?php use_helper('Global') ?>
 <?php echo include_partial('sidebar/administration') ?>
 <div id="panel_default">
+  <h2><?php echo __('Browse...') ?></h2>
+  <ul>
+    <li><?php echo link_to(__('Resource Owners'), 'agent/list') ?></li>
+    <li><?php echo link_to(__('Vocabularies'), 'vocabulary/list') ?></li>
+  </ul>
 <?php if ($sf_user->isAuthenticated()): ?>
   <h2><?php echo __('Register New...') ?></h2>
   <ul>
@@ -8,12 +13,6 @@
   <?php if ($sf_user->getAttribute('agentCount','0','subscriber')): ?>
     <li><?php echo link_to(__('Vocabulary'), 'vocabulary/create') ?></li>
   <?php endif ?>
-  </ul>
-<?php else: ?>
-  <h2><?php echo __('Browse...') ?></h2>
-  <ul>
-    <li><?php echo link_to(__('Resource Owners'), 'agent/list') ?></li>
-    <li><?php echo link_to(__('Vocabularies'), 'vocabulary/list') ?></li>
   </ul>
 <?php endif ?>
 </div>
