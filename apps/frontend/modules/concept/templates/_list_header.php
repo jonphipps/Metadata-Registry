61c063d915da93a40c7a385763e584e31bcf1edb
Jon Phipps <jphipps@madcreek.com>
2006-10-17 13:35:48 +0000

    remove link from concept list header

--- apps/frontend/modules/concept/templates/_list_header.php
+++ apps/frontend/modules/concept/templates/_list_header.php
@@ -2,6 +2,6 @@
 <h1><?php echo link_to('Vocabulary:', 'vocabulary/list') ?>
 <?php if ($sf_user->getCurrentVocabulary()): ?>
   <?php echo link_to($vocabulary->getName(), 'vocabulary/show?id=' . $vocabulary->getId()) ?>
-  <br /><?php echo link_to('Concepts', '/concept/list?vocabulary_id=' . $vocabulary->getID()) ?>
+  <br />Concepts
  <?php endif; ?>
 </h1>
