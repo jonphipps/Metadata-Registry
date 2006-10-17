330cf791a609e04122c9a59f0b1b6018811057f5
Jon Phipps <jphipps@madcreek.com>
2006-10-17 13:40:44 +0000

    add xml schema generation

--- apps/frontend/modules/xml/templates/showSchemeSuccess.php
+++ apps/frontend/modules/xml/templates/showSchemeSuccess.php
@@ -15,7 +15,7 @@
             Date created: <?php echo $vocabulary->getCreatedAt() . "\n" ?>
             Date of last update: <?php echo $vocabulary->getLastUpdated() . "\n" ?>
 <?php if ($vocabulary->getNote()): ?>
-            </xs:documentation><?php echo htmlspecialchars($vocabulary->getNote(), ENT_NOQUOTES, 'UTF-8') . "\n" ?>
+            <?php echo htmlspecialchars($vocabulary->getNote(), ENT_NOQUOTES, 'UTF-8') . "\n" ?>
 <?php endif; ?>
             Further information about this schema is available at <?php echo $vocabulary->getUri() . ".html\n" ?>
         </xs:documentation>
