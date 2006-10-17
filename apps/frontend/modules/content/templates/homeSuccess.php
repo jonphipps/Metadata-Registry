37fbb261fcef31e6ff1aecce370bcb4b4b1dc1e7
Jon Phipps <jphipps@madcreek.com>
2006-10-17 00:20:00 +0000

    setup home page

--- apps/frontend/modules/content/templates/homeSuccess.php
+++ apps/frontend/modules/content/templates/homeSuccess.php
@@ -1,19 +1,8 @@
+<div id="home" style="width:700px; margin-left:auto;margin-right:auto;">
 <?php echo $html ?>
-<div id="Home_menu">
-   <?php echo __('From here you can...') ?>
-   <ul>
-      <li><?php echo link_to(__('Browse the Registered Vocabularies'), 'vocabulary/list') ?></li>
-      <li><?php echo link_to(__('Browse the Vocabulary Owners'), 'agent/list') ?></li>
-   </ul>
+<div id="home_contacts">
+   <?php echo mail_to('dih1@cornell.edu', 'Diane Hillmann', array('encode' => true)) ?><br />
+   <?php echo mail_to('sasutton@u.washington.edu', 'Stuart Sutton', array('encode' => true)) ?>
 </div>
-<div id="home_menu2">
-   <?php echo __("Once you've registered <em>yourself</em> with The Registry, you can...") ?>
-   <ul>
-      <li><?php echo __("Register yourself or your organization as a Vocabulary Owner.")?></li>
-      <li><?php echo __("Register a vocabulary and enter concepts.")?></li>
-      <li><?php echo __("Export your vocabulary as rdf:SKOS or as an XML Schema")?></li>
-      <li><?php echo __("Register other individuals as maintainers of your vocabulary")?></li>
-      <li><?php echo __("And much more...")?></li>
-   </ul>
 </div>
 
--- apps/frontend/modules/sidebar/templates/_default.php
+++ apps/frontend/modules/sidebar/templates/_default.php
@@ -1,7 +1,7 @@
 <?php use_helper('Global') ?>
 <?php echo include_partial('sidebar/administration') ?>
-<?php if ($sf_user->isAuthenticated()): ?>
 <div id="panel_default">
+<?php if ($sf_user->isAuthenticated()): ?>
   <h2><?php echo __('Register New...') ?></h2>
   <ul>
     <li><?php echo link_to(__('Resource Owner'), 'agent/create') ?></li>
@@ -9,5 +9,11 @@
     <li><?php echo link_to(__('Vocabulary'), 'vocabulary/create') ?></li>
   <?php endif ?>
   </ul>
-</div>
+<?php else: ?>
+  <h2><?php echo __('Browse...') ?></h2>
+  <ul>
+    <li><?php echo link_to(__('Resource Owners'), 'agent/list') ?></li>
+    <li><?php echo link_to(__('Vocabularies'), 'vocabulary/list') ?></li>
+  </ul>
 <?php endif ?>
+</div>
