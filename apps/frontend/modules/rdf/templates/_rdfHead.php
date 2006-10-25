c376a7ba8ad8986af845ab12f9bf4fd4903f816e
Jon Phipps <jphipps@madcreek.com>
2006-10-25 19:24:55 +0000

    small problem with default namespace in skos rdf

--- apps/frontend/modules/rdf/templates/_rdfHead.php
+++ apps/frontend/modules/rdf/templates/_rdfHead.php
@@ -1,6 +1,7 @@
 <?php echo '<?xml version="1.0" encoding = "UTF-8"?>' ?>
 
 <rdf:RDF
+    xmlns="http://www.w3.org/2004/02/skos/core#"
     xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
     xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
     xmlns:skos="http://www.w3.org/2004/02/skos/core#"
