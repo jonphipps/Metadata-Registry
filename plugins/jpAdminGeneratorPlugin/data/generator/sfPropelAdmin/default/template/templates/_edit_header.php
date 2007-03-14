b560e90fc19de64f5714b5773cd1a8cd501d13df
Jon Phipps <jphipps@madcreek.com>
2007-03-14 00:58:37 +0000

    add support for selecting header based on mode

--- plugins/jpAdminGeneratorPlugin/data/generator/sfPropelAdmin/default/template/templates/_edit_header.php
+++ plugins/jpAdminGeneratorPlugin/data/generator/sfPropelAdmin/default/template/templates/_edit_header.php
@@ -1,1 +1,14 @@
+[?php if ('edit' == $mode): ?]
+<?php if (isset($this->params['edit']['title']['mode']['edit'])): ?>
+<h1><?php echo $this->getI18NString('edit.title.mode.edit') ?></h1>
+<?php else: ?>
 <h1><?php echo $this->getI18NString('edit.title', 'edit '.$this->getModuleName()) ?></h1>
+<?php endif; ?>
+[?php else: ?]
+<?php if (isset($this->params['edit']['title']['mode']['create'])): ?>
+<h1><?php echo $this->getI18NString('edit.title.mode.create') ?></h1>
+<?php else: ?>
+<h1>[?php echo __('Creating new') ?] <?php echo $this->getSingularName() ?></h1>
+<?php endif; ?>
+[?php endif; ?]
+
