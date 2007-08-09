bf59961f3994cfdff7c74c07c48d4ba13ff67cd8
Jon Phipps <jphipps@madcreek.com>
2007-08-09 20:25:31 +0000

    batch script to update the db for beta version

--- batch/updatedb.php
+++ batch/updatedb.php
@@ -24,41 +24,98 @@ $databaseManager = new sfDatabaseManager();
 $databaseManager->initialize();
 
 // batch process here
-$foo = new ConceptPropertyPeer();
-$selectCriteria = new Criteria(ConceptPropertyPeer::DATABASE_NAME);
+
+//get the vocabularies
+$foo = new VocabularyPeer();
+$selectCriteria = new Criteria(VocabularyPeer::DATABASE_NAME);
 $rs = $foo->doSelect($selectCriteria);
-$count = $foo->doCount($selectCriteria);
-$counter = 0;
-/**  @var ConceptProperty $property  */
-foreach ($rs as $property)
+$vocabCount = $foo->doCount($selectCriteria);
+$vocabCounter = 0;
+
+//foreach vocabulary
+/** @var Vocabulary $vocavulary **/
+foreach ($rs as $vocabulary)
 {
-  //debugbreak();
-  $counter++;
-//  $conceptCol = $property->getConceptsJoinVocabulary();
-  $concept = $property->getConceptRelatedByConceptId();
-  $criteria = new Criteria();
-  $criteria->add(VocabularyHasUserPeer::IS_REGISTRAR_FOR, true);
-  $users = $property->getConceptRelatedByConceptId()->getVocabulary()->getVocabularyHasUsersJoinUser($criteria);
+  $vocabCounter++;
+
+  //get the vocabulary registrar
+  $userCriteria = new Criteria();
+  $userCriteria->add(VocabularyHasUserPeer::IS_REGISTRAR_FOR, true);
+  $users = $vocabulary->getVocabularyHasUsersJoinUser($userCriteria);
   if (count($users))
   {
-/**  @var VocabularyHasUser $user  */
+    /**  @var VocabularyHasUser $user  */
     $user = $users[0]->getUser();
     $userId = $user->getId();
-    $property->setUpdatedUserId($userId);
-    $property->setCreatedUserId($userId);
   }
+  //created_user_id <== "use id of vocabulary registrar"
+  $vocabulary->setCreatedUserId($userId);
+  //updated_user_id <== "use id of vocabulary registrar"
+  $vocabulary->setUpdatedUserId($userId);
+  //update the vocabulary
+  $vocabulary->save();
 
-  if (!$property->getLanguage())
+  //get the vocabulary concepts
+  $concepts = $vocabulary->getConcepts();
+  $conceptCount = count($concepts);
+  $conceptCounter = 0;
+  //foreach concept
+  /** @var Concept $concept **/
+  foreach ($concepts as $concept)
   {
-    $property->setLanguage('en');
+    $conceptCounter++;
+
+    //updated_at <== last_updated
+    $concept->setUpdatedAt($concept->getLastUpdated());
+    //created_user_id <== "use id of vocabulary registrar"
+    $concept->setCreatedUserId($userId);
+    //updated_user_id <== "use id of vocabulary registrar"
+    $concept->setUpdatedUserId($userId);
+
+    //get the concept properties
+    /**  @var ConceptProperty $properties  */
+    $properties = $concept->getConceptPropertysRelatedByConceptId();
+    $propertyCount = count($properties);
+    $propertyCounter = 0;
+    //foreach property
+    foreach ($properties as $property)
+    {
+      $propertyCounter++;
+
+      //updated_at <== last_updated
+      $property->setUpdatedAt($property->getLastUpdated());
+      //created_user_id <== "use id of vocabulary registrar"
+      $property->setCreatedUserId($userId);
+      //updated_user_id <== "use id of vocabulary registrar"
+      $property->setUpdatedUserId($userId);
+      //eliminate null language
+      if (!$property->getLanguage())
+      {
+        $property->setLanguage('en');
+      }
+
+      //eliminate null status
+      if (!$property->getStatusId())
+      {
+        $property->setStatusId(1);
+      }
+
+      if ($property->getSkosPropertyId() == 19)
+      {
+        //pref_label_id <== "use id of preflabel, concept.language related to the concept, or join based on the pref_label string"
+        $concept->setPrefLabelId($property->getId());
+        $property->setPrimaryPrefLabel(true);
+      }
+
+      $property->save();
+
+      echo("\r" . 'Voc: ' . str_pad($vocabCounter,5) . ' of ' . $vocabCount . '   Con: ' . str_pad($conceptCounter,5) . ' of ' . $conceptCount . '   Prop: ' . str_pad($propertyCounter,5) . ' of ' . $propertyCount);
+    }
+
+    //update the concept
+    $concept->save();
   }
-
-  if (!$property->getStatusId())
-  {
-    $property->setStatusId(1);
-  }
-
-  $property->save();
-  echo($counter . ' of ' . $count) ."\n";
-
+  //$vocabulary->save();
 }
+
+echo("Done!");
