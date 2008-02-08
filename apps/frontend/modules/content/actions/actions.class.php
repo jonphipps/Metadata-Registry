08dba5ec1ac465bd1d780706f4fbea2b68a6a775
Jon Phipps <jphipps@madcreek.com>
2008-02-08 23:10:48 +0000

    refactor content retrieval

--- apps/frontend/modules/content/actions/actions.class.php
+++ apps/frontend/modules/content/actions/actions.class.php
@@ -12,15 +12,39 @@ class contentActions extends sfActions
 {
   public function executeHome()
   {
+    $this->outputFile('home');
+  }
+
+  public function executeAbout()
+  {
+    $this->outputFile('about');
+  }
+
+  public function executeUnavailable()
+  {
+    $this->outputFile('unavailable');
+  }
+
+  /**
+  * retrieves and parses file for output
+  *
+  * @return string
+  * @param $file Base name of file to parse
+  */
+  public function outputFile($file)
+  {
     require_once('markdown.php');
 
     $hostPrefix = '/content/';
+
     if ('sandbox.metadataregistry.org' == $this->getRequest()->getHost())
     {
-      $hostPrefix .= 'sandbox_';
+      $hostPrefix .= 'sandbox_' ;
     }
-    $fileRoot = sfConfig::get('sf_data_dir') . $hostPrefix . 'home_';
+
+    $fileRoot = sfConfig::get('sf_data_dir') . $hostPrefix . $file . '_';
     $file = $fileRoot . $this->getUser()->getCulture() . '.txt';
+
     if (!is_readable($file))
     {
       $file = $fileRoot . '_en.txt';
@@ -28,34 +52,7 @@ class contentActions extends sfActions
 
     $this->html = markdown(file_get_contents($file));
 
-    $this->getContext()->getResponse()->setTitle('The Registry! :: home');
+    $this->getContext()->getResponse()->setTitle(sfConfig::get('app_title_prefix') . $file);
   }
 
-  public function executeAbout()
-  {
-    require_once('markdown.php');
-
-    $file = sfConfig::get('sf_data_dir') . $hostPrefix . 'about_'.$this->getUser()->getCulture().'.txt';
-    if (!is_readable($file))
-    {
-      $file = sfConfig::get('sf_data_dir') . $hostPrefix . 'about_en.txt';
-    }
-
-    $this->html = markdown(file_get_contents($file));
-
-    $this->getContext()->getResponse()->setTitle('The Registry! :: about');
-  }
-
-  public function executeUnavailable()
-  {
-    require_once('markdown.php');
-
-    $file = sfConfig::get('sf_data_dir') . $hostPrefix . 'unavailable_'.$this->getUser()->getCulture().'.txt';
-    if (!is_readable($file))
-    {
-      $file = sfConfig::get('sf_data_dir') . $hostPrefix . 'unavailable_en.txt';
-    }
-
-    $this->getContext()->getResponse()->setTitle('The Registry! :: maintenance');
-  }
 }
