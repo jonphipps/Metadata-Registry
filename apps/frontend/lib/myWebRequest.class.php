76ebe892a7920e10408bfcd67e61300422b412e8
Jon Phipps <jphipps@madcreek.com>
2008-02-09 17:34:48 +0000

    make myWebRequest the default webrequest factory so we can add some new methods

--- apps/frontend/lib/myWebRequest.class.php
+++ apps/frontend/lib/myWebRequest.class.php
@@ -2,5 +2,16 @@
 
 class myWebRequest extends sfWebRequest
 {
+  /**
+  * Retrieves a single param from PathInfo
+  *
+  * @return string
+  * @param  string $param The parameter to return
+  */
+  public function getPathInfoParam($param)
+  {
+    $patharray = $this->getPathInfoArray();
+    return $patharray[$param];
+  }
 }
 
