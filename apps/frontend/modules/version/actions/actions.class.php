e7e58e4702de28659723a859dd07bcef71770475
Jon Phipps <jphipps@madcreek.com>
2008-02-12 20:07:17 +0000

    add version module

---
+++ apps/frontend/modules/version/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * version actions.
+ *
+ * @package    registry
+ * @subpackage version
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class versionActions extends autoversionActions
+{
+}
