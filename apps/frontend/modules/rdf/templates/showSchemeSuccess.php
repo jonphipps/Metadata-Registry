7b9237942bbe25b794455e6c6e1b4c8928a830a0
Jon Phipps <jphipps@madcreek.com>
2008-03-19 23:13:09 +0000

    should not show timeslice comment if no timeslice

--- apps/frontend/modules/rdf/templates/showSchemeSuccess.php
+++ apps/frontend/modules/rdf/templates/showSchemeSuccess.php
@@ -1,12 +1,15 @@
 <?php echo include_partial('rdf/rdfHead') ?>
 
+<?php if ($timestamp): ?>
 <!--
 NOTICE: This is a TimeSlice of this Vocabulary as of:
   <?php echo date(DATE_W3C, $timestamp) ?>.
 
 The most current complete Vocabulary may be retrieved from:
   <?php echo $vocabulary->getUri() ?>
+
 -->
+<?php endif; ?>
 
 <?php echo include_partial('rdf/scheme', array(
       'vocabulary' => $vocabulary,
