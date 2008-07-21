1c43e8e152633944521f72a7d8fae2273e6d1588
Jon Phipps <jphipps@madcreek.com>
2008-07-21 20:48:57 +0000

    fix the 'update the uri' question for schema properties

--- web/js/schemaprop.js
+++ web/js/schemaprop.js
@@ -2,15 +2,18 @@ function updateUri()
 {
    var domainField = $('schema_uri');
    var tokenField = $('schema_property_name');
+   var tokenValue = $('schema_property_name').value;
    var uriField = $('schema_property_uri');
-   var updateIt = true;
-   if(uriField.value != domainField.value + tokenField.value)
+   var nextField = $('schema_property_definition');
+   var updateIt;
+   if(tokenValue != tokenField.defaultValue)
    {
       updateIt = confirm("Automatically update the URI based on your changes?");
    }
    if(updateIt)
    {
-      uriField.value = domainField.value + tokenField.value;
+      uriField.value = domainField.value + tokenValue;
+      tokenField.defaultValue = tokenValue;
       uriField.select();
    }
 };
