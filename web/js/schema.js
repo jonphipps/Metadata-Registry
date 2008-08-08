7e57e9e53f5c58b9a3b192a5f87d4ee60bcaa733
Jon Phipps <jphipps@madcreek.com>
2008-08-08 23:29:59 +0000

    Fix URI generator for schema

--- web/js/schema.js
+++ web/js/schema.js
@@ -1,12 +1,12 @@
 function updateUri()
 {
-   var domainField = $('schema_base_domain');
+   var domainField = $('base_domain');
    var tokenField = $('schema_token');
    var uriField = $('schema_uri');
    var updateIt = true;
-   if(uriField.value != domainField.value + tokenField.value)
+   if('' != tokenField.value && uriField.value != domainField.value + tokenField.value)
    {
-      updateIt = confirm("Automatically update the URI based on your changes?");
+      updateIt = confirm("Automatically update the Namespace URI based on your changes?");
    }
    if(updateIt)
    {
