33d31943ebfeef3461b25e5c7aa8a364f93b4de4
Jon Phipps <jphipps@madcreek.com>
2009-03-06 21:44:29 +0000

    add support for domain/range and no specification for resource type

--- web/js/schemaprop/swapoptions.js
+++ web/js/schemaprop/swapoptions.js
@@ -8,6 +8,8 @@ function swapOptions()
     {
       Element.hide('form_row_schema_property_is_subclass_of');
       Element.show('form_row_schema_property_is_subproperty_of');
+      Element.show('form_row_schema_property_domain');
+      Element.show('form_row_schema_property_range');
       if(selectedType.onfocus)
       {
         $('schema_property_is_subproperty_of').focus();
@@ -24,11 +26,21 @@ function swapOptions()
     }
     Element.show('form_row_schema_property_parent_uri');
   }
+  else if(selectedType == 'property')
+  {
+    Element.hide('form_row_schema_property_is_subproperty_of');
+    Element.hide('form_row_schema_property_is_subclass_of');
+    Element.hide('form_row_schema_property_parent_uri');
+    Element.show('form_row_schema_property_domain');
+    Element.show('form_row_schema_property_range');
+  }
   else
   {
     Element.hide('form_row_schema_property_is_subproperty_of');
     Element.hide('form_row_schema_property_is_subclass_of');
     Element.hide('form_row_schema_property_parent_uri');
+    Element.hide('form_row_schema_property_domain');
+    Element.hide('form_row_schema_property_range');
     $('schema_property_is_subproperty_of').selectedIndex = '';
     if(selectedType.onfocus)
     {
