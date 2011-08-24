fc401f53cf63017e07a1ffba4bc9763892c3e42b
Jon Phipps <jphipps@madcreek.com>
2011-08-24 14:57:16 -0400

    move the schema property element creation code out of the property controller and into the model

--- lib/model/SchemaProperty.php
+++ lib/model/SchemaProperty.php
@@ -144,4 +144,278 @@ class SchemaProperty extends BaseSchemaProperty
   } // setIsSubpropertyOf()
 
 
+  /**
+  * overload schemaSave
+  *
+  * @return mixed
+  * @param  integer $userId
+  */
+  public function saveSchemaProperty($userId)
+  {
+    //if the property is modified then
+    if ($this->isModified() || $this->isNew())
+    {
+      $this->setUpdatedUserId($userId);
+
+      //check for changes to subclass/subproperty
+      //if property or class then
+      if ("class" == $this->getType() or "property" == $this->getType())
+      {
+        //delete is_subclassof (this also clears subproperty)
+        $this->SetIsSubclassOf(null);
+        //delete parent_uri
+        $this->setParentUri(null);
+      }
+
+      if ("class" == $this->getType() or "subclass" == $this->getType())
+      {
+        //delete domain and range
+        $this->setDomain(null);
+        $this->setOrange(null);
+      }
+
+      $fields = Schema::getProfileFields();
+      //add parent_uri to fields
+      $fields[] = "parent_uri";
+
+      $con = Propel::getConnection(SchemaPropertyPeer::DATABASE_NAME);
+
+      //did we hand-edit the URI
+      if ($this->isColumnModified(SchemaPropertyPeer::PARENT_URI))
+      {
+        //if we didn't just clear it, null the related IDs
+        if (0 != strlen(rtrim($this->getParentUri())))
+        {
+          $this->SetIsSubclassOf(null);
+          $this->setIsSubpropertyOf(null);
+        }
+      }
+
+      //get the URI for the related property or class
+      if ("subclass" == $this->getType())
+      {
+        $relatedId = $this->getIsSubclassOf();
+      }
+      if ("subproperty" == $this->getType())
+      {
+        $relatedId = $this->getIsSubpropertyOf();
+      }
+
+      if (isset($relatedId))
+      {
+        $related = SchemaPropertyPeer::retrieveByPK($relatedId);
+        if ($related)
+        {
+          $relatedUri = $related->getUri();
+          $this->setParentUri($related->getUri());
+        }
+      }
+
+      try
+      {
+        //start a transaction
+        $con->begin();
+
+        //if the property is new then
+        if ($this->isNew())
+        {
+          //set the created user
+          $this->setCreatedUserId($userId);
+
+          //save it first
+          $affectedRows = $this->save($con);
+
+          if ($affectedRows)
+          {
+            //create new elements for each part
+            foreach ($fields as $id => $field)
+            {
+              $object = $this->getFieldValue($field);
+
+              if ($object)
+              {
+                //fix the floating uri problem
+                if ('parent_uri' == $field)
+                {
+                  $key = array_keys($fields, 'is_subproperty_of');
+                  $id = count($key) ? $key[0] : null;
+                  $field = 'is_subproperty_of';
+                }
+                //fix the which sub property am I problem
+                if ('is_subproperty_of' == $field && 'subclass' == $this->getType())
+                {
+                  $key = array_keys($fields, 'is_subclass_of');
+                  $id = count($key) ? $key[0] : null;
+                  $field = 'is_subclass_of';
+                }
+                $element = SchemaPropertyElementPeer::createElement($this, $userId, $id);
+                $element = $this->updateElement($element, $userId, $field, $object, $con);
+              }
+            }
+          }
+        }
+        else
+        {
+          //FIXME if the language is modified we have to update all of the existing old languages
+          //FIXME if the status is modified we have to update all of the existing old statuses
+          //So what really needs to happen is that we leave the language and status blank
+          //  and walk up the dependency tree until we find a value:
+          //  element language == null
+          //    property language = null
+          //      schema language == en-us <== this is what we use
+
+          //get all of the existing elements
+          $elements =  $this->getSchemaPropertyElementsRelatedBySchemaPropertyId();
+          foreach ($fields as $id => $field)
+          {
+            try
+            {
+              $column = SchemaPropertyPeer::translateFieldname($field, BasePeer::TYPE_FIELDNAME,BasePeer::TYPE_COLNAME);
+            }
+            catch (PropelException $e)
+            {
+              $column = false;
+            }
+
+            $object = $this->getFieldValue($field);
+
+            //see if they've been updated
+            if ($column && $this->isColumnModified($column))
+            {
+              //fix the floating uri problem
+              if ('parent_uri' == $field)
+              {
+                $key = array_keys($fields, 'is_subproperty_of');
+                $id = count($key) ? $key[0] : null;
+                $field = 'is_subproperty_of';
+              }
+              //fix the which sub property am I problem
+              if ('is_subproperty_of' == $field && 'subclass' == $this->getType())
+              {
+                $key = array_keys($fields, 'is_subclass_of');
+                $id = count($key) ? $key[0] : null;
+                $field = 'is_subclass_of';
+              }
+              //find the element
+              $foundOne = false;
+              foreach ($elements as $element)
+              {
+                if ($id == $element->getProfilePropertyId())
+                {
+                  //did we make it null?
+                  if (0 === strlen(trim($object)))
+                  {
+                    //we have to make sure that it's not a subclass or subproperty
+                    if (('is_subproperty_of' == $field || 'is_subclass_of' == $field) && $this->getParentUri())
+                    {
+                      //there's a uri but it doesn't match anything registered
+                      //so we have to delete just the reciprocal
+                      $element->updateReciprocal('deleted', $con);
+                    }
+                    else
+                    {
+                      //delete the element
+                      $element->delete($con);
+                      $element = false;
+                    }
+                  }
+                  $foundOne = true;
+                  break;
+                }
+              }
+
+              if ($object)
+              {
+                if (!$foundOne)
+                {
+                  //we have to create one
+                  $element = SchemaPropertyElementPeer::createElement($this, $userId, $id);
+                }
+
+                if ($element)
+                {
+                  $element = $this->updateElement($element, $userId, $field, $object, $con);
+                }
+              }
+            }
+          }
+
+          //save it last
+          $affectedRows = $this->save($con);
+
+        }
+
+        //commit the transaction
+        $con->commit();
+
+        return $affectedRows;
+      }
+      catch (PropelException $e)
+      {
+        $con->rollback();
+        throw $e;
+      }
+    }
+  } //saveSchemaProperty
+
+  /**
+  * create a new element
+  *
+  * @return SchemaPropertyElement
+  * @param  SchemaPropertyElement $element
+  * @param  Connection $con
+  */
+  public function updateElement(SchemaPropertyElement $element, $userId, $field, $object, $con)
+  {
+    static $updatedUri;
+
+    if ($element)
+    {
+      $element->setIsSchemaProperty(true);
+      $element->setUpdatedUserId($userId);
+      //SchemaPropertyElementPeer::updateElement($schema_property, $element, $userId, $field, $con);
+
+      if ('is_subproperty_of' == $field || 'is_subclass_of' == $field)
+      {
+        if (!$updatedUri)
+        {
+          $element->setRelatedSchemaPropertyId($this->getIsSubpropertyOf());
+          $object = $this->getParentUri();
+          $updatedUri = true;
+        }
+        Else
+        {
+          return false;
+        }
+      }
+
+      $element->setObject($object ? $object : '');
+
+      $element->save($con);
+
+    }
+
+    return $element;
+  }
+
+  /**
+  * gets the value of a field by name
+  *
+  * @return mixed
+  * @param  string $field name to fetch
+  */
+  public function getFieldValue($field)
+  {
+    try
+    {
+      $fieldTest = $this->getByName($field, BasePeer::TYPE_FIELDNAME);
+    }
+    catch (PropelException $e)
+    {
+      $fieldTest = false;
+    }
+    return $fieldTest;
+  }
+
+
 }
