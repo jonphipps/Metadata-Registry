1b0ebc226608290eb68449cbf2d130d9901482b0
Jon Phipps <jphipps@madcreek.com>
2011-10-04 13:59:12 -0400

    Added trailing slash to feed url

--- apps/frontend/modules/content/actions/actions.class.php
+++ apps/frontend/modules/content/actions/actions.class.php
@@ -66,7 +66,7 @@ class contentActions extends sfActions
     $RSS_PHP = new rss_php();
     try
     {
-      $feed = $RSS_PHP->load('http://metadataregistry.org/blog/category/registry-development/the-registry/feed');
+      $feed = $RSS_PHP->load('http://metadataregistry.org/blog/category/registry-development/the-registry/feed/');
       if ($feed)
       {
         $this->rssItems = $RSS_PHP->getItems();
