cba3be4cd4e815d63a3b90368544a42a84867d71
Jon Phipps <jphipps@madcreek.com>
2011-10-04 18:07:41 -0400

    remove quotes around script

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -24,7 +24,7 @@
 </head>
 <body>
 <?php if ($_SERVER['SERVER_NAME'] == 'sandbox.metadataregistry.org' or $_SERVER['SERVER_NAME'] == 'metadataregistry.org'): //Get Satisfaction  ?>
-    '<script type="text/javascript" charset="utf-8"> var is_ssl = ("https:" == document.location.protocol); var asset_host = is_ssl ? "https://s3.amazonaws.com/getsatisfaction.com/" : "http://s3.amazonaws.com/getsatisfaction.com/"; document.write(unescape("%3Cscript src='" + asset_host + "javascripts/feedback-v2.js' type='text/javascript'%3E%3C/script%3E")); </script><script type="text/javascript" charset="utf-8"> var feedback_widget_options = {}; feedback_widget_options.display = "overlay"; feedback_widget_options.company = "metadataregistry"; feedback_widget_options.placement = "right"; feedback_widget_options.color = "#D8732F"; feedback_widget_options.style = "question"; var feedback_widget = new GSFN.feedback_widget(feedback_widget_options); </script>';
+    <script type="text/javascript" charset="utf-8"> var is_ssl = ("https:" == document.location.protocol); var asset_host = is_ssl ? "https://s3.amazonaws.com/getsatisfaction.com/" : "http://s3.amazonaws.com/getsatisfaction.com/"; document.write(unescape("%3Cscript src='" + asset_host + "javascripts/feedback-v2.js' type='text/javascript'%3E%3C/script%3E")); </script><script type="text/javascript" charset="utf-8"> var feedback_widget_options = {}; feedback_widget_options.display = "overlay"; feedback_widget_options.company = "metadataregistry"; feedback_widget_options.placement = "right"; feedback_widget_options.color = "#D8732F"; feedback_widget_options.style = "question"; var feedback_widget = new GSFN.feedback_widget(feedback_widget_options); </script>
 <?php endif; ?>
 
   <?php use_helper('Javascript')?>
