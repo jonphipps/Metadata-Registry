357284657fa1bb7ac5e2899469ba8093ec3c63de
Jon Phipps <jphipps@madcreek.com>
2014-04-14 19:24:29 -0400

    add maintainer to some schemauser actions

--- apps/frontend/modules/schemauser/templates/_list_td_actions.php
+++ apps/frontend/modules/schemauser/templates/_list_td_actions.php
@@ -1,7 +1,7 @@
 <td>
 <ul class="sf_admin_td_actions">
   <li><?php echo link_to(image_tag('/jpAdminPlugin/images/show_icon.png', array('alt' => __('show'), 'title' => __('show'))), 'schemauser/show?id='.$schema_has_user->getId()) ?></li>
-  <li><?php if ($sf_user->hasObjectCredential($schema_has_user->getSchemaId(), 'schema',  array (   0 =>    array (     0 => 'administrator',     1 => 'schemaadmin',     2 => 'agentadmin',   ), ))
+  <li><?php if ($sf_user->hasObjectCredential($schema_has_user->getSchemaId(), 'schema',  array (   0 =>    array (     0 => 'administrator',     1 => 'schemaadmin',     2 => 'schemamaintainer',     3 => 'agentadmin'   ), ))
   && $schema_has_user->getUserId() != $sf_user->getAttribute('subscriber_id','','subscriber')): ?>
 <?php echo link_to(image_tag('/jpAdminPlugin/images/edit_icon.png', array('alt' => __('edit'), 'title' => __('edit'))), 'schemauser/edit?id='.$schema_has_user->getId()) ?>
 <?php else: ?>
--- apps/frontend/modules/schemauser/templates/_show_actions.php
+++ apps/frontend/modules/schemauser/templates/_show_actions.php
@@ -10,7 +10,7 @@
 )) ?><?php endif; ?>
 </li>
  <?php endif; ?>
-<?php if ($sf_user->hasObjectCredential($schema_has_user->getSchemaId(), 'schema',  array (   0 =>    array (     0 => 'administrator',     1 => 'schemaadmin',     2 => 'agentadmin',   ), ))
+<?php if ($sf_user->hasObjectCredential($schema_has_user->getSchemaId(), 'schema',  array (   0 =>    array (     0 => 'administrator',     1 => 'schemaadmin',     2 => 'agentadmin',     3 => 'schemamaintainer',  ), ))
  && $schema_has_user->getUserId() != $sf_user->getAttribute('subscriber_id','','subscriber')): ?>
 <li><?php echo button_to(__('Edit'), 'schemauser/edit?id='.$schema_has_user->getId(), array (
   'title' => 'Edit',
