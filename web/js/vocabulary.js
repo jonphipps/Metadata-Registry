e026c53a1565dd97901a3202faaa53c4cab2e35f
Jon Phipps <jphipps@madcreek.com>
2014-04-18 13:35:13 -0400

    add select2 to javascript for vocab

--- web/js/vocabulary.js
+++ web/js/vocabulary.js
@@ -1,15 +1,25 @@
-function updateUri()
-{
-   var domainField = $('vocabulary_base_domain');
-   var tokenField = $('vocabulary_token');
-   var uriField = $('vocabulary_uri');
-   var updateIt = true;
-   if(uriField.value != domainField.value + tokenField.value)
-   {
-      updateIt = confirm("Automatically update the URI based on your changes?");
-   }
-   if(updateIt)
-   {
-      uriField.value = domainField.value + tokenField.value
-   }
-};
+function updateUri () {
+  var domainField = $('vocabulary_base_domain');
+  var tokenField = $('vocabulary_token');
+  var uriField = $('vocabulary_uri');
+  var updateIt = true;
+  if (uriField.value != domainField.value + tokenField.value) {
+    updateIt = confirm("Automatically update the URI based on your changes?");
+  }
+  if (updateIt) {
+    uriField.value = domainField.value + tokenField.value
+  }
+}
+
+var $jq = jQuery.noConflict();
+$jq(document).ready(function () {
+  $jq("#vocabulary_languages").select2({
+    placeholder: "Select a Language",
+    allowClear: true
+  });
+  $jq("#vocabulary_languages").on("change",
+      function () {
+        $jq("#vocabulary_language").select2("data",
+            $jq("#vocabulary_languages").select2("data"))
+      });
+});
