5f79fa872c964f2491f12b89a0398589e3e73f67
Jon Phipps <jphipps@madcreek.com>
2014-04-22 17:11:24 -0400

    comment, whitespace

--- web/frontend_test-empty.php
+++ web/frontend_test-empty.php
@@ -1,10 +1,11 @@
 <?php
 
-define('SF_ROOT_DIR',    realpath(dirname(__FILE__).'/..'));
-define('SF_APP',         'frontend');
-define('SF_ENVIRONMENT', 'test-empty');
-define('SF_DEBUG',       true);
+  define('SF_ROOT_DIR', realpath(dirname(__FILE__) . '/..'));
+  define('SF_APP', 'frontend');
+  define('SF_ENVIRONMENT', 'test-empty');
+  define('SF_DEBUG', TRUE);
 
-require_once(SF_ROOT_DIR.DIRECTORY_SEPARATOR.'apps'.DIRECTORY_SEPARATOR.SF_APP.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php');
+  /** @noinspection PhpIncludeInspection */
+  require_once(SF_ROOT_DIR . DIRECTORY_SEPARATOR . 'apps' . DIRECTORY_SEPARATOR . SF_APP . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'config.php');
 
-sfContext::getInstance()->getController()->dispatch();
+  sfContext::getInstance()->getController()->dispatch();
