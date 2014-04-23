7c5640131256278d73de2ae7cf7d138cf31ccb52
Jon Phipps <jphipps@madcreek.com>
2014-04-23 17:51:55 -0400

    update codeception to 2.0

---
+++ test/codeception/acceptance/loginCest.php
@@ -0,0 +1,18 @@
+<?php
+use \WebDev;
+
+class loginCest
+{
+    public function _before()
+    {
+    }
+
+    public function _after()
+    {
+    }
+
+    // tests
+    public function tryToTest(WebDev $I)
+    {
+    }
+}
