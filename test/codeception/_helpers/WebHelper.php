3ca9830016b09f4a3e76bd0bead3887eaf11c7bd
Jon Phipps <jphipps@madcreek.com>
2014-05-13 11:11:29 -0400

    add setwindowsize to webhelper (redundant but informational)

--- test/codeception/_helpers/WebHelper.php
+++ test/codeception/_helpers/WebHelper.php
@@ -4,7 +4,20 @@ namespace Codeception\Module;
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
-class WebHelper extends \Codeception\Module
+use Codeception\Module;
+
+class WebHelper extends Module
 {
+  /**
+   * @param int $width
+   * @param int $height
+   *
+   * @throws \Codeception\Exception\Module
+   */
+  public function setWindowSize($width, $height) {
+    /** @var $module WebDriver */
+    $module = $this->getModule("WebDriver");
+    $module->resizeWindow($width, $height);
+  }
 
 }
