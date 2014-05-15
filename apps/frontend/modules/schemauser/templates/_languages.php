3963105f4c6115d034bd22ecce75ccd480fce5e6
Jon Phipps <jphipps@madcreek.com>
2014-05-15 15:54:04 -0400

    add all languages to schema user

--- apps/frontend/modules/schemauser/templates/_languages.php
+++ apps/frontend/modules/schemauser/templates/_languages.php
@@ -1,8 +1,16 @@
 <?php  $languages = '';
-if (is_array($schema_has_user->getLanguages())) {
-  foreach ($schema_has_user->getLanguages() as $language) {
-    $languages .= format_language($language) . ", ";
+  $languageArray  = $schema_has_user->getLanguages();
+  if (is_array($languageArray)) {
+    if (in_array("*", $languageArray)) {
+      //get the data from the schema instead
+      $languageArray = $schema_has_user->getLanguagesForSchema();
+    }
+    if (is_array($languageArray)) {
+
+      foreach ($languageArray as $language) {
+        $languages .= format_language($language) . ", ";
+      }
+      $languages = rtrim($languages, ", ");
+    }
   }
-  $languages = rtrim($languages, ", ");
-}
-echo $languages ?>
+  echo $languages ?>
