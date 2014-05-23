a4605d458f5c97ba5509fbefc691c3277a1f8c8a
Jon Phipps <jphipps@madcreek.com>
2014-05-23 15:50:13 -0400

    adds some property hints

--- apps/frontend/modules/schemaprop/actions/components.class.php
+++ apps/frontend/modules/schemaprop/actions/components.class.php
@@ -1,11 +1,14 @@
 <?php
-class schemapropComponents extends sfComponents
-{
-  public function executePropertylist()
-  {
-    $c = new Criteria();
-    $c->add(SchemaPropertyPeer::SCHEMA_ID, $this->schemaId);
+
+  /**
+   * @property SchemaProperty[] properties
+   * @property int              schemaId
+   */
+  class schemapropComponents extends sfComponents {
+    public function executePropertylist() {
+      $c = new Criteria();
+      $c->add(SchemaPropertyPeer::SCHEMA_ID, $this->schemaId);
 //    $c->addDescendingOrderByColumn(ConceptPropertyPeer::SKOS_PROPERTY_ID);
-    $this->properties = SchemaPropertyPeer::doSelect($c);
+      $this->properties = SchemaPropertyPeer::doSelect($c);
+    }
   }
-}
