ab5733de414d040cc04adc024cac6e5a232f9ba3
Jon Phipps <jphipps@madcreek.com>
2014-06-13 17:26:09 -0400

    reformat code

--- apps/frontend/modules/schema/actions/actions.class.php
+++ apps/frontend/modules/schema/actions/actions.class.php
@@ -1,70 +1,59 @@
 <?php
 
-/**
- * schema actions.
- *
- * @package    registry
- * @subpackage schema
- * @author     Jon Phipps <jonphipps@gmail.com>
- * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
- */
-class schemaActions extends autoschemaActions
-{
-/**
-* Set defaults
-*
-* @param  Schema $schema
-*/
-  public function setDefaults($schema)
-  {
-    $baseDomain = $this->getRequest()->getUriPrefix() . '/uri';
-    $schema->setBaseDomain($baseDomain . "/schema/");
-    $schema->setLanguage(sfConfig::get('app_default_language'));
-    $schema->setProfileId(sfConfig::get('app_schema_profile_id'));
-    parent::setDefaults($schema);
-  }
-
-  public function executeImport ()
-  {
-    //set the form to display just the import if it's a get
-    //if it's a post, we redirect to the import module
-    if ($this->getRequest()->getMethod() == sfRequest::POST)
-    {
-      $this->forward('file', 'import');
+  /**
+   * schema actions.
+   *
+   * @package    registry
+   * @subpackage schema
+   * @author     Jon Phipps <jonphipps@gmail.com>
+   * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+   */
+  class schemaActions extends autoschemaActions {
+    /**
+     * Set defaults
+     *
+     * @param  Schema $schema
+     */
+    public function setDefaults($schema) {
+      $baseDomain = $this->getRequest()->getUriPrefix() . '/uri';
+      $schema->setBaseDomain($baseDomain . "/schema/");
+      $schema->setLanguage(sfConfig::get('app_default_language'));
+      $schema->setProfileId(sfConfig::get('app_schema_profile_id'));
+      parent::setDefaults($schema);
     }
 
-
-  }
-
-  public function executeList ()
-  {
-    //clear any detail filters
-    $this->getUser()->getAttributeHolder()->removeNamespace('sf_admin/schema_property/filters');
-    $this->getUser()->getAttributeHolder()->removeNamespace('sf_admin/schema_property_element/filters');
-    $this->getUser()->getAttributeHolder()->removeNamespace('sf_admin/schema_property_element_history/filters');
-    parent::executeList();
-  }
-
-  public function executeSave()
-  {
-    $this->getUser()->getAttributeHolder()->remove('schema');
-    parent::executeSave();
-  }
-
-  public function executeShowRdf ()
-  {
-    $ts = strtotime($this->getRequestParameter('ts'));
-    $this->timestamp = $ts;
-    if (!$this->schema)
-    {
-      $this->schema = SchemaPeer::retrieveByPk($this->getRequestParameter('id'));
+    public function executeImport() {
+      //set the form to display just the import if it's a get
+      //if it's a post, we redirect to the import module
+      if ($this->getRequest()->getMethod() == sfRequest::POST) {
+        $this->forward('file', 'import');
+      }
     }
-    $this->labels = $this->getLabels('show');
 
-    $this->forward404Unless($this->schema);
-    $this->properties = $this->schema->getProperties();
-    $this->classes = $this->schema->getClasses();
+    public function executeList() {
+      //clear any detail filters
+      $this->getUser()->getAttributeHolder()->removeNamespace('sf_admin/schema_property/filters');
+      $this->getUser()->getAttributeHolder()->removeNamespace('sf_admin/schema_property_element/filters');
+      $this->getUser()->getAttributeHolder()->removeNamespace('sf_admin/schema_property_element_history/filters');
+      parent::executeList();
+    }
 
-     //$this->forward('rdf','ShowSchema');
+    public function executeSave() {
+      $this->getUser()->getAttributeHolder()->remove('schema');
+      parent::executeSave();
+    }
+
+    public function executeShowRdf() {
+      $ts              = strtotime($this->getRequestParameter('ts'));
+      $this->timestamp = $ts;
+      if (!$this->schema) {
+        $this->schema = SchemaPeer::retrieveByPk($this->getRequestParameter('id'));
+      }
+      $this->labels = $this->getLabels('show');
+
+      $this->forward404Unless($this->schema);
+      $this->properties = $this->schema->getProperties();
+      $this->classes    = $this->schema->getClasses();
+      //$this->forward('rdf','ShowSchema');
+    }
   }
-}
