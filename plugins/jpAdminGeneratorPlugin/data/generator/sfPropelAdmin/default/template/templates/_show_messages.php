c3e269abf42fbd326f9e5b816328a422c929d27d
Jon Phipps <jphipps@madcreek.com>
2014-06-17 15:55:13 -0400

    add error message to flash messages

--- plugins/jpAdminGeneratorPlugin/data/generator/sfPropelAdmin/default/template/templates/_show_messages.php
+++ plugins/jpAdminGeneratorPlugin/data/generator/sfPropelAdmin/default/template/templates/_show_messages.php
@@ -3,3 +3,8 @@
 <h2>[?php echo __($sf_flash->get('notice')) ?]</h2>
 </div>
 [?php endif; ?]
+[?php if ($sf_flash->has('error')): ?]
+<div class="save-error">
+  <h2>[?php echo __($sf_flash->get('error')) ?]</h2>
+</div>
+[?php endif; ?]
