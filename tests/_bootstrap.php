382c1772f184d02a2a1e5552fedb97df146bab14
Jon Phipps <jphipps@madcreek.com>
2015-01-19 18:36:38 -0500

    change codeception load to get params from environment

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -11,6 +11,18 @@ if (file_exists(SF_ROOT_DIR.'/tests/.env'))
 {
     Dotenv::load(SF_ROOT_DIR.'/tests');
 }
+//initialize codeception db
+\Codeception\Configuration::$defaultSuiteSettings['modules']['config'] = [
+  'Db' => [
+    'dsn' => getenv('DB_DSN'),
+    'user' => getenv('DB_USER'),
+    'password' => getenv('DB_PASS'),
+    'dump' => 'tests/_data/swregistry_test.sql',
+    'populate' => true,
+    'cleanup' => false
+  ]
+];
+
 //initialize composer
 require_once(SF_ROOT_DIR.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php');
 // initialize symfony
@@ -18,3 +30,4 @@ require_once(SF_ROOT_DIR.DIRECTORY_SEPARATOR.'apps'.DIRECTORY_SEPARATOR.SF_APP.D
 // initialize database manager
 $databaseManager = new \sfDatabaseManager();
 $databaseManager->initialize();
+
