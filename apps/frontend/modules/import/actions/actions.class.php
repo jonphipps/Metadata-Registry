4363d9a1fe4e12d748e441c42215335aa0d0e00b
Jon Phipps <jphipps@madcreek.com>
2015-04-12 13:13:30 -0400

    Add error messages to import

--- apps/frontend/modules/import/actions/actions.class.php
+++ apps/frontend/modules/import/actions/actions.class.php
@@ -60,8 +60,46 @@ class importActions extends autoimportActions
       $import = new ImportVocab( 'schema', $filePath, $schemaId );
       $prolog = $import->processProlog();
       //todo we need a validation check to make sure that if there's a schema_id in the prolog that it matches the current ID
+      if (isset($prolog['meta']['schema_id']) and $prolog['meta']['schema_id'] != $schemaId)
+      {
+        $this->getRequest()->setError('Schema Id match', 'The Schema Id in the file you are importing does not match the Schema Id of this
+        Element Set');
+        $this->setFlash( 'error', 'The Schema Id ('. $prolog['meta']['schema_id'] .') in the file you are importing
+        does not match the Schema Id (' . $schemaId . ') of this Element Set. They must be the same Schema' );
+        return $this->redirect( 'import/create?schema_id=' . $this->file_import_history->getId() );
+
+      }
+      //todo we need to check to make sure the user is an admin
+      $user = $this->getUser();
+      if ( ! $user->hasCredential(array (
+            0 => array (
+                  0 => 'administrator',
+                  1 => 'schemaadmin',
+            ),
+      ))
+      ) {
+        $this->setFlash('error', 'You must be an administrator of this Element Set to import.');
+
+        return $this->redirect('import/create?schema_id=' . $this->file_import_history->getId());
+      }
       //todo update the prefixes table with prefixes
       //todo update the schema table with prefixes
+      $schema = $this->getCurrentSchema();
+      $schemaPrefixes = $schema->getPrefixes();
+      $countSchemaPrefixes = count($schemaPrefixes);
+      $importPrefixes = $import->prolog['prefix'];
+      foreach ($importPrefixes as $prefix => $url) {
+        if (!array_key_exists($prefix,$schemaPrefixes))
+        {
+          $schemaPrefixes[$prefix] = $url;
+        }
+      }
+      if (count($schemaPrefixes) != $countSchemaPrefixes)
+      {
+        $schema->setPrefixes($schemaPrefixes);
+        $schema->save();
+      }
+
       //todo display some sort of progress indicator on the page, or enqueue the process and send a results email
       $import->processData();
       $this->file_import_history->setResults($import->results);
