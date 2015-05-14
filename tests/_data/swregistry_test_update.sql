94723862e67ed060bccc62c57d461871808a631a
Jon Phipps <jphipps@madcreek.com>
2015-05-14 17:07:29 -0400

    revise the sql

--- tests/_data/swregistry_test_update.sql
+++ tests/_data/swregistry_test_update.sql
@@ -646,7 +646,7 @@ UNLOCK TABLES;
 LOCK TABLES `reg_schema_property` WRITE;
 ALTER TABLE `reg_schema_property` DISABLE KEYS;
 INSERT INTO `reg_schema_property` (`id`, `created_at`, `updated_at`, `deleted_at`, `created_user_id`, `updated_user_id`, `schema_id`, `name`, `label`, `definition`, `comment`, `type`, `is_subproperty_of`, `parent_uri`, `uri`, `status_id`, `language`, `note`, `domain`, `orange`, `is_deprecated`, `url`, `lexical_alias`) VALUES 
-	(15536,'2014-01-19 11:29:58','2015-04-30 22:00:17',NULL,422,422,81,'respondentOf','is respondent of','Relates a candidate for a degree who defends or opposes a thesis provided by the praeses in an academic disputation to the work.','','property',NULL,'http://rdaregistry.info/Elements/a/P50204','http://rdaregistry.info/Elements/a/P50001',1,'en','','http://rdaregistry.info/Elements/c/C10004','http://rdaregistry.info/Elements/c/C10001',NULL,NULL,'http://rdaregistry.info/Elements/a/respondentOf');
+	(15536,'2014-01-19 11:29:58','2014-01-19 06:29:58',NULL,422,422,81,'respondentOf','is respondent of','Relates a candidate for a degree who defends or opposes a thesis provided by the praeses in an academic disputation to the work.','','property',NULL,'http://rdaregistry.info/Elements/a/P50204','http://rdaregistry.info/Elements/a/P50001',1,'en','','http://rdaregistry.info/Elements/c/C10004','http://rdaregistry.info/Elements/c/C10001',NULL,NULL,NULL);
 ALTER TABLE `reg_schema_property` ENABLE KEYS;
 UNLOCK TABLES;
 
@@ -657,25 +657,15 @@ INSERT INTO `reg_schema_property_element` (`id`, `created_at`, `updated_at`, `de
 	(121276,'2014-01-19 11:29:58','2014-01-19 06:29:58',NULL,422,422,15536,1,1,'respondentOf',NULL,'en',1),
 	(121277,'2014-01-19 11:29:58','2014-01-19 06:29:58',NULL,422,422,15536,2,1,'is respondent of',NULL,'en',1),
 	(121278,'2014-01-19 11:29:58','2014-01-19 06:29:58',NULL,422,422,15536,3,1,'Relates a candidate for a degree who defends or opposes a thesis provided by the praeses in an academic disputation to the work.',NULL,'en',1),
-	(121279,'2014-01-19 11:29:58','2015-04-30 21:34:57','2015-04-30 21:34:57',422,422,15536,4,1,'subproperty',NULL,'en',1),
-	(121280,'2014-01-19 11:29:58','2015-04-30 21:34:38','2015-04-30 21:34:38',422,422,15536,11,1,'http://rdaregistry.info/Elements/c/C10004',NULL,'en',1),
-	(121281,'2014-01-19 11:29:58','2015-04-30 21:34:39','2015-04-30 21:34:39',422,422,15536,12,1,'http://rdaregistry.info/Elements/c/C10001',NULL,'en',1),
+	(121279,'2014-01-19 11:29:58','2014-01-19 06:29:58',NULL,422,422,15536,4,1,'subproperty',NULL,'en',1),
+	(121280,'2014-01-19 11:29:58','2014-01-19 06:29:58',NULL,422,422,15536,11,1,'http://rdaregistry.info/Elements/c/C10004',NULL,'en',1),
+	(121281,'2014-01-19 11:29:58','2014-01-19 06:29:58',NULL,422,422,15536,12,1,'http://rdaregistry.info/Elements/c/C10001',NULL,'en',1),
 	(121282,'2014-01-19 11:29:58','2014-01-19 06:29:58',NULL,422,422,15536,13,1,'http://rdaregistry.info/Elements/a/P50001',NULL,'en',1),
-	(121283,'2014-01-19 11:29:58','2015-04-30 21:35:00','2015-04-30 21:35:00',422,422,15536,14,1,'1',NULL,'en',1),
-	(121284,'2014-01-19 11:29:58','2015-04-30 21:34:40','2015-04-30 21:34:40',422,422,15536,6,1,'http://rdaregistry.info/Elements/a/P50204',NULL,'en',1),
-	(121285,'2014-01-19 11:31:45','2015-04-30 21:40:30','2015-04-30 21:40:30',422,422,15536,6,NULL,'http://rdaregistry.info/Elements/u/P60001',14603,'en',1),
-	(121286,'2014-01-19 11:33:14','2015-04-30 21:34:55','2015-04-30 21:34:55',422,422,15536,15,NULL,'http://rdaregistry.info/Elements/w/P10001',NULL,'en',1),
-	(122794,'2014-04-26 06:27:36','2014-04-26 02:27:36',NULL,422,422,15536,16,NULL,'http://rdaregistry.info/Elements/a/respondentOf',NULL,'en',1),
-	(122935,'2015-04-30 22:00:17','2015-04-30 22:00:17',NULL,422,NULL,15536,27,NULL,'rdaa:respondentOf',NULL,'en',1),
-	(122936,'2015-04-30 22:00:18','2015-04-30 22:00:18',NULL,422,NULL,15536,7,NULL,'',NULL,'en',1),
-	(122937,'2015-04-30 22:00:18','2015-04-30 22:00:18',NULL,422,NULL,15536,11,NULL,'http://rdaregistry.info/Elements/c/C10004',NULL,'',1),
-	(122938,'2015-04-30 22:00:18','2015-04-30 22:00:18',NULL,422,NULL,15536,12,NULL,'http://rdaregistry.info/Elements/c/C10001',NULL,'',1),
-	(122939,'2015-04-30 22:00:18','2015-04-30 22:00:18',NULL,422,NULL,15536,6,NULL,'http://rdaregistry.info/Elements/a/P50204',NULL,'',1),
-	(122940,'2015-04-30 22:00:19','2015-04-30 22:00:19',NULL,422,NULL,15536,26,NULL,'http://rdaregistry.info/Elements/u/P60001',NULL,'',1),
-	(122941,'2015-04-30 22:00:19','2015-04-30 22:00:19',NULL,422,NULL,15536,15,NULL,'http://rdaregistry.info/Elements/w/P10001',NULL,'',1),
-	(122942,'2015-04-30 22:00:19','2015-04-30 22:00:19',NULL,422,NULL,15536,4,NULL,'property',NULL,'',1),
-	(122943,'2015-04-30 22:00:19','2015-04-30 22:00:19',NULL,422,NULL,15536,31,NULL,'',NULL,'',1),
-	(122944,'2015-04-30 22:00:20','2015-04-30 22:00:20',NULL,422,NULL,15536,14,NULL,'1',NULL,'',1);
+	(121283,'2014-01-19 11:29:58','2014-01-19 06:29:58',NULL,422,422,15536,14,1,'1',NULL,'en',1),
+	(121284,'2014-01-19 11:29:58','2014-01-19 06:29:58',NULL,422,422,15536,6,1,'http://rdaregistry.info/Elements/a/P50204',14069,'en',1),
+	(121285,'2014-01-19 11:31:45','2014-01-19 06:31:45',NULL,422,422,15536,6,NULL,'http://rdaregistry.info/Elements/u/P60001',14603,'en',1),
+	(121286,'2014-01-19 11:33:14','2014-01-19 06:33:14',NULL,422,422,15536,15,NULL,'http://rdaregistry.info/Elements/w/P10001',NULL,'en',1),
+	(122794,'2014-04-26 06:27:36','2014-04-26 02:27:36',NULL,422,422,15536,16,NULL,'http://rdaregistry.info/Elements/a/respondentOf',NULL,'en',1);
 ALTER TABLE `reg_schema_property_element` ENABLE KEYS;
 UNLOCK TABLES;
 
@@ -694,24 +684,7 @@ INSERT INTO `reg_schema_property_element_history` (`id`, `created_at`, `created_
 	(139300,'2014-01-19 06:29:58',422,'added',121284,15536,81,6,'http://rdaregistry.info/Elements/a/P50204',NULL,'en',1,NULL,NULL),
 	(139301,'2014-01-19 06:31:45',422,'added',121285,15536,81,6,'http://rdaregistry.info/Elements/u/P60001',NULL,'en',1,NULL,NULL),
 	(139302,'2014-01-19 06:33:14',422,'added',121286,15536,81,15,'http://rdaregistry.info/Elements/w/P10001',NULL,'en',1,NULL,NULL),
-	(141444,'2014-04-26 02:27:36',422,'added',122794,15536,81,16,'http://rdaregistry.info/Elements/a/respondentOf',NULL,'en',1,NULL,NULL),
-	(141876,'2015-04-30 21:34:38',422,'deleted',121280,15536,81,11,'http://rdaregistry.info/Elements/c/C10004',NULL,'en',1,NULL,NULL),
-	(141877,'2015-04-30 21:34:39',422,'deleted',121281,15536,81,12,'http://rdaregistry.info/Elements/c/C10001',NULL,'en',1,NULL,NULL),
-	(141878,'2015-04-30 21:34:40',422,'deleted',121284,15536,81,6,'http://rdaregistry.info/Elements/a/P50204',NULL,'en',1,NULL,NULL),
-	(141879,'2015-04-30 21:34:55',422,'deleted',121286,15536,81,15,'http://rdaregistry.info/Elements/w/P10001',NULL,'en',1,NULL,NULL),
-	(141880,'2015-04-30 21:34:57',422,'deleted',121279,15536,81,4,'subproperty',NULL,'en',1,NULL,NULL),
-	(141881,'2015-04-30 21:35:00',422,'deleted',121283,15536,81,14,'1',NULL,'en',1,NULL,NULL),
-	(141882,'2015-04-30 21:40:30',422,'deleted',121285,15536,81,6,'http://rdaregistry.info/Elements/u/P60001',14603,'en',1,NULL,NULL),
-	(141883,'2015-04-30 22:00:17',NULL,'added',122935,15536,81,27,'rdaa:respondentOf',NULL,'en',1,NULL,41),
-	(141884,'2015-04-30 22:00:18',NULL,'added',122936,15536,81,7,'',NULL,'en',1,NULL,41),
-	(141885,'2015-04-30 22:00:18',NULL,'added',122937,15536,81,11,'http://rdaregistry.info/Elements/c/C10004',NULL,'',1,NULL,41),
-	(141886,'2015-04-30 22:00:18',NULL,'added',122938,15536,81,12,'http://rdaregistry.info/Elements/c/C10001',NULL,'',1,NULL,41),
-	(141887,'2015-04-30 22:00:18',NULL,'added',122939,15536,81,6,'http://rdaregistry.info/Elements/a/P50204',NULL,'',1,NULL,41),
-	(141888,'2015-04-30 22:00:19',NULL,'added',122940,15536,81,26,'http://rdaregistry.info/Elements/u/P60001',NULL,'',1,NULL,41),
-	(141889,'2015-04-30 22:00:19',NULL,'added',122941,15536,81,15,'http://rdaregistry.info/Elements/w/P10001',NULL,'',1,NULL,41),
-	(141890,'2015-04-30 22:00:19',NULL,'added',122942,15536,81,4,'property',NULL,'',1,NULL,41),
-	(141891,'2015-04-30 22:00:19',NULL,'added',122943,15536,81,31,'',NULL,'',1,NULL,41),
-	(141892,'2015-04-30 22:00:20',NULL,'added',122944,15536,81,14,'1',NULL,'',1,NULL,41);
+	(141444,'2014-04-26 02:27:36',422,'added',122794,15536,81,16,'http://rdaregistry.info/Elements/a/respondentOf',NULL,'en',1,NULL,NULL);
 ALTER TABLE `reg_schema_property_element_history` ENABLE KEYS;
 UNLOCK TABLES;
 
