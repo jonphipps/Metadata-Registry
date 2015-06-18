9ceae85dd1fb682c8c57724ab68349a3efa21fd8
Jon Phipps <jphipps@madcreek.com>
2015-06-18 08:11:44 -0400

    various table fixes

--- swregistry_update_step_2_run_this.sql
+++ swregistry_update_step_2_run_this.sql
@@ -1,10 +1,44 @@
 
 --
+-- FOREIGN KEYS [DROP]
+--
+
+ALTER TABLE reg_concept DROP FOREIGN KEY reg_concept_FK_1;
+
+--
+-- reg_batch
+--
+
+
+--
+-- reg_concept
+--
+
+
+--
 -- reg_concept_property
 --
 
 ALTER TABLE reg_concept_property
 ADD is_concept_property TINYINT(1) DEFAULT 0 NOT NULL;
+
+--
+-- reg_concept_property_history
+--
+
+
+--
+-- reg_file_import_history
+--
+
+ALTER TABLE reg_file_import_history CHANGE COLUMN map map TEXT NULL COMMENT 'stores the serialized column map array';
+ALTER TABLE reg_file_import_history CHANGE COLUMN file_type file_type VARCHAR(30) NULL COMMENT '';
+ALTER TABLE reg_file_import_history CHANGE COLUMN results results TEXT NULL COMMENT 'stores the serialized results of the import';
+
+--
+-- reg_lookup
+--
+
 
 --
 -- reg_schema
@@ -21,34 +55,38 @@ ADD repo VARCHAR(255) NOT NULL;
 -- reg_schema_property
 --
 
+ALTER TABLE reg_schema_property CHANGE COLUMN type type ENUM('property','class') DEFAULT 'property' NOT NULL COMMENT '';
 ALTER TABLE reg_schema_property
-ADD url VARCHAR(255),
+ADD url VARCHAR(255);
+ALTER TABLE reg_schema_property
 ADD lexical_alias VARCHAR(255);
 
 --
 -- reg_schema_property_element
 --
 
-ALTER TABLE `reg_schema_property_element`
-CHANGE COLUMN `language` `language`
-CHAR(6)
-CHARACTER SET utf8
-COLLATE utf8_general_ci
-  NOT NULL
-COMMENT ''
-AFTER `related_schema_property_id`;
+ALTER TABLE reg_schema_property_element CHANGE COLUMN language language CHAR(6) NOT NULL COMMENT '';
+DROP INDEX reg_schema_property_element_idx1 ON reg_schema_property_element;
+CREATE INDEX reg_schema_property_element_idx1 ON reg_schema_property_element (object(150) ASC);
 
 --
 -- reg_schema_property_element_history
 --
 
+ALTER TABLE reg_schema_property_element_history CHANGE COLUMN language language CHAR(6) NOT NULL COMMENT '';
 ALTER TABLE reg_schema_property_element_history
 ADD import_id INT(11);
 CREATE INDEX reg_schema_property_element_history_fk7 ON reg_schema_property_element_history (import_id ASC);
-ALTER TABLE `reg_schema_property_element_history`
-CHANGE COLUMN `language` `language`
-CHAR(6) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
-COMMENT '' AFTER `related_schema_property_id`;
+
+--
+-- reg_skos_property
+--
+
+
+--
+-- reg_status
+--
+
 
 --
 -- reg_user
@@ -56,6 +94,7 @@ COMMENT '' AFTER `related_schema_property_id`;
 
 ALTER TABLE reg_user
 ADD culture VARCHAR(7) DEFAULT 'en_US';
+DROP INDEX id ON reg_user;
 
 --
 -- reg_vocabulary
@@ -81,6 +120,10 @@ ALTER TABLE reg_vocabulary_has_user
 ADD current_language CHAR(6) DEFAULT 'en';
 
 --
+-- reg_vocabulary_has_version
+--
+
+--
 -- schema_has_user
 --
 
@@ -92,32 +135,36 @@ ALTER TABLE schema_has_user
 ADD current_language CHAR(6);
 
 --
+-- schema_has_version
+--
+
+--
 -- update property/class
 --
 
 update reg_schema_property
-set type='Property'
+set type='property'
 where type='subproperty' OR type='property';
 update reg_schema_property
-set type='Class'
+set type='class'
 where type='subclass' OR type='class';
 
 update reg_schema_property_element
-set object='Property'
+set object='property'
 WHERE profile_property_id=4
       AND (object='subproperty' OR object='property');
 update reg_schema_property_element
-set object='Class'
+set object='class'
 WHERE profile_property_id=4
       and (object='subclass' OR object='class');
 
 ALTER TABLE `reg_schema_property`
 CHANGE COLUMN `type` `type`
-ENUM('Property','Class')
+ENUM('property','class')
 CHARACTER SET utf8
 COLLATE utf8_general_ci
   NOT NULL
-  DEFAULT 'Property'
+  DEFAULT 'property'
 COMMENT ''
 AFTER `comment`;
 
@@ -125,12 +172,17 @@ AFTER `comment`;
 -- update related
 --
 
+-- fix no uri
+UPDATE reg_schema_property_element, reg_schema_property
+set reg_schema_property_element.object = reg_schema_property.uri
+where reg_schema_property_element.related_schema_property_id = reg_schema_property.id
+AND reg_schema_property_element.object = '';
+
+-- fix empty or broken related id
 update reg_schema_property_element, reg_schema_property
 set reg_schema_property_element.related_schema_property_id = reg_schema_property.id
 WHERE reg_schema_property_element.object = reg_schema_property.uri
-      and (
-        reg_schema_property_element.related_schema_property_id <> reg_schema_property.id
-        or reg_schema_property_element.related_schema_property_id is NULL);
+  and reg_schema_property_element.related_schema_property_id <> reg_schema_property.id;
 
 --
 -- remove language from elements that don't have language
