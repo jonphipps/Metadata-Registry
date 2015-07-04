3ccbe1c1d85f045706b0b9e86b86b0f241d003f2
Jon Phipps <jphipps@madcreek.com>
2015-07-03 20:04:04 -0400

    crash when user is admin and does not have a default language set (very rare, but it just bit me)

--- apps/frontend/modules/schemapropel/actions/actions.class.php
+++ apps/frontend/modules/schemapropel/actions/actions.class.php
@@ -37,7 +37,7 @@ class schemapropelActions extends autoschemapropelActions
 
       if ("edit" == $this->getActionName() && $userId) {
         $schemaUser    = $schemaObj->GetUserForSchema($userId);
-        $UserLanguages = (isset($schemaUser))? $schemaUser->getLanguages() : ['en'];
+        $UserLanguages = isset($schemaUser) ? $schemaUser->getLanguages() : ['en'];
         $this->getUser()->setAttribute("languages", $UserLanguages);
 
         if (! in_array($language, $UserLanguages)) {
