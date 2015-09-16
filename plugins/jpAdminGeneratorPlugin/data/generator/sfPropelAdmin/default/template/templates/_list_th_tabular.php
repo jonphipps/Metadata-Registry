33cb7ddad28d7fd9cd93cc7e06d75055b19d3fd4
Jon Phipps <jphipps@madcreek.com>
2015-09-16 12:19:17 -0400

    add filters to sort in lists

--- plugins/jpAdminGeneratorPlugin/data/generator/sfPropelAdmin/default/template/templates/_list_th_tabular.php
+++ plugins/jpAdminGeneratorPlugin/data/generator/sfPropelAdmin/default/template/templates/_list_th_tabular.php
@@ -1,4 +1,13 @@
 <?php $hides = $this->getParameterValue('list.hide', array()) ?>
+[?php $filterParam = ''; ?]
+<?php $urlFilters = $this->getParameterValue('list.urlfilters') ?>
+<?php if ($urlFilters): ?>
+  <?php foreach ($urlFilters as $field => $param): ?>
+    [?php if ($sf_params->has('filters[<?php echo $param ?>]')): ?]
+    [?php $filterParam .= '&filters[<?php echo $param ?>]=' . $sf_params->get('filters[<?php echo $param ?>]'); ?]
+    [?php endif; ?]
+  <?php endforeach; ?>
+<?php endif; ?>
 <?php foreach ($this->getColumns('list.display') as $column): ?>
 <?php if (in_array($column->getName(), $hides)) continue ?>
 <?php $condition = $this->getParameterValue('list.fields.'.$column->getName().'.condition') ?>
@@ -12,14 +21,14 @@
   <th id="sf_admin_list_th_<?php echo $column->getName() ?>">
     <?php if ($column->isReal()): ?>
       [?php if ($sf_user->getAttribute('sort', null, 'sf_admin/<?php echo $this->getSingularName() ?>/sort') == '<?php echo $column->getName() ?>'): ?]
-      [?php echo link_to(__('<?php echo str_replace("'", "\\'", $this->getParameterValue('list.fields.'.$column->getName().'.name')) ?>'), '<?php echo $this->getModuleName() ?>/list?sort=<?php echo $column->getName() ?>&type='.($sf_user->getAttribute('type', 'asc', 'sf_admin/<?php echo $this->getSingularName() ?>/sort') == 'asc' ? 'desc' : 'asc')) ?]
+      [?php echo link_to(__('<?php echo str_replace("'", "\\'", $this->getParameterValue('list.fields.'.$column->getName().'.name')) ?>'), '<?php echo $this->getModuleName() ?>/list?sort=<?php echo $column->getName() ?>&type='.($sf_user->getAttribute('type', 'asc', 'sf_admin/<?php echo $this->getSingularName() ?>/sort') == 'asc' ? 'desc' : 'asc') . $filterParam) ?]
       [?php if ($sf_user->getAttribute('type', null, 'sf_admin/<?php echo $this->getSingularName() ?>/sort') == 'asc'): ?]
       [?php echo image_tag(sfConfig::get('sf_admin_web_dir').'/images/s_asc.png', array('align' => 'absmiddle', 'alt' => __('Ascending Order'), 'title' => __('List has been sorted in ascending order'))) ?]
       [?php elseif ($sf_user->getAttribute('type', null, 'sf_admin/<?php echo $this->getSingularName() ?>/sort') == 'desc'): ?]
       [?php echo image_tag(sfConfig::get('sf_admin_web_dir').'/images/s_desc.png', array('align' => 'absmiddle', 'alt' => __('Descending Order'), 'title' => __('List has been sorted in descending order'))) ?]
       [?php endif; ?]
       [?php else: ?]
-      [?php echo link_to(__('<?php echo str_replace("'", "\\'", $this->getParameterValue('list.fields.'.$column->getName().'.name')) ?>'), '<?php echo $this->getModuleName() ?>/list?sort=<?php echo $column->getName() ?>&type=asc') ?]
+      [?php echo link_to(__('<?php echo str_replace("'", "\\'", $this->getParameterValue('list.fields.'.$column->getName().'.name')) ?>'), '<?php echo $this->getModuleName() ?>/list?sort=<?php echo $column->getName() ?>&type=asc' . $filterParam) ?]
       [?php endif; ?]
     <?php else: ?>
     [?php echo __('<?php echo str_replace("'", "\\'", $this->getParameterValue('list.fields.'.$column->getName().'.name')) ?>') ?]
