48366ea2d6878c1e6bdffaf201245cb7c0232fa0
Jon Phipps <jphipps@madcreek.com>
2016-06-07 12:17:37 -0400

    update tests core codecept 2.2

--- tests/functional/_bootstrap.php
+++ tests/functional/_bootstrap.php
@@ -1,2 +1,19 @@
 <?php
-// Here you can initialize variables that will be available to your tests
+// This is global bootstrap for autoloading
+
+use Codeception\Configuration;
+
+define('SF_ROOT_DIR',    realpath(dirname(__file__).'/../../'));
+define('SF_APP',         'frontend');
+define('SF_ENVIRONMENT', 'dev');
+define('SF_DEBUG',       isset($debug) ? $debug : true);
+
+//initialize composer
+require_once(SF_ROOT_DIR.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php');
+
+// initialize symfony
+require_once(SF_ROOT_DIR.DIRECTORY_SEPARATOR.'apps'.DIRECTORY_SEPARATOR.SF_APP.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php');
+// initialize database manager
+$databaseManager = new \sfDatabaseManager();
+$databaseManager->initialize();
+
