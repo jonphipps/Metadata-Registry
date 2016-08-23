8ce90d669d078ca6fc8ffc3224fa5dfe67609275
Jon Phipps <jphipps@madcreek.com>
2016-08-23 08:32:25 -0400

    change import detail link to use named route

--- apps/frontend/modules/history/templates/_import.php
+++ apps/frontend/modules/history/templates/_import.php
@@ -3,7 +3,7 @@
 /** @var \ConceptPropertyHistory $concept_property_history */
 $property = $concept_property_history->getImportId();
 if ($property) {
-    echo link_to($property, '/import/show?id=' . $concept_property_history->getImportId());
+    echo link_to($property, '@import_detail?id=' . $concept_property_history->getImportId());
 } else {
     echo "&nbsp;";
 }
