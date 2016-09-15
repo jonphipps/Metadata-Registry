48162143005d9ed30be0da1046c9f50ff3a8e73d
Jon Phipps <jphipps@madcreek.com>
2016-09-14 22:53:58 -0400

    update google analytics script

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -67,23 +67,37 @@
                            'https://www.digitalocean.com/'); ?></div>
 </div>
 <?php
+$trackingId = '';
+
 //Google analytics
 if ($_SERVER['SERVER_NAME'] == 'sandbox.metadataregistry.org') {
-    echo '<script src="http://www.google-analytics.com/urchin.js" type="text/javascript"></script>';
-    echo javascript_tag('
-      _uacct = "UA-840150-2";
-      urchinTracker();
-    ');
+    $trackingId = 'UA-840150-2';
 }
 
 if ($_SERVER['SERVER_NAME'] == 'metadataregistry.org') {
-    echo '<script src="http://www.google-analytics.com/urchin.js" type="text/javascript"></script>';
-    echo javascript_tag('
-      _uacct = "UA-840150-1";
-      urchinTracker();
-    ');
+    $trackingId = 'UA-840150-1';
 }
-?>
+if($trackingId): ?>
+    <!-- Google Analytics -->
+    <script>
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function () {
+                  (i[r].q = i[r].q || []).push(arguments)
+              }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+              m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', '<?php echo $trackingId ?>', 'auto');
+        ga('send', 'pageview');
+    </script>
+    <!-- End Google Analytics -->
+<?php endif; ?>
+
 <?php //<script type="text/javascript" src="http://cetrk.com/pages/scripts/0005/6031.js"> </script> ?>
 </body>
 </html>
