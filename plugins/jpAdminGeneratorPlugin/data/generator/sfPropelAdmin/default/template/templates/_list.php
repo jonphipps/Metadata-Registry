b215fd141e39b9490a73d13c79117386b1a88e87
Jon Phipps <jphipps@madcreek.com>
2016-09-23 22:20:13 -0400

    update list geerator to fix pager links

--- plugins/jpAdminGeneratorPlugin/data/generator/sfPropelAdmin/default/template/templates/_list.php
+++ plugins/jpAdminGeneratorPlugin/data/generator/sfPropelAdmin/default/template/templates/_list.php
@@ -9,26 +9,29 @@
 </thead>
 <tfoot>
 <tr><th colspan="<?php echo $this->getParameterValue('list.object_actions') ? count($this->getColumns('list.display')) + 1 : count($this->getColumns('list.display')) ?>">
-<div class="float-right">
-[?php if ($pager->haveToPaginate()): ?]
-  [?php $filterParam = ''; ?]
-<?php $urlFilters = $this->getParameterValue('list.urlfilters') ?>
-<?php if ($urlFilters): ?>
-<?php foreach ($urlFilters as $field => $param): ?>
-[?php if ($sf_params->has('<?php echo $param ?>')): ?]
-    [?php $filterParam .= '&<?php echo $param ?>=' . $sf_params->get('<?php echo $param ?>'); ?]
-[?php endif; ?]
-<?php endforeach; ?>
+<div class="float-right pager">
+[?php /** @var sfPropelPager $pager */
+  if ($pager->haveToPaginate()):
+  $filterParam = ''; ?]
+[?php /** @var sfParameterHolder $sf_params */ ?]
+<?php $parents = $this->getParameterValue('parents');
+if ($parents): ?>
+  <?php foreach ($parents as $module => $param):
+    ?>
+  [?php if ($sf_params->has('<?php echo $param['requestid'] ?>')): ?]
+    [?php $filterParam .= '<?php echo $param['requestid'] ?>=' . $sf_params->get('<?php echo $param['requestid'] ?>'); $parent = '<?php echo $module ?>_';  ?]
+  [?php endif; ?]
+  <?php endforeach; ?>
 <?php endif; ?>
-  [?php echo link_to(image_tag(sfConfig::get('sf_admin_web_dir').'/images/first.png', array('align' => 'absmiddle', 'alt' => __('First'), 'title' => __('First'))), '<?php echo $this->getModuleName() ?>/list?page=1' . $filterParam) ?]
-  [?php echo link_to(image_tag(sfConfig::get('sf_admin_web_dir').'/images/previous.png', array('align' => 'absmiddle', 'alt' => __('Previous'), 'title' => __('Previous'))), '<?php echo $this->getModuleName() ?>/list?page='.$pager->getPreviousPage() . $filterParam) ?]
+  [?php echo link_to(image_tag(sfConfig::get('sf_admin_web_dir').'/images/first.png', array('alt' => __('First'), 'title' => __('First'))), '@' . $parent . '<?php echo $this->getModuleName() ?>_list?' .$filterParam, ['query_string' => '&page=1']) ?]
+  [?php echo link_to(image_tag(sfConfig::get('sf_admin_web_dir').'/images/previous.png', array('alt' => __('Previous'), 'title' => __('Previous'))), '@' . $parent . '<?php echo $this->getModuleName() ?>_list?' .$filterParam, ['query_string' => '&page='.$pager->getPreviousPage()]) ?]
 
   [?php foreach ($pager->getLinks() as $page): ?]
-    [?php echo link_to_unless($page == $pager->getPage(), $page, '<?php echo $this->getModuleName() ?>/list?page='.$page . $filterParam) ?]
+    [?php echo link_to_unless($page == $pager->getPage(), $page, '@' . $parent . '<?php echo $this->getModuleName() ?>_list?' . $filterParam, ['query_string' => '&page=' . $page]) ?]
   [?php endforeach; ?]
 
-  [?php echo link_to(image_tag(sfConfig::get('sf_admin_web_dir').'/images/next.png', array('align' => 'absmiddle', 'alt' => __('Next'), 'title' => __('Next'))), '<?php echo $this->getModuleName() ?>/list?page='.$pager->getNextPage() . $filterParam) ?]
-  [?php echo link_to(image_tag(sfConfig::get('sf_admin_web_dir').'/images/last.png', array('align' => 'absmiddle', 'alt' => __('Last'), 'title' => __('Last'))), '<?php echo $this->getModuleName() ?>/list?page='.$pager->getLastPage() . $filterParam) ?]
+  [?php echo link_to(image_tag(sfConfig::get('sf_admin_web_dir').'/images/next.png', array('alt' => __('Next'), 'title' => __('Next'))), '@' . $parent . '<?php echo $this->getModuleName() ?>_list?' .$filterParam, ['query_string' => '&page='.$pager->getNextPage()]) ?]
+  [?php echo link_to(image_tag(sfConfig::get('sf_admin_web_dir').'/images/last.png', array('alt' => __('Last'), 'title' => __('Last'))), '@' . $parent . '<?php echo $this->getModuleName() ?>_list?' .$filterParam, ['query_string' => '&page='.$pager->getLastPage()]) ?]
 [?php endif; ?]
 </div>
 [?php echo format_number_choice('[0] no result|[1] 1 result|(1,+Inf] %1% results', array('%1%' => $pager->getNbResults()), $pager->getNbResults()) ?]
