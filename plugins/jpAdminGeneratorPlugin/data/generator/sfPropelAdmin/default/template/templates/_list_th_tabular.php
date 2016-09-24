d5b10dc53e8c25e0a11abc3e543cae0ef76f92d2
Jon Phipps <jphipps@madcreek.com>
2016-09-23 23:18:04 -0400

    clean up problems with sort headers

--- plugins/jpAdminGeneratorPlugin/data/generator/sfPropelAdmin/default/template/templates/_list_th_tabular.php
+++ plugins/jpAdminGeneratorPlugin/data/generator/sfPropelAdmin/default/template/templates/_list_th_tabular.php
@@ -1,14 +1,15 @@
 <?php $hides = $this->getParameterValue('list.hide', array()) ?>
 [?php $filterParam = ''; ?]
-<?php $urlFilters = $this->getParameterValue('list.urlfilters') ?>
-<?php if ($urlFilters): ?>
-  <?php foreach ($urlFilters as $field => $param): ?>
-    [?php if ($sf_params->has('<?php echo $param ?>')): ?]
-    [?php $filterParam .= '&<?php echo $param ?>=' . $sf_params->get('<?php echo $param ?>'); ?]
-    [?php endif; ?]
+<?php $parents = $this->getParameterValue('parents');
+if ($parents): ?>
+  <?php foreach ($parents as $module => $param): ?>
+[?php if ($sf_params->has('<?php echo $param['requestid'] ?>')): ?]
+  [?php $filterParam .= '?<?php echo $param['requestid'] ?>=' . $sf_params->get('<?php echo $param['requestid'] ?>'); $parent = '<?php echo $module ?>_'; ?]
+[?php endif; ?]
   <?php endforeach; ?>
 <?php endif; ?>
-<?php foreach ($this->getColumns('list.display') as $column): ?>
+<?php /** @var sfAdminColumn $column */
+foreach ($this->getColumns('list.display') as $column): ?>
 <?php if (in_array($column->getName(), $hides)) continue ?>
 <?php $condition = $this->getParameterValue('list.fields.'.$column->getName().'.condition') ?>
 <?php if ($condition): ?>
@@ -20,15 +21,16 @@
 <?php endif; ?>
   <th id="sf_admin_list_th_<?php echo $column->getName() ?>">
     <?php if ($column->isReal()): ?>
-      [?php if ($sf_user->getAttribute('sort', null, 'sf_admin/<?php echo $this->getSingularName() ?>/sort') == '<?php echo $column->getName() ?>'): ?]
-      [?php echo link_to(__('<?php echo str_replace("'", "\\'", $this->getParameterValue('list.fields.'.$column->getName().'.name')) ?>'), '<?php echo $this->getModuleName() ?>/list?sort=<?php echo $column->getName() ?>&type='.($sf_user->getAttribute('type', 'asc', 'sf_admin/<?php echo $this->getSingularName() ?>/sort') == 'asc' ? 'desc' : 'asc') . $filterParam) ?]
+      [?php /** @var sfUser $sf_user */
+      if ($sf_user->getAttribute('sort', null, 'sf_admin/<?php echo $this->getSingularName() ?>/sort') == '<?php echo $column->getName() ?>'): ?]
+      [?php echo link_to(__('<?php echo str_replace("'", "\\'", $this->getParameterValue('list.fields.'.$column->getName().'.name')) ?>'), '@' . $parent . '<?php echo $this->getModuleName() ?>_list' . $filterParam, ['query_string' => 'sort=<?php echo $column->getName() ?>&type='.($sf_user->getAttribute('type', 'asc', 'sf_admin/<?php echo $this->getSingularName() ?>/sort') == 'asc' ? 'desc' : 'asc')]) ?]
       [?php if ($sf_user->getAttribute('type', null, 'sf_admin/<?php echo $this->getSingularName() ?>/sort') == 'asc'): ?]
       [?php echo image_tag(sfConfig::get('sf_admin_web_dir').'/images/s_asc.png', array('align' => 'absmiddle', 'alt' => __('Ascending Order'), 'title' => __('List has been sorted in ascending order'))) ?]
       [?php elseif ($sf_user->getAttribute('type', null, 'sf_admin/<?php echo $this->getSingularName() ?>/sort') == 'desc'): ?]
       [?php echo image_tag(sfConfig::get('sf_admin_web_dir').'/images/s_desc.png', array('align' => 'absmiddle', 'alt' => __('Descending Order'), 'title' => __('List has been sorted in descending order'))) ?]
       [?php endif; ?]
       [?php else: ?]
-      [?php echo link_to(__('<?php echo str_replace("'", "\\'", $this->getParameterValue('list.fields.'.$column->getName().'.name')) ?>'), '<?php echo $this->getModuleName() ?>/list?sort=<?php echo $column->getName() ?>&type=asc' . $filterParam) ?]
+      [?php echo link_to(__('<?php echo str_replace("'", "\\'", $this->getParameterValue('list.fields.'.$column->getName().'.name')) ?>'), '@' . $parent . '<?php echo $this->getModuleName() ?>_list' . $filterParam, ['query_string' => 'sort=<?php echo $column->getName() ?>&type=asc']) ?]
       [?php endif; ?]
     <?php else: ?>
     [?php echo __('<?php echo str_replace("'", "\\'", $this->getParameterValue('list.fields.'.$column->getName().'.name')) ?>') ?]
