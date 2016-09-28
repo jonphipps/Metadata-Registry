776a38d6b4460b6f8833b629e7369b3c5e7f599c
Jon Phipps <jphipps@madcreek.com>
2016-09-27 22:46:21 -0400

    make sure that Laravel doesn't try to process the symfony config file

--- config/config.php
+++ config/config.php
@@ -1,5 +1,11 @@
 <?php
+// This directory contains a mix of symfony and laravel config files.
+// This is one of only two non-laravel files.
+// So if no symfony bootstrap settings, then we skip it.
+if ( ! defined('SF_ROOT_DIR')) {
+  return;
+}
 
 // symfony directories
-$sf_symfony_lib_dir  = dirname(__FILE__).'/../lib/symfony';
-$sf_symfony_data_dir = dirname(__FILE__).'/../data/symfony';
+$sf_symfony_lib_dir  = dirname(__FILE__) . '/../lib/symfony';
+$sf_symfony_data_dir = dirname(__FILE__) . '/../data/symfony';
