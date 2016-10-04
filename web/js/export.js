2e7e248ef959a1937cf8d163e96fc661ee6927cc
Jon Phipps <jphipps@madcreek.com>
2016-10-03 22:45:35 -0400

    update export.js for jqwidgets

--- web/js/export.js
+++ web/js/export.js
@@ -1,11 +1,15 @@
-var $jq = jQuery.noConflict();
-$jq(document).ready(function () {
+//var $jq = jQuery.noConflict();
+$(document).ready(function () {
 
-  var $selLang = $jq('select#addLanguage');
+  var $selLang = $('select#export_history_selected_language');
 
   $selLang.select2({
     placeholder: "Select an Additional Language (optional)",
     allowClear: true
   });
+
+  $("#export_history_selected_columns").jqxListBox({checkboxes: true,
+    allowDrop: true,
+    allowDrag: true, width: 300, height: 300});
 
 });
