5b0a50a5c7b4daee058e90aad389c08aac452336
Jon Phipps <jphipps@madcreek.com>
2016-10-08 01:15:19 -0400

    bootstrap laravel

--- web/frontend_dev.php
+++ web/frontend_dev.php
@@ -5,6 +5,10 @@ define('SF_APP',         'frontend');
 define('SF_ENVIRONMENT', 'dev');
 define('SF_DEBUG',       true);
 
+//initialize composer through laravel bootstrap
+require __DIR__ . '/../bootstrap/autoload.php';
+
+// initialize symfony
 require_once(SF_ROOT_DIR.DIRECTORY_SEPARATOR.'apps'.DIRECTORY_SEPARATOR.SF_APP.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php');
 
 sfContext::getInstance()->getController()->dispatch();
