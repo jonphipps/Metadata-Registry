96fdc298bc43516983f96b8eb6070cc8d8300b02
Jon Phipps <jphipps@madcreek.com>
2016-10-08 01:16:45 -0400

    start of export class

---
+++ app/Jobs/Export.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Container\Container;
+
+class Export implements ShouldQueue
+{
+    use InteractsWithQueue, Queueable, SerializesModels;
+
+  /**
+   * Create a new job instance.
+   *
+   * @param \ExportHistory $export
+   *
+   * @throws \PropelException
+   */
+    public function __construct(\ExportHistory $export)
+    {
+      //new up a laravel container
+      Container::setInstance(new Container);
+
+    $addLanguage     = $export->getSelectedLanguage();
+      $schema          = $export->getSchema();
+      $vocabulary      = $export->getVocabulary();
+      $defaultLanguage = $schema ? $schema->getLanguage() : $vocabulary->getLanguage();
+
+      if ($addLanguage) {
+        $languages = [ $defaultLanguage, $addLanguage, ];
+      } else {
+        $languages = [ $defaultLanguage, ];
+      }
+
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //
+    }
+}
