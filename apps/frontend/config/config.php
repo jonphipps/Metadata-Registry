f7c14ddbe3f2f8158cc8d60b732ecd0bbbdd14a3
Jon Phipps <jphipps@madcreek.com>
2016-10-21 10:47:38 -0400

    bugsnag release set from env (or not)

--- apps/frontend/config/config.php
+++ apps/frontend/config/config.php
@@ -46,31 +46,32 @@ unset( $capsule, $container );
 
 $dotenv = new Dotenv\Dotenv(SF_ROOT_DIR);
 $dotenv->load();
+$bugsnagStage = env('BUGSNAG_RELEASE_STAGE', '');
 /** @var Bugsnag $bugsnag */
 $bugsnag = Bugsnag\Client::make(env('BUGSNAG_API_KEY'));
-$bugsnag->setReleaseStage(env('BUGSNAG_RELEASE_STAGE', 'production'));
+$bugsnag->setReleaseStage($bugsnagStage);
 $bugsnag->setErrorReportingLevel(E_ALL & ~E_NOTICE);
 
 if (array_key_exists('SERVER_NAME', $_SERVER)) {
   Bugsnag\Handler::register($bugsnag);
   switch ($_SERVER['SERVER_NAME']) {
     case 'registry.dev':
-    case 'beta.metadataregistry.net':
-      $bugsnag->setReleaseStage('development');
+      if ( ! $bugsnagStage) $bugsnag->setReleaseStage('development');
       $icon = 'registry_favicon_dev.ico';
       break;
+    case 'beta.metadataregistry.net':
     case 'beta.metadataregistry.org':
     case 'beta-sand.metadataregistry.org':
     case 'beta-prod.metadataregistry.org':
-      $bugsnag->setReleaseStage('beta');
+      if ( ! $bugsnagStage) $bugsnag->setReleaseStage('beta');
       $icon = 'registry_favicon_beta.ico';
       break;
     case 'sandbox.metadataregistry.org':
-      $bugsnag->setReleaseStage('sandbox');
+      if ( ! $bugsnagStage) $bugsnag->setReleaseStage('sandbox');
       $icon = 'registry_favicon_sand.ico';
       break;
     default:
-      $bugsnag->setReleaseStage('production');
+      if ( ! $bugsnagStage) $bugsnag->setReleaseStage('production');
       $icon = 'registry_favicon_prod.ico';
   }
   putenv("FAVICON=$icon");
