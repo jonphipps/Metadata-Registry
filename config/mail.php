92fb28b2c06c80be23245cef30ac5f4f5c5f70e7
Jon Phipps <jphipps@madcreek.com>
2016-10-28 09:45:47 -0400

    mail config for laravel

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
+        'address' => 'sysadmin@metadataregistry.org',
+        'name' => 'Jon Phipps',
     ],
 
     /*
