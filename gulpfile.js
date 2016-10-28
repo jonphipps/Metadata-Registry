e04d3ad73785141d9f9613d3b8fcd04cf41c80e2
Jon Phipps <jphipps@madcreek.com>
2016-10-28 10:11:04 -0400

    change path for asset compilation to web

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,10 @@ const elixir = require('laravel-elixir');
 require('laravel-elixir-codeception');
 require('laravel-elixir-vue-2');
 
-elixir.config.publicDir = 'web';
+elixir.config.publicPath = 'web';
 
 elixir((mix) => {
     mix.sass('app.scss')
        .webpack('app.js');
 });
 
-//
-// elixir(function(mix) {
-//     mix.sass('app.scss');
-// });
