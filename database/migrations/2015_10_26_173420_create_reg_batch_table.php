585a50561bc1d522912a1b2e4224a967efed9e6a
Jon Phipps <jphipps@madcreek.com>
2016-10-31 10:31:27 -0400

    text fields don't need length

--- database/migrations/2015_10_26_173420_create_reg_batch_table.php
+++ database/migrations/2015_10_26_173420_create_reg_batch_table.php
@@ -18,12 +18,12 @@ class CreateRegBatchTable extends Migration
             function (Blueprint $table) {
                 $table->integer('id', true);
                 $table->dateTime('run_time')->nullable();
-                $table->text('run_description', 65535)->nullable();
+                $table->text('run_description')->nullable();
                 $table->string('object_type', 20)->nullable();
                 $table->integer('object_id')->nullable();
                 $table->dateTime('event_time')->nullable();
                 $table->string('event_type', 20)->nullable();
-                $table->text('event_description', 65535)->nullable();
+                $table->text('event_description')->nullable();
                 $table->string('registry_uri')->nullable();
             });
     }
