0b06b19ba510ac50e39ed399ff79ca1f019777b3
Jon Phipps <jphipps@madcreek.com>
2016-11-01 18:14:37 -0400

    profile has nickname

--- resources/views/frontend/user/account/tabs/edit.blade.php
+++ resources/views/frontend/user/account/tabs/edit.blade.php
@@ -1,9 +1,9 @@
 {{ Form::model($logged_in_user, ['route' => 'frontend.user.profile.update', 'class' => 'form-horizontal', 'method' => 'PATCH']) }}
 
     <div class="form-group">
-        {{ Form::label('name', trans('validation.attributes.frontend.name'), ['class' => 'col-md-4 control-label']) }}
+        {{ Form::label('nickname', trans('validation.attributes.frontend.name'), ['class' => 'col-md-4 control-label']) }}
         <div class="col-md-6">
-            {{ Form::input('text', 'name', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.name')]) }}
+            {{ Form::input('text', 'nickname', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.name')]) }}
         </div>
     </div>
 
