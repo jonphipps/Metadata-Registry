1bbcd65cd7b01faf1ea2bb1ca07a99e5e9900c32
Jon Phipps <jphipps@madcreek.com>
2016-11-01 14:45:58 -0400

    define the sf_environment and sf_debug from .env This means we don't have to use a bunch of alternate starter scripts

--- web/frontend.php
+++ web/frontend.php
@@ -1,26 +1,31 @@
 <?php
 
 define('SF_ROOT_DIR', realpath(dirname(__FILE__) . '/..'));
-define('SF_APP', 'frontend');
-define('SF_ENVIRONMENT', 'prod');
-define('SF_DEBUG', false);
 
 //initialize composer through laravel bootstrap
 require_once SF_ROOT_DIR . DIRECTORY_SEPARATOR . 'bootstrap/autoload.php';
 
 //fire up Laravel
 $app = require_once SF_ROOT_DIR . DIRECTORY_SEPARATOR . 'bootstrap/app.php';
+
+//we may use this in future to prescreen URLs to bypass laravel processing
 $uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+
 /** @var \Illuminate\Contracts\Http\Kernel $kernel */
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
+//let laravel handle the response
 $response = $kernel->handle($request = Illuminate\Http\Request::capture());
 if ($response->getContent() !== "symfony") {
   $response->send();
   $kernel->terminate($request, $response);
 } else {
+  //it's not a route that laravel recognizes
+  //so we fire up symfony
+  define('SF_APP', 'frontend');
+  define('SF_ENVIRONMENT', env('SF_ENVIRONMENT', 'dev'));
+  define('SF_DEBUG', env('SF_DEBUG', 'true'));
 
   require_once SF_ROOT_DIR . DIRECTORY_SEPARATOR . 'apps' . DIRECTORY_SEPARATOR . SF_APP . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'config.php';
-
   sfContext::getInstance()->getController()->dispatch();
 }
--- web/index.php
+++ web/index.php
@@ -1,19 +1,19 @@
 <?php
 
 define('SF_ROOT_DIR', realpath(dirname(__FILE__) . '/..'));
-define('SF_APP', 'frontend');
-define('SF_ENVIRONMENT', 'dev');
-define('SF_DEBUG', true);
 
 //initialize composer through laravel bootstrap
 require_once SF_ROOT_DIR . DIRECTORY_SEPARATOR . 'bootstrap/autoload.php';
 
 //fire up Laravel
 $app = require_once SF_ROOT_DIR . DIRECTORY_SEPARATOR . 'bootstrap/app.php';
+
+//we may use this in future to prescreen URLs to bypass laravel processing
 $uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
   /** @var \Illuminate\Contracts\Http\Kernel $kernel */
   $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
+//let laravel handle the response
   $response = $kernel->handle($request = Illuminate\Http\Request::capture());
 if ($response->getContent() !== "symfony") {
   $response->send();
@@ -21,6 +21,11 @@ if ($response->getContent() !== "symfony") {
   $kernel->terminate($request, $response);
 
 } else {
+  //it's not a route that laravel recognizes
+  //so we fire up symfony
+  define('SF_APP', 'frontend');
+  define('SF_ENVIRONMENT', env('SF_ENVIRONMENT', 'prod'));
+  define('SF_DEBUG', env('SF_DEBUG', 'false'));
 
   require_once SF_ROOT_DIR . DIRECTORY_SEPARATOR . 'apps' . DIRECTORY_SEPARATOR . SF_APP . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'config.php';
 
