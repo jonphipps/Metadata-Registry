efbe0a3599890625b19f7fc93b74b4581d94fff0
Jon Phipps <jphipps@madcreek.com>
2016-12-16 14:50:01 -0500

    add relationships to user table

--- app/Models/Access/User/User.php
+++ app/Models/Access/User/User.php
@@ -64,81 +64,102 @@ use App\Models\Access\User\Traits\Relationship\UserRelationship;
  * @property-read string $action_buttons
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Access\Role\Role[] $roles
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Access\User\SocialLogin[] $providers
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereId($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereCreatedAt($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereUpdatedAt($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereLastUpdated($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereDeletedAt($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereNickname($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereSalutation($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereFirstName($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereLastName($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereEmail($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereSha1Password($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereSalt($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereWantToBeModerator($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereIsModerator($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereIsAdministrator($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereDeletions($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User wherePassword($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereCulture($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereRememberToken($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereConfirmationCode($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereName($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereConfirmed($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereStatus($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User confirmed($confirmed = true)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User active($status = true)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereId( $value )
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereCreatedAt( $value )
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereUpdatedAt( $value )
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereLastUpdated( $value )
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereDeletedAt( $value )
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereNickname( $value )
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereSalutation( $value )
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereFirstName( $value )
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereLastName( $value )
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereEmail( $value )
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereSha1Password( $value )
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereSalt( $value )
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereWantToBeModerator( $value )
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereIsModerator( $value )
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereIsAdministrator( $value )
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereDeletions( $value )
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User wherePassword( $value )
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereCulture( $value )
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereRememberToken( $value )
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereConfirmationCode( $value )
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereName( $value )
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereConfirmed( $value )
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User whereStatus( $value )
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User confirmed( $confirmed = true )
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Access\User\User active( $status = true )
  * @mixin \Eloquent
  */
 class User extends Authenticatable
 {
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
+  /**
+   * The database table used by the model.
+   *
+   * @var string
+   */
   protected $table = self::TABLE;
   const TABLE = 'reg_user';
 
   use UserScope, UserAccess, Notifiable, SoftDeletes, UserAttribute, UserRelationship, UserSendPasswordReset;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = ['name', 'email', 'password', 'status', 'confirmation_code', 'confirmed'];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [ 'name', 'email', 'password', 'status', 'confirmation_code', 'confirmed' ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = ['password', 'remember_token'];
+  /**
+   * The attributes that should be hidden for arrays.
+   *
+   * @var array
+   */
+  protected $hidden = [ 'password', 'remember_token' ];
 
-    /**
-     * @var array
-     */
-    protected $dates = ['deleted_at'];
+  /**
+   * @var array
+   */
+  protected $dates = [ 'deleted_at' ];
 
-	/**
-	 * @param array $attributes
-	 */
-	public function __construct(array $attributes = [])
-	{
-		parent::__construct($attributes);
-		$this->table = config('access.users_table');
-	}
 
-    /**
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
-     */
-    public function Projects()
-    {
-        return $this->belongsToMany(Project::class, ProjectHasUser::TABLE, 'user_id', 'agent_id')
-                    ->withPivot('is_registrar_for', 'is_admin_for')
-                    ->withTimestamps();
-    }
+  /**
+   * @param array $attributes
+   */
+  public function __construct(array $attributes = [])
+  {
+    parent::__construct($attributes);
+    $this->table = config('access.users_table');
+  }
+
+
+  /**
+   * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+   */
+  public function projects()
+  {
+    return $this->belongsToMany(Project::class, ProjectHasUser::TABLE, 'user_id', 'agent_id')
+        ->withPivot('is_registrar_for', 'is_admin_for')->withTimestamps();
+  }
+
+
+  /**
+   * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+   */
+  public function vocabs()
+  {
+    return $this->belongsToMany(Vocabulary::class, VocabularyHasUser::TABLE, 'user_id', 'vocabulary_id')
+        ->withPivot('is_registrar_for', 'is_admin_for', 'is_maintainer_for')->withTimestamps();
+  }
+
+
+  /**
+   * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+   */
+  public function elementsets()
+  {
+    return $this->belongsToMany(ElementSet::class, ElementSetHasUser::TABLE, 'user_id', 'schema_id')
+        ->withPivot('is_registrar_for', 'is_admin_for', 'is_maintainer_for')->withTimestamps();
+  }
 
 }
