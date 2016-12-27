e9623fb2868c8a3ff864923f63973fbb80543e38
Jon Phipps <jphipps@madcreek.com>
2016-12-27 11:53:46 -0500

    update TestCase for Boilerplate
    Former-commit-id: 70e40e8b1d7ef04c44e18850576814c96e52548e

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,25 +1,77 @@
 <?php
 
+use App\Models\Access\User\User;
+use App\Models\Access\Role\Role;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+/**
+ * Class TestCase
+ */
 abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
 {
-    /**
-     * The base URL to use while testing the application.
-     *
-     * @var string
-     */
-    protected $baseUrl = 'http://localhost';
 
-    /**
-     * Creates the application.
-     *
-     * @return \Illuminate\Foundation\Application
-     */
-    public function createApplication()
-    {
-        $app = require __DIR__.'/../bootstrap/app.php';
+  use DatabaseTransactions;
+  /**
+   * The base URL to use while testing the application.
+   *
+   * @var string
+   */
+  protected $baseUrl = 'http://registry.dev';
+  protected $admin;
+  protected $executive;
+  protected $user;
+  protected $adminRole;
+  protected $executiveRole;
+  protected $userRole;
 
-        $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+  public static $setupDatabase = false;
 
-        return $app;
+
+  /**
+   * Creates the application.
+   *
+   * @return \Illuminate\Foundation\Application
+   */
+  public function createApplication()
+  {
+    $app = require __DIR__ . '/../bootstrap/app.php';
+
+    $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+
+    return $app;
+  }
+
+
+  public function setUp()
+  {
+    parent::setUp();
+    // Run the tests in English
+    App::setLocale('en');
+
+    if (self::$setupDatabase) {
+      $this->setupDatabase();
     }
+  }
+
+
+  public function setupDatabase()
+  {
+    // Set up the database
+    Artisan::call('migrate:refresh');
+    Artisan::call('db:seed');
+
+    /**
+     * Create class properties to be used in tests
+     */
+    $this->admin         = User::find(1);
+    $this->executive     = User::find(2);
+    $this->user          = User::find(3);
+    $this->adminRole     = Role::find(1);
+    $this->executiveRole = Role::find(2);
+    $this->userRole      = Role::find(3);
+
+    self::$setupDatabase = false;
+  }
 }
