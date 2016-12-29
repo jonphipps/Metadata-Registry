48ccf11c09b060eef301b6768aca5d3fd34cd2be
Jon Phipps <jphipps@madcreek.com>
2016-12-29 14:50:57 -0500

    reinit behat
    Former-commit-id: 8f41103d6b3e5e56a2c1cbab679c61bd2d7054da

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,14 +1,19 @@
 <?php
 
+use Behat\Behat\Hook\Scope\AfterStepScope;
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
-use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+
+#This will be needed if you require "behat/mink-selenium2-driver"
+#use Behat\Mink\Driver\Selenium2Driver;
+use Behat\MinkExtension\Context\MinkContext;
 
 /**
  * Defines application features from the specific context.
  */
-class FeatureContext implements Context, SnippetAcceptingContext
+class FeatureContext extends MinkContext implements Context
 {
     /**
      * Initializes context.
