080347756c48c7b17ed83d2fb5b3329b4558f828
Jon Phipps <jphipps@madcreek.com>
2017-01-13 20:44:44 -0500

    force setup of the symfony db config to be driven by the laravel config databases.yml needs to look like this: ```xml all:   propel:     class:                sfPropelDatabase     param:       phptype:            mysqli       host:               %DB_HOST%       port:               %DB_PORT%       database:           %DB_DATABASE%       username:           %DB_USERNAME%       password:           %DB_PASSWORD%       compat_assoc_lower: true       datasource:         propel       encoding:           %DB_ENCODING% ```

--- apps/frontend/config/config.php
+++ apps/frontend/config/config.php
@@ -9,6 +9,18 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 // include project configuration
 include SF_ROOT_DIR . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'config.php';
 
+if (class_exists('DB')) {
+  require_once $sf_symfony_lib_dir . '/config/sfConfig.class.php';
+  sfConfig::set('db_host', DB::connection()->getConfig("host"));
+  sfConfig::set('db_port', DB::connection()->getConfig("port"));
+  sfConfig::set('db_database', DB::connection()->getConfig("database"));
+  sfConfig::set('db_username', DB::connection()->getConfig("username"));
+  sfConfig::set('db_password', DB::connection()->getConfig("password"));
+  sfConfig::set('db_encoding', DB::connection()->getConfig("charset"));
+}
+
+//override the symfony database settings with current Laravel
+
 // symfony bootstraping
 require_once $sf_symfony_lib_dir . '/util/sfCore.class.php';
 sfCore::bootstrap($sf_symfony_lib_dir, $sf_symfony_data_dir);
