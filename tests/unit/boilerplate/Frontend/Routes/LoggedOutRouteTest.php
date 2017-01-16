c9ed09850961dadec60eeaf3ef4eddd142338c3a
Jon Phipps <jphipps@madcreek.com>
2017-01-16 18:02:49 -0500

    404 page is 200 plus text

--- tests/unit/boilerplate/Frontend/Routes/LoggedOutRouteTest.php
+++ tests/unit/boilerplate/Frontend/Routes/LoggedOutRouteTest.php
@@ -13,125 +13,139 @@ use App\Notifications\Frontend\Auth\UserNeedsConfirmation;
 class LoggedOutRouteTest extends TestCase
 {
 
-	/**
-	 * User Logged Out Frontend
-	 */
+  /**
+   * User Logged Out Frontend
+   */
 
-	/**
-	 * Test the homepage works
-	 */
-	public function testHomePage()
-	{
-		$this->visit('/')
-			->assertResponseOk();
-	}
+  /**
+   * Test the homepage works
+   */
+  public function testHomePage()
+  {
+    $this->visit('/')
+         ->assertResponseOk();
+  }
 
-	/**
-	 * Test the macro page works
-	 */
-	public function testMacroPage()
-	{
-		$this->visit('/macros')
-			->see('Macro Examples');
-	}
 
-	/**
-	 * Test the login page works
-	 */
-	public function testLoginPage()
-	{
-		$this->visit('/login')
-			->see('Login');
-	}
+  /**
+   * Test the macro page works
+   */
+  public function testMacroPage()
+  {
+    $this->visit('/macros')
+         ->see('Macro Examples');
+  }
 
-	/**
-	 * Test the register page works
-	 */
-	public function testRegisterPage()
-	{
-		$this->visit('/register')
-			->see('Register');
-	}
 
-	/**
-	 * Test the forgot password page works
-	 */
-	public function testForgotPasswordPage()
-	{
-		$this->visit('password/reset')
-			->see('Reset Password');
-	}
+  /**
+   * Test the login page works
+   */
+  public function testLoginPage()
+  {
+    $this->visit('/login')
+         ->see('Login');
+  }
 
-	/**
-	 * Test the dashboard page redirects to login
-	 */
-	public function testDashboardPageLoggedOut() {
-		$this->visit('/dashboard')
-			->seePageIs('/login');
-	}
 
-	/**
-	 * Test the account page redirects to login
-	 */
-	public function testAccountPageLoggedOut() {
-		$this->visit('/account')
-			->seePageIs('/login');
-	}
+  /**
+   * Test the register page works
+   */
+  public function testRegisterPage()
+  {
+    $this->visit('/register')
+         ->see('Register');
+  }
 
-	/**
-	 * Create an unconfirmed user and assure the user gets
-	 * confirmed when hitting the confirmation route
-	 */
-	public function testConfirmAccountRoute() {
-		Event::fake();
 
-		// Create default user to test with
-		$unconfirmed = factory(User::class)
-			->states('unconfirmed')
-			->create();
-		$unconfirmed->attachRole(3); //User
+  /**
+   * Test the forgot password page works
+   */
+  public function testForgotPasswordPage()
+  {
+    $this->visit('password/reset')
+         ->see('Reset Password');
+  }
 
-		$this->visit('/account/confirm/'.$unconfirmed->confirmation_code)
-			->seePageIs('/login')
-			->see('Your account has been successfully confirmed!')
-			->seeInDatabase(config('access.users_table'), ['email' => $unconfirmed->email, 'confirmed'  => 1]);
 
-		Event::assertFired(UserConfirmed::class);
-	}
+  /**
+   * Test the dashboard page redirects to login
+   */
+  public function testDashboardPageLoggedOut()
+  {
+    $this->visit('/dashboard')
+         ->seePageIs('/login');
+  }
 
-	/**
-	 * Assure the user gets resent a confirmation email
-	 * after hitting the resend confirmation route
-	 */
-	public function testResendConfirmAccountRoute() {
-		Notification::fake();
 
-		$this->visit('/account/confirm/resend/'.$this->user->id)
-			->seePageIs('/login')
-			->see('A new confirmation e-mail has been sent to the address on file.');
+  /**
+   * Test the account page redirects to login
+   */
+  public function testAccountPageLoggedOut()
+  {
+    $this->visit('/account')
+         ->seePageIs('/login');
+  }
 
-		Notification::assertSentTo(
-			[$this->user], UserNeedsConfirmation::class
-		);
-	}
 
-	/**
-	 * Test the language switcher changes the desired language in the session
-	 */
-	public function testLanguageSwitcher() {
-		$this->visit('lang/es')
-			->see('Registrarse')
-			->assertSessionHas('locale', 'es');
+  /**
+   * Create an unconfirmed user and assure the user gets
+   * confirmed when hitting the confirmation route
+   */
+  public function testConfirmAccountRoute()
+  {
+    Event::fake();
 
-		App::setLocale('en');
-	}
+    // Create default user to test with
+    $unconfirmed = factory(User::class)
+        ->states('unconfirmed')
+        ->create();
+    $unconfirmed->attachRole(3); //User
 
-	/**
-	 * Test the generic 404 page
-	 */
-	public function test404Page()
-	{
-		$this->get('7g48hwbfw9eufj')
-			->seeStatusCode(418);
-	}
+    $this->visit('/account/confirm/' . $unconfirmed->confirmation_code)
+         ->seePageIs('/login')
+         ->see('Your account has been successfully confirmed!')
+         ->seeInDatabase(config('access.users_table'), [ 'email' => $unconfirmed->email, 'confirmed' => 1 ]);
+
+    Event::assertFired(UserConfirmed::class);
+  }
+
+
+  /**
+   * Assure the user gets resent a confirmation email
+   * after hitting the resend confirmation route
+   */
+  public function testResendConfirmAccountRoute()
+  {
+    Notification::fake();
+
+    $this->visit('/account/confirm/resend/' . $this->user->id)
+         ->seePageIs('/login')
+         ->see('A new confirmation e-mail has been sent to the address on file.');
+
+    Notification::assertSentTo([ $this->user ],
+        UserNeedsConfirmation::class);
+  }
+
+
+  /**
+   * Test the language switcher changes the desired language in the session
+   */
+  public function testLanguageSwitcher()
+  {
+    $this->visit('lang/es')
+         ->see('Registrarse')
+         ->assertSessionHas('locale', 'es');
+
+    App::setLocale('en');
+  }
+
+
+  /**
+   * Test the generic 404 page
+   */
+  public function test404Page()
+  {
+    $this->get('7g48hwbfw9eufj')
+         ->see('The server returned a 404 response');
+  }
 }
