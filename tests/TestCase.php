d6319480edd4cce707e888fb3c0c496679cab562
Jon Phipps <jphipps@madcreek.com>
2017-02-05 14:44:44 -0500

    disable exception handling on demand in tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -119,4 +119,16 @@ abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
     parent::assertPageLoaded($uri);
   }
 
+
+  protected function disableExceptionHandling()
+  {
+    $this->app->instance(ExceptionHandler::class,
+        new class extends Handler
+        {
+          public function __construct(){}
+          public function report(Exception $e) {}
+          public function render($request, Exception $e) { throw $e; }
+        });
+  }
+
 }
