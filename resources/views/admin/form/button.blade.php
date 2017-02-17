1c0cec769988fd5207683eea1ff1ff7babefb8cf
Jon Phipps <jphipps@madcreek.com>
2017-02-17 13:37:59 -0500

    delete views/admin and replace with vendor/admin

--- resources/views/admin/form/button.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="form-group">
-
-    <label class="col-sm-2 col-lg-2 control-label"></label>
-
-    <div class="col-sm-10 col-lg-8">
-        <input type='button' value='{{$label}}' class="btn {{ $class }}" {!! $attributes !!} />
-    </div>
-</div>
