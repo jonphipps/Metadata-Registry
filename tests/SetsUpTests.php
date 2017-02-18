4cb6ce5b916eabc4b87173c2b5e320b070d7bc8b
Jon Phipps <jphipps@madcreek.com>
2017-02-18 09:51:51 -0500

    only load a subset of seeds

--- tests/SetsUpTests.php
+++ tests/SetsUpTests.php
@@ -12,76 +12,75 @@ use Illuminate\Support\Facades\Artisan;
 trait SetsUpTests
 {
 
-  /**
-   * The base URL to use while testing the application.
-   *
-   * @var string
-   */
+    /**
+     * The base URL to use while testing the application.
+     *
+     * @var string
+     */
     protected $baseUrl;
 
-  protected $admin;
+    protected $admin;
 
-  /** @var  User $executive */
-  protected $executive;
+    /** @var  User $executive */
+    protected $executive;
 
-  /** @var  User $user */
-  protected $user;
+    /** @var  User $user */
+    protected $user;
 
-  /** @var  Role $adminRole */
-  protected $adminRole;
+    /** @var  Role $adminRole */
+    protected $adminRole;
 
-  /** @var  Role $executiveRole */
-  protected $executiveRole;
+    /** @var  Role $executiveRole */
+    protected $executiveRole;
 
-  /** @var  Role $userRole */
-  protected $userRole;
+    /** @var  Role $userRole */
+    protected $userRole;
 
-  public static $setupDatabase = true;
-
-  /**
-   * Set up tests.
-   */
-  public function setUpTests()
-  {
-        $this->baseUrl = config('app.url', 'http://registry.dev');
-
-    // Run the tests in English
-    App::setLocale('en');
-
-    if (self::$setupDatabase) {
-      $this->setupDatabase();
-    }
-
-    ini_restore('arg_separator.output');
-
-    /*
-     * Create class properties to be used in tests
-     */
-    $this->admin         = User::find(1);
-    $this->executive     = User::find(2);
-    $this->user          = User::find(3);
-    $this->adminRole     = Role::find(1);
-    $this->executiveRole = Role::find(2);
-    $this->userRole      = Role::find(3);
-
-  }
-
-
-  public function setupDatabase()
-  {
-    // Set up the database
-    Artisan::call('migrate:refresh');
-    Artisan::call('db:seed', ['--class' => 'AdminSeeder']);
-
-    self::$setupDatabase = false;
-  }
+    public static $setupDatabase = true;
 
     /**
-   * @param string $uri
-   * @param null|string|null $message
-   *
-   * @return $this
-   */
+     * Set up tests.
+     */
+    public function setUpTests()
+    {
+        $this->baseUrl = config('app.url', 'http://registry.dev');
+
+        // Run the tests in English
+        App::setLocale('en');
+
+        if (self::$setupDatabase) {
+            $this->setupDatabase();
+        }
+
+        ini_restore('arg_separator.output');
+
+        /*
+         * Create class properties to be used in tests
+         */
+        $this->admin = User::find(1);
+        $this->executive = User::find(2);
+        $this->user = User::find(3);
+        $this->adminRole = Role::find(1);
+        $this->executiveRole = Role::find(2);
+        $this->userRole = Role::find(3);
+    }
+
+    public function setupDatabase()
+    {
+        // Set up the database
+        Artisan::call('migrate:refresh');
+        Artisan::call('db:seed', ['--class' => 'AdminSeeder']);
+        // Artisan::call('db:seed');
+
+        self::$setupDatabase = false;
+    }
+
+    /**
+     * @param string           $uri
+     * @param null|string|null $message
+     *
+     * @return $this
+     */
     protected function assertPageLoaded($uri, $message = null)
     {
         if ($this->response->getStatusCode() == 418) {
@@ -91,9 +90,11 @@ trait SetsUpTests
             define('SF_ENVIRONMENT', env('SF_ENVIRONMENT', 'prod'));
             define('SF_DEBUG', env('SF_DEBUG', 'false'));
 
-            require_once SF_ROOT_DIR . DIRECTORY_SEPARATOR . 'apps' . DIRECTORY_SEPARATOR . SF_APP . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'config.php';
+            require_once SF_ROOT_DIR.DIRECTORY_SEPARATOR.'apps'.DIRECTORY_SEPARATOR.SF_APP.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php';
 
-            sfContext::getInstance()->getController()->dispatch();
+            sfContext::getInstance()
+                     ->getController()
+                     ->dispatch();
         }
         parent::assertPageLoaded($uri);
     }
