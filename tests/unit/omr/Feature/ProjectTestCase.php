64b9d1e07f476b70e3a56426662ed27f42340a33
Jon Phipps <jphipps@madcreek.com>
2017-02-18 09:56:35 -0500

    add project test

---
+++ tests/unit/omr/Feature/ProjectTestCase.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\BrowserKitTestCase;
+
+/**
+ * Class LoggedInRouteTest
+ */
+class ProjectTestCase extends BrowserKitTestCase
+{
+  use DatabaseTransactions;
+
+
+
+  /**
+   * @test
+   */
+  public function a_guest_sees_the_register_and_login_items_in_the_header()
+  {
+    //given I am NOT logged in
+    Auth::logout();
+
+    $this->get('projects')
+        ->seePageIs('projects')
+        ->assertResponseStatus(200)
+        ->see('Login')
+        ->see('Register');
+  }
+
+
+
+
+}
