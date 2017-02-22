2836685a09be2a09e31083b3894f224ae2c119a7
Jon Phipps <jphipps@madcreek.com>
2017-02-22 14:32:16 -0500

    reenable ‘make a project’ for new users

--- app/Http/Controllers/Frontend/User/DashboardController.php
+++ app/Http/Controllers/Frontend/User/DashboardController.php
@@ -20,31 +20,18 @@ class DashboardController extends Controller
     return Admin::content(function (Content $content) {
 
       $user = Auth::user();
-      $content->row(new Box('My Projects', $this->projectGrid()));
 
-      // if ($user->projects()->count()) {
-      //   $content->header('Dashboard');
-      //   $content->description('This project has...');
-      //
-      //   $content->row(function ($row) {
-      //     /** @var \Encore\Admin\Layout\Row $row */
-      //     $row->column(3, new InfoBox('Value Vocabularies', 'users', 'aqua', '/admin/users', '1024'));
-      //     $row->column(3, new InfoBox('Concepts', 'shopping-cart', 'green', '/admin/orders', '150%'));
-      //     $row->column(3, new InfoBox('Element Sets', 'book', 'yellow', '/admin/articles', '2786'));
-      //     $row->column(3, new InfoBox('Elements', 'file', 'red', '/admin/files', '698726'));
-      //   });
-      // } else {
-      //   $content->header('Start by Adding a Project!');
-      //   $content->row(function (Row $row) {
-      //
-      //     $row->column(6,
-      //         '<a class="btn btn-primary btn-lg" href="projects/create">Add New Project...</a>');
-      //   });
-      // }
-
+            if ($user->projects()->count()) {
+                $content->row(new Box('My Projects', $this->projectGrid()));
+            } else {
+                $content->header('Start by Adding a Project!');
+                $content->row(function (Row $row) {
+                    $row->column(6,
+                        '<a class="btn btn-primary btn-lg" href="projects/create">Add New Project...</a>');
+                });
+            }
     });
   }
-
 
   /**
    * Make a grid builder.
