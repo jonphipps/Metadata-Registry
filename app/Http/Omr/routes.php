b42a64ee5e3db9f4a17b33fcb0697d814dd54a4d
Jon Phipps <jphipps@madcreek.com>
2017-02-23 13:55:13 -0500

    cleanup router to allow controller to handle ‘show, index’

--- app/Http/Omr/routes.php
+++ app/Http/Omr/routes.php
@@ -5,30 +5,22 @@ use Illuminate\Routing\Router;
 Route::group([
     'prefix'     => config('admin.prefix'),
     'namespace'  => Admin::controllerNamespace(),
-    'middleware' => [ 'web'],
+    'middleware' => [ 'web' ],
 ],
     function (Router $router) {
-
       $router->get('/', 'HomeController@index');
-      $router->resource('/projects', 'ProjectController');
+      $router->resource('/projects',
+          'ProjectController');
       $router->resource('/vocabularies', 'VocabularyController');
-
-        Route::group([
-            'middleware' => ['admin'],
-        ],
-            function (Router $router) {
-                $router->resource('/users', 'UserController');
-                $router->resource('projects',
-                    'ProjectController',
-                    [
-                        'except' => [
-                            'index',
-                            'show',
-                        ],
-                    ]);
-                $router->get('project_user/{id}',
-                    'ProjectHasUserController@edit')->name('project.user.edit');
-            });
+      Route::group([
+          'middleware' => [ 'admin' ],
+      ],
+          function (Router $router) {
+            $router->resource('/users', 'UserController');
+            $router->get('project_user/{id}',
+                'ProjectHasUserController@edit')
+                   ->name('project.user.edit');
+          });
 
     });
 
