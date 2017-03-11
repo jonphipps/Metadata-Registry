a1cb4d1ee722c07e3807d59a6440d1f1ecafe926
Jon Phipps <jphipps@madcreek.com>
2017-03-11 18:11:28 -0500

    move projecthasusercontroller to omr

--- app/Http/Omr/Controllers/ProjectHasUserController.php
+++ app/Http/Omr/Controllers/ProjectHasUserController.php
@@ -1,16 +1,15 @@
 <?php
 
-namespace App\Http\Controllers\Frontend;
+namespace App\Http\Omr\Controllers;
 
 use App\Models\ProjectHasUser;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Admin;
 use Encore\Admin\Layout\Content;
-use App\Http\Controllers\Controller;
 use Encore\Admin\Controllers\ModelForm;
 
-class ProjectHasUserController extends Controller
+class ProjectHasUserController extends OmrController
 {
   use ModelForm;
 
