a735650b72a4c0176615e6ff50ec25f450960bda
Jon Phipps <jphipps@madcreek.com>
2017-03-25 10:36:58 -0400

    update composer, yarn

--- webpack.mix.js
+++ webpack.mix.js
@@ -31,3 +31,14 @@ mix.sass('resources/assets/sass/frontend/app.scss', 'web/css/frontend.css')
         ]
     })
     .version();
+/*
+ | See https://browsersync.io/docs/options
+*/
+// mix.browsersync({
+//     files: [
+//         'app/**/*.php',
+//         'resources/views/**/*.php',
+//         'web/js/**/*.js',
+//         'web/css/**/*.css'
+//     ]
+// });
