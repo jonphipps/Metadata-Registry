0168c7e48c0d75b2e6f4ccd747444fb67e83447b
Jon Phipps <jphipps@madcreek.com>
2017-03-29 13:15:44 -0400

    adding files to be built by gulp

---
+++ resources/assets/js/components/frontend/Example.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="col-xs-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">Example Vue.js Component</div>
+
+            <div class="panel-body">
+                I'm an example Vue.js component!
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Vue.js Component mounted.')
+        }
+    }
+</script>
