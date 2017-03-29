fd94f429d56c33c8731c9eec35f1cad84620fbb6
Jon Phipps <jphipps@madcreek.com>
2017-03-29 17:48:35 -0400

    update app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,92 +2,81 @@
 
 namespace App\Providers;
 
-use Carbon\Carbon;
-use Psr\Log\LoggerInterface;
+use Antennaio\Codeception\DbDumpServiceProvider;
+use Barryvdh\Debugbar\Facade;
+use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
 use Illuminate\Contracts\Logging\Log;
-use Illuminate\Support\Facades\Schema;
+use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\ServiceProvider;
+use Mpociot\LaravelTestFactoryHelper\TestFactoryHelperServiceProvider;
+use Psr\Log\LoggerInterface;
+use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
 
   /**
-     * Bootstrap any application services.
-     *
-     * @return void
+   * Bootstrap any application services.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+    /**
+     * Application locale defaults for various components
+     * These will be overridden by LocaleMiddleware if the session local is set
      */
-    public function boot()
-    {
-        /*
-         * Application locale defaults for various components
-         *
-         * These will be overridden by LocaleMiddleware if the session local is set
-         */
-
-        /*
-         * setLocale for php. Enables ->formatLocalized() with localized values for dates
-         */
-        setlocale(LC_TIME, config('app.locale_php'));
-
-        /*
-         * setLocale to use Carbon source locales. Enables diffForHumans() localized
-         */
-        Carbon::setLocale(config('app.locale'));
-
-        /*
-         * Set the session variable for whether or not the app is using RTL support
-         * For use in the blade directive in BladeServiceProvider
-         */
-        if (config('locale.languages')[config('app.locale')][2]) {
-            session([ 'lang-rtl' => true ]);
-        } else {
-            session()->forget('lang-rtl');
-        }
-
-        // Force SSL in production
-        if ($this->app->environment() == 'production') {
-            //URL::forceScheme('https');
-        }
-
-        // Set the default string length for Laravel5.4
-        // https://laravel-news.com/laravel-5-4-key-too-long-error
-        Schema::defaultStringLength(191);
+    /**
+     * setLocale for php. Enables ->formatLocalized() with localized values for dates
+     */
+    setlocale(LC_TIME, config('app.locale_php'));
+    /**
+     * setLocale to use Carbon source locales. Enables diffForHumans() localized
+     */
+    Carbon::setLocale(config('app.locale'));
+    /**
+     * Set the session variable for whether or not the app is using RTL support
+     * For use in the blade directive in BladeServiceProvider
+     */
+    if (config('locale.languages')[config('app.locale')][2]) {
+      session([ 'lang-rtl' => true ]);
+    } else {
+      session()->forget('lang-rtl');
     }
+    // Force SSL in production
+    if ($this->app->environment() === 'production') {
+      //URL::forceSchema('https');
+    }
+  }
 
   /**
-     * Register any application services.
-     *
-     * @return void
+   * Register any application services.
+   *
+   * @return void
+   */
+  public function register()
+  {
+    /**
+     * Sets third party service providers that are only needed on local environments
      */
-    public function register()
-    {
-        /*
-         * Sets third party service providers that are only needed on local/testing environments
-         */
-        if ($this->app->environment() != 'production') {
-            /**
-             * Loader for registering facades.
-             */
-            $loader = \Illuminate\Foundation\AliasLoader::getInstance();
-
-            /*
-             * Load third party local providers
-             */
-            $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
-
-            /*
-             * Load third party local aliases
-             */
-            $loader->alias('Debugbar', \Barryvdh\Debugbar\Facade::class);
-
-      $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
-      $this->app->register(\Mpociot\LaravelTestFactoryHelper\TestFactoryHelperServiceProvider::class);
-      $this->app->register(\Antennaio\Codeception\DbDumpServiceProvider::class);
-      $this->app->register(\Laravel\Dusk\DuskServiceProvider::class);
+    $environment = $this->app->environment();
+    if ($environment === 'dev' || $environment === 'testing') {
+      /**
+       * Loader for registering facades
+       */
+      $loader = AliasLoader::getInstance();
+      /**
+       * Load third party local providers and facades
+       */
+      $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
+      $loader->alias('Debugbar', Facade::class);
+      $this->app->register(IdeHelperServiceProvider::class);
+      $this->app->register(TestFactoryHelperServiceProvider::class);
+      $this->app->register(DbDumpServiceProvider::class);
 
     }
-    $this->app->alias('bugsnag.multi', Log::class);
-    $this->app->alias('bugsnag.multi', LoggerInterface::class);
+    $this->app->alias('bugsnag.logger', Log::class);
+    $this->app->alias('bugsnag.logger', LoggerInterface::class);
     $this->app->bind('path.public',
         function () {
           return base_path() . '/web';
