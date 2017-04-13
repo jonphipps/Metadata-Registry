3a2723846883a8de3c5b7f22489a77f8b11bf3cb
Jon Phipps <jphipps@madcreek.com>
2017-04-13 09:56:51 -0400

    mysql 5.7

--- scripts/travis_mysql_5.7.sh
+++ scripts/travis_mysql_5.7.sh
@@ -2,8 +2,8 @@
 sudo service mysql stop || echo "mysql not stopped"
 sudo stop mysql-5.6 || echo "mysql-5.6 not stopped"
 echo mysql-apt-config mysql-apt-config/select-server select mysql-5.7 | sudo debconf-set-selections
-wget http://dev.mysql.com/get/mysql-apt-config_0.7.3-1_all.deb
-sudo dpkg --install mysql-apt-config_0.7.3-1_all.deb
+wget http://dev.mysql.com/get/mysql-apt-config_0.8.4-1_all.deb
+sudo dpkg --install mysql-apt-config_0.8.4-1_all.deb
 sudo apt-get update -q
 sudo apt-get install -q -y -o --force-yes Dpkg::Options::=--force-confnew mysql-server
 sudo mysql_upgrade
