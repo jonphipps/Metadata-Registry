03f971e2c5ee77ee822480dd5349a4d52934dcf5
Jon Phipps <jphipps@madcreek.com>
2017-04-14 14:45:37 -0400

    add an htmlheader partial

---
+++ resources/views/frontend/layouts/partials/htmlheader.blade.php
@@ -0,0 +1,35 @@
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    	  <link rel="icon" href="/<?php echo env('FAVICON', 'registry_favicon_prod.ico') ?>"/>
+
+        <title>@yield('title', app_name())</title>
+
+        <!-- Meta -->
+        <meta name="description" content="@yield('meta_description', 'Open Metadata Registry')">
+        <meta name="author" content="@yield('meta_author', 'Jon Phipps')">
+        @yield('meta')
+
+        <!-- Styles -->
+        @yield('before-styles')
+
+        <!-- Check if the language is set to RTL, so apply the RTL layouts -->
+        <!-- Otherwise apply the normal LTR layouts -->
+        @langRTL
+            {{ Html::style(getRtlCss(mix('css/frontend.css'))) }}
+        @else
+            {{ Html::style(mix('css/frontend.css')) }}
+        @endif
+
+        @yield('after-styles')
+
+        <!-- Scripts -->
+        <script>
+            window.Laravel = <?php echo json_encode([
+                'csrfToken' => csrf_token(),
+            ]); ?>
+        </script>
+    </head>
