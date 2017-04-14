e309f331ac6deb20dfe9af624dc8c1d279439817
Jon Phipps <jphipps@madcreek.com>
2017-04-14 14:45:14 -0400

    normalize the main layout

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
-<html lang="{{ config('app.locale') }}">
+<html class="no-js" lang="{{ config('app.locale') }}">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-    	<link rel="icon" href="/<?php echo env('FAVICON', 'registry_favicon_prod.ico') ?>"/>
         <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    	  <link rel="icon" href="/<?php echo env('FAVICON', 'registry_favicon_prod.ico') ?>"/>
 
         <title>@yield('title', app_name())</title>
 
