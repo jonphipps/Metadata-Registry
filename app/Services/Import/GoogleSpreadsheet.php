e1c0b2702bed91db449e4ad00ecae2dac8727ed7
Jon Phipps <jphipps@madcreek.com>
2017-05-05 18:06:43 -0400

    ignore the google spreadsheet service startup

---
+++ app/Services/Import/GoogleSpreadsheet.php
@@ -0,0 +1,15 @@
+<?php
+
+/** Created by PhpStorm,  User: jonphipps,  Date: 2017-05-05,  Time: 5:57 PM */
+
+namespace App\Services\Import;
+
+
+class GoogleSpreadsheet
+{
+/** Google spreadsheet object has
+ * a google services connection <— built with service
+ * a URL <— input in constructor
+ * an identifier <— gets from the URL
+ * a collection of worksheets <— gets from service */
+}
