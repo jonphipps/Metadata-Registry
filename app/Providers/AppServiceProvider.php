69986457ba391d40ce5ca46023cc42410894d210
Jon Phipps <jphipps@madcreek.com>
2017-05-09 11:12:02 -0400

    add reliese/laravel model generator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -97,6 +97,7 @@ class AppServiceProvider extends ServiceProvider
       $this->app->register(DbDumpServiceProvider::class);
         $this->app->register(\Way\Generators\GeneratorsServiceProvider::class);
         $this->app->register(\Xethron\MigrationsGenerator\MigrationsGeneratorServiceProvider::class);
+        $this->app->register(\Reliese\Coders\CodersServiceProvider::class);
     }
     $this->app->bind('path.public', function () {return base_path() . '/web';});
     $this->app->alias('bugsnag.multi', \Illuminate\Contracts\Logging\Log::class);
