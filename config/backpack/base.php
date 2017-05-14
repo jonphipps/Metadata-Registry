716def728dbfbfc11609969ecebfdd3c4eed3b78
Jon Phipps <jphipps@madcreek.com>
2017-05-14 13:48:59 -0400

    add backpack configs

---
+++ config/backpack/base.php
@@ -0,0 +1,78 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Look & feel customizations
+    |--------------------------------------------------------------------------
+    |
+    | Make it yours.
+    |
+    */
+
+    // Project name. Shown in the breadcrumbs and a few other places.
+    'project_name' => 'Backpack',
+
+    // Menu logos
+    'logo_lg'   => '<b>Open</b>Registry',
+    'logo_mini' => '<b>OMR</b>p',
+
+    // Developer or company name. Shown in footer.
+    'developer_name' => 'Jon Phipps',
+
+    // Developer website. Link in footer.
+    'developer_link' => 'http://managemetadata.com',
+
+    // Show powered by Laravel Backpack in the footer?
+    'show_powered_by' => true,
+
+    // The AdminLTE skin. Affects menu color and primary/secondary colors used throughout the application.
+    'skin' => 'skin-blue',
+    // Options: skin-black, skin-blue, skin-purple, skin-red, skin-yellow, skin-green, skin-blue-light, skin-black-light, skin-purple-light, skin-green-light, skin-red-light, skin-yellow-light
+
+    // Date & Datetime Format Syntax: https://github.com/jenssegers/date#usage
+    // (same as Carbon)
+    'default_date_format'     => 'F j\, Y',
+    'default_datetime_format' => 'F j\, Y H:i',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Registration Open
+    |--------------------------------------------------------------------------
+    |
+    | Choose whether new users are allowed to register.
+    | This will show up the Register button in the menu and allow access to the
+    | Register functions in AuthController.
+    |
+    */
+
+    'registration_open' => (env('APP_ENV') === 'dev') ? true : false,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Routing
+    |--------------------------------------------------------------------------
+    */
+
+    // The prefix used in all base routes (the 'admin' in admin/dashboard)
+    'route_prefix' => '',
+
+    // Set this to false if you would like to use your own AuthController and PasswordController
+    // (you then need to setup your auth routes manually in your routes.php file)
+    'setup_auth_routes' => false,
+
+    // Set this to false if you would like to skip adding the dashboard routes
+    // (you then need to overwrite the login route on your AuthController)
+    'setup_dashboard_routes' => false,
+
+    /*
+    |--------------------------------------------------------------------------
+    | User Model
+    |--------------------------------------------------------------------------
+    */
+
+    // Fully qualified namespace of the User model
+    'user_model_fqn' => '\App\Models\Access\User\User',
+
+];
--- config/filesystems.php
+++ config/filesystems.php
@@ -63,6 +63,10 @@ return [
             'bucket' => env('AWS_BUCKET'),
         ],
 
+        'uploads' => [
+            'driver' => 'local',
+            'root' => public_path('uploads'),
+        ],
     ],
 
 ];
