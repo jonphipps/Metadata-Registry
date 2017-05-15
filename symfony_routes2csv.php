70ceaab7474c55f475ab9753b08fb15106f3b35f
Jon Phipps <jphipps@madcreek.com>
2017-05-15 15:28:07 -0400

    little csv route builder script

---
+++ symfony_routes2csv.php
@@ -0,0 +1,12 @@
+<?php
+include __DIR__ . '/vendor/autoload.php';
+$routes = unserialize(file_get_contents(__DIR__ . '/symfony_routes.php'),[true]);
+$list[] = '"url","route name","module","action","tabnav"';
+foreach ($routes as $key => $route) {
+    $list[] = implode(",", ['"'.$route["url"].'"',
+        '"' . $key . '"',
+        '"' . ( $route['param']['module'] ?? '' ) . '"',
+        '"' . ( ( strpos($key, '_resource') !== false ) ? 'RESOURCE' : $route['param']['action'] ?? '' ) . '"',
+        '"' . ( $route['param']['tabnav'] ?? '' ) . '"']);
+}
+file_put_contents(__DIR__ . '/symfony_routes.csv', implode("\n", $list));
