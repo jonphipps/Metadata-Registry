3a374973d60df611a5bf86dedeba835d3c5b64ea
Jon Phipps <jphipps@madcreek.com>
2017-05-15 20:01:30 -0400

    new user dashboard

--- resources/views/frontend/user/dashboard.blade.php
+++ resources/views/frontend/user/dashboard.blade.php
@@ -1,4 +1,5 @@
-@extends('frontend.layouts.app')
+@extends('backpack::layout')
+
 
 @section('content')
     <div class="row">
@@ -7,11 +8,8 @@
                 <div class="panel-heading">{{ trans('navs.frontend.dashboard') }}</div>
 
                 <div class="panel-body">
-                    <div class="row">
-                        @include('includes.partials.r-sidebar')
-                        <div class="col-md-8 col-md-pull-4">
                             <div class="row">
-                                <div class="col-xs-12">
+                                <div class="col-md-6">
                                     <div class="panel panel-default">
                                         <div class="panel-heading">
                                             <h4>My Projects</h4>
@@ -28,6 +26,28 @@
                                         </div><!--panel-body-->
                                     </div><!--panel-->
                                 </div><!--col-xs-12-->
+                                <div class="col-md-3">
+                                    <div class="panel panel-default">
+                                        <div class="panel-heading">
+                                            <h4>Members</h4>
+                                        </div><!--panel-heading-->
+                                        <div class="panel-body">
+                                            <p>This will be a list of members of this project
+                                                (maybe). Languages in use is another possibility. Or maps.</p>
+                                        </div><!--panel-body-->
+                                    </div><!--panel-->
+                                </div><!--col-md-6-->
+                                <div class="col-md-3">
+                                    <div class="panel panel-default">
+                                        <div class="panel-heading">
+                                            <h4>Profiles</h4>
+                                        </div><!--panel-heading-->
+                                        <div class="panel-body">
+                                            <p>This will be a list of Application Profiles used by this project
+                                                (maybe)</p>
+                                        </div><!--panel-body-->
+                                    </div><!--panel-->
+                                </div><!--col-md-6-->
                             </div><!--row-->
                             <div class="row">
                                 <div class="col-md-6">
@@ -62,31 +82,7 @@
                                         </div><!--panel-body-->
                                     </div><!--panel-->
                                 </div><!--col-md-6-->
-                                <div class="col-md-6">
-                                    <div class="panel panel-default">
-                                        <div class="panel-heading">
-                                            <h4>Profiles</h4>
-                                        </div><!--panel-heading-->
-                                        <div class="panel-body">
-                                            <p>This will be a list of Application Profiles used by this project
-                                                (maybe)</p>
-                                        </div><!--panel-body-->
-                                    </div><!--panel-->
-                                </div><!--col-md-6-->
-                                <div class="col-md-6">
-                                    <div class="panel panel-default">
-                                        <div class="panel-heading">
-                                            <h4>Members</h4>
-                                        </div><!--panel-heading-->
-                                        <div class="panel-body">
-                                            <p>This will be a list of members of this project
-                                                (maybe). Languages in use is another possibility. Or maps.</p>
-                                        </div><!--panel-body-->
-                                    </div><!--panel-->
-                                </div><!--col-md-6-->
                             </div><!--row-->
-                        </div><!--col-md-8-->
-                    </div><!--row-->
                 </div><!--panel body-->
             </div><!-- panel -->
         </div><!-- col-md-10 -->
