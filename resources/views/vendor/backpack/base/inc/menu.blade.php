a853eea09fe4dabc715c5fdd0efb776244a2850c
Jon Phipps <jphipps@madcreek.com>
2017-05-15 20:02:30 -0400

    rework backpack top menu to work with boilerplate auth

--- resources/views/vendor/backpack/base/inc/menu.blade.php
+++ resources/views/vendor/backpack/base/inc/menu.blade.php
@@ -3,9 +3,19 @@
         <!-- =================================================== -->
         <!-- ========== Top menu items (ordered left) ========== -->
         <!-- =================================================== -->
-
-        <!-- <li><a href="{{ url('/') }}"><i class="fa fa-home"></i> <span>Home</span></a></li> -->
-
+        <ul class="nav navbar-nav">
+            @if ($logged_in_user)
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                        {{ trans('navs.frontend.projects') }}
+                        <span class="caret"></span>
+                    </a>
+                    <ul class="dropdown-menu" role="menu">
+                        @include('includes.partials.projects')
+                    </ul>
+                </li>
+            @endif
+        </ul>
         <!-- ========== End of top menu left items ========== -->
     </ul>
 </div>
@@ -16,18 +26,32 @@
       <!-- ========================================================= -->
       <!-- ========== Top menu right items (ordered left) ========== -->
       <!-- ========================================================= -->
-
-      <!-- <li><a href="{{ url('/') }}"><i class="fa fa-home"></i> <span>Home</span></a></li> -->
-
-        @if (Auth::guest())
-            <li><a href="{{ url(config('backpack.base.route_prefix', 'admin').'/login') }}">{{ trans('backpack::base.login') }}</a></li>
-            @if (config('backpack.base.registration_open'))
-            <li><a href="{{ url(config('backpack.base.route_prefix', 'admin').'/register') }}">{{ trans('backpack::base.register') }}</a></li>
-            @endif
-        @else
-            <li><a href="{{ url(config('backpack.base.route_prefix', 'admin').'/logout') }}"><i class="fa fa-btn fa-sign-out"></i> {{ trans('backpack::base.logout') }}</a></li>
+        @if ($logged_in_user)
+            <li>{{ laravel_link_to_route('frontend.user.dashboard', trans('navs.frontend.dashboard'), [], ['class' => active_class(Active::checkRoute('frontend.user.dashboard')) ]) }}</li>
         @endif
 
-       <!-- ========== End of top menu right items ========== -->
+        @if (! $logged_in_user)
+            <li>{{ laravel_link_to_route('frontend.auth.login', trans('navs.frontend.login'), [], ['class' => active_class(Active::checkRoute('frontend.auth.login')) ]) }}</li>
+
+            @if (config('access.users.registration'))
+                <li>{{ laravel_link_to_route('frontend.auth.register', trans('navs.frontend.register'), [], ['class' => active_class(Active::checkRoute('frontend.auth.register')) ]) }}</li>
+            @endif
+        @else
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                    {{ $logged_in_user->name }} <span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu" role="menu">
+                    @permission('view-backend')
+                    <li>{{ laravel_link_to_route('admin.dashboard', trans('navs.frontend.user.administration')) }}</li>
+                    @endauth
+                    <li>{{ laravel_link_to_route('frontend.user.account', trans('navs.frontend.user.account'), [], ['class' => active_class(Active::checkRoute('frontend.user.account')) ]) }}</li>
+                    <li>{{ laravel_link_to_route('frontend.auth.logout', trans('navs.general.logout')) }}</li>
+                </ul>
+            </li>
+    @endif
+
+
+    <!-- ========== End of top menu right items ========== -->
     </ul>
 </div>
