d53b9e361049d4fc61696306d967026aa09019a6
Jon Phipps <jphipps@madcreek.com>
2017-05-17 16:36:26 -0400

    rearrange dashboard

--- resources/views/frontend/project/dashboard.blade.php
+++ resources/views/frontend/project/dashboard.blade.php
@@ -4,58 +4,9 @@
         <div class="col-xs-12">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    <h1>{{ $project->org_name }}</h1>
+                    <h1>{{ $project->label }}</h1>
                 </div>
                 <div class="panel-body">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <h4>Project Detail</h4>
-                                </div><!--panel-heading-->
-                                <div class="panel-body">
-                                    <div class="list-group">Metadata</div>
-                                    <dl class="dl-horizontal">
-                                        <dt>Created At</dt>
-                                        <dd>{{$project->created_at->toFormattedDateString()}}</dd>
-                                        <dt>Updated At</dt>
-                                        <dd>{{$project->updated_at->toFormattedDateString()}}</dd>
-                                    </dl>
-                                    <div class="list-group">Description</div>
-                                    <dl class="dl-horizontal">
-                                        @foreach($project->toArray() as $property => $value)
-                                            <dt>{{$property}}</dt>
-                                            <dd>{{$value}}</dd>
-                                        @endforeach
-                                    </dl>
-                                    @can('edit', $project)
-                                        <a class="btn btn-default btn-sm pull-right" href="projects/{{$project->id}}/edit" role="button">Edit</a>
-                                    @endcan
-                                </div><!--panel-body-->
-                            </div><!--panel-->
-                        </div><!--col-xs-12-->
-                        <div class="col-md-6">
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <h4>Profiles</h4>
-                                </div><!--panel-heading-->
-                                <div class="panel-body">
-                                    <p>This will be a list of Application Profiles used by this project
-                                        (maybe)</p>
-                                </div><!--panel-body-->
-                            </div><!--panel-->
-                        </div><!--col-md-6-->
-                        <div class="col-md-6">
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <h4>Members</h4>
-                                </div><!--panel-heading-->
-                                <div class="panel-body">
-                                    <p>This will be a list of members of this project</p>
-                                </div><!--panel-body-->
-                            </div><!--panel-->
-                        </div><!--col-md-6-->
-                    </div><!--row-->
                     <div class="row">
                         <div class="col-md-6">
                             <div class="panel panel-default">
@@ -86,6 +37,44 @@
                                             No Element Sets defined
                                         @endforelse
                                     </ul>
+                                </div><!--panel-body-->
+                            </div><!--panel-->
+                        </div><!--col-md-6-->
+                    </div><!--row-->
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4>Project Detail</h4>
+                                </div><!--panel-heading-->
+                                <div class="panel-body">
+                                    <div class="list-group">Metadata</div>
+                                    <dl class="dl-horizontal">
+                                        <dt>Created At</dt>
+                                        <dd>{{$project->created_at->toFormattedDateString()}}</dd>
+                                        <dt>Updated At</dt>
+                                        <dd>{{$project->updated_at->toFormattedDateString()}}</dd>
+                                    </dl>
+                                    <div class="list-group">Description</div>
+                                    <dl class="dl-horizontal">
+                                        @foreach($project->toArray() as $property => $value)
+                                            <dt>{{ title_case(str_replace('_',' ',$property))}}</dt>
+                                            <dd>{{ is_array($value) ? 'ARRAY' : $value }}</dd>
+                                        @endforeach
+                                    </dl>
+                                    @can('edit', $project)
+                                        <a class="btn btn-default btn-sm pull-right" href="projects/{{$project->id}}/edit" role="button">Edit</a>
+                                    @endcan
+                                </div><!--panel-body-->
+                            </div><!--panel-->
+                        </div><!--col-xs-12-->
+                        <div class="col-md-6">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4>Activity</h4>
+                                </div><!--panel-heading-->
+                                <div class="panel-body">
+                                    <p>This will be a running display of project activity</p>
                                 </div><!--panel-body-->
                             </div><!--panel-->
                         </div><!--col-md-6-->
@@ -132,6 +121,29 @@
                                 </div><!--panel-heading-->
                                 <div class="panel-body">
                                     <p>This will be a list of maps maintained by this project</p>
+                                </div><!--panel-body-->
+                            </div><!--panel-->
+                        </div><!--col-md-6-->
+                    </div><!--row-->
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4>Members</h4>
+                                </div><!--panel-heading-->
+                                <div class="panel-body">
+                                    <p>This will be a list of members of this project
+                                        (maybe)</p>
+                                </div><!--panel-body-->
+                            </div><!--panel-->
+                        </div><!--col-md-6-->
+                        <div class="col-md-6">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4>Profiles</h4>
+                                </div><!--panel-heading-->
+                                <div class="panel-body">
+                                    <p>This will be a list of Application Profiles used by this project</p>
                                 </div><!--panel-body-->
                             </div><!--panel-->
                         </div><!--col-md-6-->
