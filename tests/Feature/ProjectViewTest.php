3f593d7596388e60c4ee12a050aa6e054bf2bd4c
Jon Phipps <jphipps@madcreek.com>
2017-05-18 18:28:26 -0400

    refactor the project view test to match the new project view

--- tests/Feature/ProjectViewTest.php
+++ tests/Feature/ProjectViewTest.php
@@ -26,7 +26,7 @@ class ProjectViewTest extends TestCase
     $project = factory(Project::class)->create();
     //when I go to the url
     $this->get($this->baseUrl . '/projects/' . $project->id)
-        ->assertSeeText(htmlspecialchars($project->org_name))
+        ->assertSeeText(htmlspecialchars($project->label))
         ->assertDontSee("projects/{$project->id}/edit");
   }
 
@@ -48,16 +48,14 @@ class ProjectViewTest extends TestCase
                                'user_id'          => $this->user->id, ]);
     //check the list for editability
     $this->actingAs($this->user);
-    $this->get($this->baseUrl . '/projects')->assertSeeText($project->org_name)
-        ->assertSee("/projects/{$project->id}/edit")
-        ->assertDontSee('/projects/58/edit');
+    $this->get($this->baseUrl . '/projects')->assertSeeText($project->label);
     $this->get($this->baseUrl . '/projects/' . $project->id)
-        ->assertSeeText(htmlspecialchars($project->org_name))
+        ->assertSeeText(htmlspecialchars($project->label))
         ->assertSee("projects/{$project->id}/edit");
-    $this->get($this->baseUrl . '/projects/' . $project->id)
-        ->assertSeeText($project->org_name)
+    $this->get($this->baseUrl . '/projects/58')
+        ->assertSeeText('NSDL Registry')
         ->assertDontSee('projects/58/edit');
-    $this->get($this->baseUrl . '/dashboard' )->assertSee($project->org_name);
+    $this->get($this->baseUrl . '/dashboard' )->assertSee($project->label);
   }
 
 }
