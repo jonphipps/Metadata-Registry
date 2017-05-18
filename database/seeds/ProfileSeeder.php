dd6efc3246ef9fef6fd99db16e1884f82c22ee08
Jon Phipps <jphipps@madcreek.com>
2017-05-18 18:24:41 -0400

    new profile seeder

--- database/seeds/ProfileSeeder.php
+++ database/seeds/ProfileSeeder.php
@@ -16,7 +16,7 @@ class ProfileSeeder extends Seeder
         $this->disableForeignKeys();
 
         $updateStatement = "
-        INSERT INTO `projects` (`id`, `name`, `label`, `description`, `is_private`, `created_at`, `updated_at`, `deleted_at`, `repo`, `url`, `license`, `uri_strategy`, `uri_type`, `uri_prepend`, `uri_append`, `created_by`, `updated_by`, `deleted_by`, `starting_number`, `license_uri`, `default_language_id`, `google_sheet_url`) VALUES 
+        INSERT INTO `projects` (`id`, `name`, `label`, `description`, `is_private`, `created_at`, `updated_at`, `deleted_at`, `repo`, `url`, `license`, `uri_strategy`, `uri_type`, `uri_prepend`, `uri_append`, `created_by`, `updated_by`, `deleted_by`, `starting_number`, `license_uri`, `default_language`, `google_sheet_url`) VALUES 
 	    (58,NULL,'NSDL Registry',NULL,0,'2006-06-05 15:58:02','2006-06-05 22:16:33',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
        ";
         DB::update(DB::raw($updateStatement));
