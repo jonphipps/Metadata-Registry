15fa9dbf94da1e9c4b00e96fbfe363cb6b1e9336
Jon Phipps <jphipps@madcreek.com>
2017-05-21 12:24:39 -0400

    add new fields to reg_agent

--- database/migrations/2017_05_12_230942_create_reg_agent_table.php
+++ database/migrations/2017_05_12_230942_create_reg_agent_table.php
@@ -4,53 +4,55 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRegAgentTable extends Migration {
+class CreateRegAgentTable extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('reg_agent', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->timestamps();
-			$table->timestamp('last_updated')->default(DB::raw('CURRENT_TIMESTAMP'));
-			$table->softDeletes();
-			$table->string('org_email', 100)->default('');
-			$table->string('org_name')->default('');
-			$table->string('ind_affiliation')->nullable();
-			$table->string('ind_role', 45)->nullable();
-			$table->string('address1')->nullable();
-			$table->string('address2')->nullable();
-			$table->string('city', 45)->nullable();
-			$table->char('state', 2)->nullable();
-			$table->string('postal_code', 15)->nullable();
-			$table->char('country', 3)->nullable();
-			$table->string('phone', 45)->nullable();
-			$table->string('web_address')->nullable();
-			$table->char('type', 15)->nullable();
-			$table->string('repo')->nullable();
-			$table->boolean('is_private')->default(0);
-			$table->string('license')->nullable();
-			$table->text('description')->nullable();
-			$table->integer('created_by')->unsigned()->nullable()->index();
-			$table->integer('updated_by')->unsigned()->nullable()->index();
-			$table->integer('deleted_by')->unsigned()->nullable()->index();
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create( 'reg_agent',
+            function( Blueprint $table ) {
+                $table->increments( 'id' );
+                $table->text( 'description' )->nullable();
+                $table->boolean( 'is_private' )->nullable()->default( 0 );
+                $table->string( 'repo' )->nullable();
+                $table->text( 'license' )->nullable();
+//legacy
+                $table->timestamp( 'last_updated' )->default( DB::raw( 'CURRENT_TIMESTAMP' ) );
+                $table->string( 'org_email', 100 )->default( '' );
+                $table->string( 'org_name' )->default( '' );
+                $table->string( 'ind_affiliation' )->nullable();
+                $table->string( 'ind_role', 45 )->nullable();
+                $table->string( 'address1' )->nullable();
+                $table->string( 'address2' )->nullable();
+                $table->string( 'city', 45 )->nullable();
+                $table->char( 'state', 2 )->nullable();
+                $table->string( 'postal_code', 15 )->nullable();
+                $table->char( 'country', 3 )->nullable();
+                $table->string( 'phone', 45 )->nullable();
+                $table->string( 'web_address' )->nullable();
+                $table->char( 'type', 15 )->nullable();
+//users
+                $table->integer( 'created_by' )->unsigned()->nullable()->index();
+                $table->integer( 'updated_by' )->unsigned()->nullable()->index();
+                $table->integer( 'deleted_by' )->unsigned()->nullable()->index();
+                $table->timestamps();
+                $table->softDeletes();
+            } );
+    }
 
-
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('reg_agent');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop( 'reg_agent' );
+    }
 
 }
---
+++ database/migrations/2017_05_21_161444_add_project_fields_reg_agent_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddProjectFieldsRegAgentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table( 'reg_agent', function( Blueprint $table ) {
+            $table->string( 'name' )->nullable();
+            $table->string( 'label' )->nullable();
+            $table->string( 'url' )->nullable();
+            $table->string( 'license_uri' )->nullable();
+            $table->string( 'base_domain' )->nullable();
+            $table->enum( 'namespace_type', [ 'Hash', 'Slash' ] )->nullable();
+            $table->enum( 'uri_strategy', [ 'Numeric', 'snake_case', 'SCREAMING_SNAKE_CASE', 'hyphen-case', 'dot.case', 'camelCase', 'PascalCase' ] )->nullable();
+            $table->string( 'uri_prepend' )->nullable();
+            $table->string( 'uri_append' )->nullable();
+            $table->integer( 'starting_number' )->unsigned()->nullable();
+            $table->string( 'default_language' )->nullable();
+            $table->text( 'languages' )->nullable();
+            $table->text( 'prefixes' )->nullable();
+            $table->string( 'google_sheet_url' )->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
