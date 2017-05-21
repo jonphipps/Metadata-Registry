05dacde62c8d5e927cab686bb259c8e76ff256b1
Jon Phipps <jphipps@madcreek.com>
2017-05-21 17:25:30 -0400

    remove united fields from user table

--- database/migrations/2015_05_12_230942_create_users_table.php
+++ database/migrations/2015_05_12_230942_create_users_table.php
@@ -18,18 +18,12 @@ class CreateUsersTable extends Migration {
 			$table->increments('id');
 			$table->timestamps();
 			$table->softDeletes();
-			$table->timestamp('last_updated')->default(DB::raw('CURRENT_TIMESTAMP'));
 			$table->string('nickname', 60)->nullable();
 			$table->string('salutation', 5)->nullable();
 			$table->string('first_name', 125)->nullable();
 			$table->string('last_name', 125)->nullable();
 			$table->string('email', 100)->nullable();
-			$table->string('sha1_password', 40)->nullable();
-			$table->string('salt', 32)->nullable();
-			$table->boolean('want_to_be_moderator')->nullable()->default(0);
-			$table->boolean('is_moderator')->nullable()->default(0);
 			$table->boolean('is_administrator')->nullable()->default(0);
-			$table->integer('deletions')->nullable()->default(0);
 			$table->string('password')->nullable();
 			$table->boolean('status')->default(1);
 			$table->string('culture', 7)->nullable()->default('en_US');
