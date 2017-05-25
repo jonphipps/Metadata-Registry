54ec05e42c4fc32210d3900d734d0986e90d9c42
Jon Phipps <jphipps@madcreek.com>
2017-05-25 12:20:06 -0400

    add dd command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,8 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Spatie\ArtisanDd\DdCommand;
+use Spatie\MigrateFresh\Commands\MigrateFresh;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,7 +15,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        \Spatie\MigrateFresh\Commands\MigrateFresh::class,
+        MigrateFresh::class,
+        DdCommand::class,
 
     ];
 
