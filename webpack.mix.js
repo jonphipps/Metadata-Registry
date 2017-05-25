c228eb271410bfa3846f763c010e9ea9bff5273d
Jon Phipps <jphipps@madcreek.com>
2017-05-25 10:08:08 -0400

    add the smart wizard themes

--- webpack.mix.js
+++ webpack.mix.js
@@ -32,6 +32,7 @@ mix.sass('resources/assets/sass/frontend/app.scss', 'web/css/frontend.css')
     })
 mix.copy('node_modules/smartwizard/dist/js/jquery.smartWizard.min.js', 'web/js/frontend/jquery.smartWizard.min.js');
 mix.copy('node_modules/smartwizard/dist/css/smart_wizard.min.css', 'web/css/frontend/smart_wizard.css');
+mix.copy('node_modules/smartwizard/dist/css/smart_wizard_theme_dots.min.css', 'web/css/frontend/smart_wizard_theme_dots.css');
 
 if(mix.config.inProduction){
     mix.version();
