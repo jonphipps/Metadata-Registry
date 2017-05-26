4706408bb8f7642aa49cbfdb3c0e72e4073d9025
Jon Phipps <jphipps@madcreek.com>
2017-05-26 13:26:42 -0400

    align the ProjectUser class/table (agent_has_user) with other member tables

---
+++ database/migrations/2017_05_26_152250_add_project_user_fields_to_reg_agent_has_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddProjectUserFieldsToRegAgentHasUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table( 'reg_agent_has_user',
+            function( Blueprint $table ) {
+                $table->boolean( 'is_maintainer_for' )->nullable()->default( 1 );
+                $table->text( 'languages' )->nullable();
+                $table->char( 'default_language', 6 )->default( 'en' );
+                $table->char( 'current_language', 6 )->nullable();
+            } );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
