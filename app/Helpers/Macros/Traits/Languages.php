a609eedb97e529634171ed24f1d47d8058fbc9c2
Jon Phipps <jphipps@madcreek.com>
2017-05-26 13:24:47 -0400

    move language attributes into languages trait

--- app/Helpers/Macros/Traits/Languages.php
+++ app/Helpers/Macros/Traits/Languages.php
@@ -7,13 +7,33 @@ use Cache;
 
 trait Languages
 {
-  public static function list($language = '')
-  {
-    foreach (unserialize(file_get_contents(base_path("data/symfony/i18n/en.dat"), "r"))['Languages'] as $key => $value)
+    public static function list( $language = '' )
     {
-      Cache::forever('language_'.$key, $value[0]);
+        $languages =
+            unserialize( file_get_contents( base_path( 'data/symfony/i18n/en.dat' ), "r" ),
+                [ true ] )['Languages'];
+        foreach ( $languages as $key => $value ) {
+            Cache::forever( 'language_' . $key, $value[0] );
+        }
+
+        return $language ? cache::get( 'language_' . $language ) : '';
     }
 
-    return $language ? cache::get('language_'.$language) : '';
-  }
+    public function getLanguageAttribute( $value )
+    {
+        return Cache::get( 'language_' . $value,
+            function() use ( $value ) {
+                return Languages::list( $value );
+            } );
+    }
+
+    public function getDefaultLanguageAttribute( $value )
+    {
+        return $this->getLanguageAttribute( $value );
+    }
+
+    public function getCurrentLanguageAttribute( $value )
+    {
+        return $this->getLanguageAttribute( $value );
+    }
 }
