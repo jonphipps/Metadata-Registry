80ba02ccd3ce8e30c7e749e8327387ed8c67f666
Jon Phipps <jphipps@madcreek.com>
2017-05-28 14:09:24 -0400

    add hint to not warn about autoload

--- database/migrations/2015_05_12_230942_create_users_table.php
+++ database/migrations/2015_05_12_230942_create_users_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+/** @noinspection AutoloadingIssuesInspection */
+/** @noinspection AutoloadingIssuesInspection */
 class CreateUsersTable extends Migration {
 
 	/**
