b99781c445703af7b928e8dc652bbdd8367505ec
Jon Phipps <jphipps@madcreek.com>
2017-05-28 15:19:42 -0400

    add a note to the languages migration file

--- database/migrations/2017_05_12_230942_create_languages_table.php
+++ database/migrations/2017_05_12_230942_create_languages_table.php
@@ -1,5 +1,9 @@
 <?php
-
+/**
+ * Support file for Laravel-Backpack/LangFileManager
+ * https://github.com/Laravel-Backpack/LangFileManager
+ * 
+ */
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
