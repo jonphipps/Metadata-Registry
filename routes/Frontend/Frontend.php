7f8e1726b116c6d891917eb06f3cf7848f43d6f3
Jon Phipps <jphipps@madcreek.com>
2017-05-31 09:12:09 -0400

    add import controller, routes

--- routes/Frontend/Frontend.php
+++ routes/Frontend/Frontend.php
@@ -8,15 +8,9 @@ Route::get('/', 'FrontendController@index')->name('index');
 Route::get('macros', 'FrontendController@macros')->name('macros');
 CRUD::resource('projects', 'ProjectCrudController', [ 'except' => [ 'show' ] ])->with(function() {
     // add extra routes to this resource
-    Route::get('projects/{project}/{type}/{step?}', 'ProjectImportController@importStep')->name('project.import');
-    Route::post('projects/{project}/{id}/{step?}', 'ProjectImportController@processImportStep')->name('project.import.process');
+    Route::get('projects/{project}/import/{type}/{step?}', 'ImportCrudController@importProject')->name('project.import');
+    Route::post('projects/{project}/import/{type}/{step?}', 'ImportCrudController@processImportProject')->name('project.import.process');
 });
-Route::group([],
-    function() {
-        // Flow controller
-        Route::get('flows/{id}/{step?}', [ 'as' => 'flows.step', 'uses' => '\Ixudra\Wizard\Http\Controllers\FlowController@step' ]);
-        Route::post('flows/{id}/{step?}', [ 'as' => 'flows.step.process', 'uses' => '\Ixudra\Wizard\Http\Controllers\FlowController@processStep' ]);
-    });
 /* ----------------------------------------------------------------------- */
 
 Route::get('projects/{project}', 'ProjectController@show')->name('project.show');
