098a6ebd7e47181309bf72afd6fd3f922bcd473e
Jon Phipps <jphipps@madcreek.com>
2017-06-07 18:30:54 -0400

    latest laravel 5.4.25 changes

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -8,7 +8,7 @@ window._ = require('lodash');
  */
 
 try {
-global.$ = global.jQuery = require('jquery');
+    window.$ = window.jQuery = require('jquery');
 
 require('bootstrap-sass');
 } catch (e) {}
