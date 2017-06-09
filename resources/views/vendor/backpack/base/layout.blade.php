ef31dd6d08bafe89f6e60e5cc7745377616de266
Jon Phipps <jphipps@madcreek.com>
2017-06-09 13:25:30 -0400

    remove comments

--- resources/views/vendor/backpack/base/layout.blade.php
+++ resources/views/vendor/backpack/base/layout.blade.php
@@ -27,21 +27,6 @@
         {{ Html::style(mix('css/frontend.css')) }}
     @endif
     {{ Html::style(mix('css/all.css')) }}
-<!-- Bootstrap 3.3.5
-    <link rel="stylesheet" href="{{ asset('vendor/adminlte/') }}/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <link rel="stylesheet" href="{{ asset('vendor/adminlte/dist/css/AdminLTE.min.css') }}">
- -->
-<!-- AdminLTE Skins. Choose a skin from the css/skins folder instead of downloading all of them to reduce the load.
-    <link rel="stylesheet" href="{{ asset('vendor/adminlte/dist/css/skins/skin-blue.min.css') }}">
-
-    <link rel="stylesheet" href="{{ asset('vendor/adminlte/plugins/pace/pace.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('vendor/backpack/pnotify/pnotify.custom.min.css') }}">
- -->
-<!-- BackPack Base CSS
-    <link rel="stylesheet" href="{{ asset('vendor/backpack/backpack.base.css') }}">
-    -->
     @yield('after_styles')
 
 <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -94,17 +79,6 @@
 {!! Html::script(mix('js/vendor.js')) !!}
 {!! Html::script(mix('js/frontend.js')) !!}
 
-<!-- jQuery 2.2.0
-    <script src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
-    <script>window.jQuery || document.write('<script src="{{ asset('vendor/adminlte') }}/plugins/jQuery/jQuery-2.2.0.min.js"><\/script>')</script>
--->
-<!-- Bootstrap 3.3.5
-    <script src="{{ asset('vendor/adminlte') }}/bootstrap/js/bootstrap.min.js"></script>
-    <script src="{{ asset('vendor/adminlte') }}/plugins/pace/pace.min.js"></script>
-    <script src="{{ asset('vendor/adminlte') }}/plugins/slimScroll/jquery.slimscroll.min.js"></script>
-    <script src="{{ asset('vendor/adminlte') }}/plugins/fastclick/fastclick.js"></script>
-    <script src="{{ asset('vendor/adminlte') }}/dist/js/app.min.js"></script>
-    -->
 @include('backpack::inc.alerts')
 
 @yield('after_scripts')
