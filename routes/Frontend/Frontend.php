681a0e7281837e5ef44d1d6e48ae7b8e0def4709
Jon Phipps <jphipps@madcreek.com>
2017-06-13 14:24:55 -0400

    refactor project routes

--- routes/Frontend/Frontend.php
+++ routes/Frontend/Frontend.php
@@ -6,14 +6,16 @@
  */
 Route::get('/', 'FrontendController@index')->name('index');
 Route::get('macros', 'FrontendController@macros')->name('macros');
-CRUD::resource('projects', 'ProjectCrudController', [ 'except' => [ 'show' ] ])->with(function() {
-    // add extra routes to this resource
-    Route::get('projects/{project}/import/{step?}', 'ImportCrudController@importProject')->name('project.import');
-    Route::post('projects/{project}/import/{step}', 'ImportCrudController@processImportProject')->name('project.import.post');
-});
-/* ----------------------------------------------------------------------- */
+Route::group([ 'namespace' => 'Project' ],
+    function() {
+        CRUD::resource('projects', 'ProjectCrudController');
 
-Route::get('projects/{project}', 'ProjectController@show')->name('project.show');
+        //Route::get('projects/{project}', 'ProjectController@show')->name('project.show');
+    });
+        /* ----------------------------------------------------------------------- */
+Route::get('projects/{project}/imports/create', 'ImportCrudController@importProject')->name('project.import.create');
+Route::get('projects/{project}/import/{step?}', 'ImportCrudController@importProject')->name('project.import');
+Route::post('projects/{project}/import/{step}', 'ImportCrudController@processImportProject')->name('project.import.post');
 /*
  * These frontend controllers require the user to be logged in
  * All route names are prefixed with 'frontend.'
