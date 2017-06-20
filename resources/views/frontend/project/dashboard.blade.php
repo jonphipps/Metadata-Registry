41df222af91da3117431264113c0289d8d1406e1
Jon Phipps <jphipps@madcreek.com>
2017-06-20 16:30:42 -0400

    add imports table to dashboard

--- resources/views/frontend/project/dashboard.blade.php
+++ resources/views/frontend/project/dashboard.blade.php
@@ -3,6 +3,7 @@
 /** @var \App\Models\Project $project */
 /** @var \App\Http\Controllers\Frontend\Vocabulary\VocabularyCrudController $vocabulary */
 /** @var \App\Models\Import $import */
+/** @var \App\Models\Batch $batch */
 ?>
 @section('content')
     <div class="row">
@@ -64,7 +65,7 @@
                                         @endforeach
                                     </dl>
                                     @can('edit', $project)
-                                        <a class="btn btn-default btn-sm pull-right" href="{{route('frontend.crud.projects.edit', ['id' => $project->id])}}" role="button">Edit</a>
+                                        <a class="btn btn-default btn-sm pull-right" href="{{route('frontend.crud.projects.edit', ['id' => $project->id])}}">Edit</a>
                                     @endcan
                                 </div><!--panel-body-->
                             </div><!--panel-->
@@ -97,11 +98,35 @@
                                 @include('frontend.partials.panelheader', ['crud' => $import->crud, 'policy_model' => $project, 'permission' =>'edit' ])<!--panel-heading-->
                                 <div class="panel-body">
                                     <ul class="list-unstyled">
-                                        @forelse ($project->imports->sortBy('date') as $import)
-                                            <li>{{ laravel_link_to('imports/'.$import->id . '/elements', $import->source_file_name) }}</li>
-                                        @empty
-                                            No Project Imports yet
-                                        @endforelse
+                                            <table class="table table-bordered table-striped table-hover table-condensed table-responsive">
+                                                <thead>
+                                                <tr><th>Import</th><th>Started at</th><th>Next step</th><th>Action</th></tr>
+                                                </thead>
+                                                <tbody>
+                                                @forelse ($project->importBatches->sortBy('created_at') as $batch)
+                                                    <tr>
+                                                    <td>
+                                                        @can('edit', $project)
+                                                            {{ laravel_link_to('projects/' . $project->id . '/imports/' . $batch->id . '/'. $batch->next_step , $batch->run_description) }}
+                                                        @else
+                                                            {{$batch->run_description}}
+                                                        @endcan
+                                                    </td>
+                                                    <td>
+                                                        {{ $batch->created_at }}
+                                                    </td>
+                                                    <td>
+                                                        {{ $batch->next_step }}
+                                                    </td>
+                                                    <td>
+                                                        {{ '<link to results>' }}
+                                                    </td>
+                                                </tr>
+                                                @empty
+                                                    <tr><td colspan="4">No Project Imports yet</td></tr>
+                                                @endforelse
+                                                </tbody>
+                                            </table>
                                     </ul>
                                 </div><!--panel-body-->
                             </div><!--panel-->
