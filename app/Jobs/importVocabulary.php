f09f5ea045fea57a938ecad0bfae8f7222d77130
Jon Phipps <jphipps@madcreek.com>
2017-07-26 14:42:30 -0400

    refactor import job

--- app/Jobs/importVocabulary.php
+++ app/Jobs/importVocabulary.php
@@ -5,6 +5,7 @@ namespace App\Jobs;
 use App\Models\Concept;
 use App\Models\Element;
 use App\Models\Import;
+use Exception;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Database\Eloquent\Collection;
@@ -25,17 +26,23 @@ class ImportVocabulary implements ShouldQueue
     /**
      * Create a new job instance.
      *
-     * @param Import $import
+     * @param int $import
      */
-    public function __construct(Import $import)
+    public function __construct(int $importId)
     {
-        //
-        $this->import = $import;
+        $this->import = Import::find($importId);
     }
 
     /**
      * Execute the job.
-     *
+     *   step through the changeset and change/add the statements
+     *   each row of the changeset is a resource
+     *   if the row has an id then we're updating it
+     *   if there's a statement id for a column, we're updating it
+     *   if we delete a row, we have to delete all of the statements
+     *   changing the statements should trigger an addition to the history table for each one
+     *   update each resource base model with data from the statements
+ *
      * @return void
      * @throws \Throwable
      * @throws \Exception
@@ -44,8 +51,10 @@ class ImportVocabulary implements ShouldQueue
     {
         //start a transaction
         DB::transaction(function() {
+            $timer = new \DateTime();
             $vocabId   = $this->isElementSet()? $this->import->schema_id: $this->import->vocabulary_id;
             $changeset = $this->import->instructions;
+            $total_processed = 0;
             //each item in the main array is a row. Each item in the statements array is a statement
             foreach ($changeset['update'] as $reg_id => $row) {
                 $statements = $this->getStatements($reg_id);
@@ -66,6 +75,7 @@ class ImportVocabulary implements ShouldQueue
                     }
                 }
                 $this->resource->fresh([ 'statements' ])->updateFromStatements();
+                $total_processed++;
             }
             foreach ($changeset['add'] as $row) {
                 $resource = $this->createResource($vocabId);
@@ -73,15 +83,26 @@ class ImportVocabulary implements ShouldQueue
                     $this->addStatement($statement, $resource->id);
                 }
                 $this->resource->fresh([ 'statements' ])->updateFromStatements();
+                $total_processed++;
             }
-            //step through the changeset and change/add the statements
-            // each row of the changeset is a resource
-            //  if the row has an id then we're updating it
-            //  if there's a statement id for a column, we're updating it
-            // if we delete a row, we have to delete all of the statements
-            //changing the statements should trigger an addition to the history table for each one
-            //update each resource base model with data from the statements
+            foreach ($changeset['delete'] as $row) {
+                //TODO implement this...
+                //delete the resource
+                //cascade delete the statements, which should cascade delete the reciprocals
+                $total_processed++;
+            }
+            $this->import->results =
+                'Your file has been imported. It took us: ' .
+                $timer->diff(new \DateTime())->format('%h hours; %i minutes; %s seconds');
+            $this->import->batch->increment('handled_count');
+            $this->import->total_processed_count = $total_processed;
+            $this->import->imported_at = new \DateTime();
         });
+    }
+
+    public function failed(Exception $exception)
+    {
+        //report a failed import
     }
 
     private function getStatements(int $reg_id): ?Collection
