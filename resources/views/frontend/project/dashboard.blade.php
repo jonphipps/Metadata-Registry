eba1e80b7bdb57c6974b5f3fee91dcc4e529365e
Jon Phipps <jphipps@madcreek.com>
2017-09-29 16:35:08 -0400

    remove the dead last column from import list

--- resources/views/frontend/project/dashboard.blade.php
+++ resources/views/frontend/project/dashboard.blade.php
@@ -96,15 +96,18 @@
                         <div class="col-md-6">
                             <div class="panel panel-default">
                                 @include('frontend.partials.panelheader', ['crud' => $import->crud, 'policy_model' => $project, 'permission' =>'edit' ])<!--panel-heading-->
-                                <div class="panel-body">
-                                    <ul class="list-unstyled">
-                                            <table class="table table-bordered table-striped table-hover table-condensed table-responsive">
-                                                <thead>
-                                                <tr><th>Import</th><th>Started at</th><th>Next step</th><th>Action</th></tr>
-                                                </thead>
-                                                <tbody>
-                                                @forelse ($project->importBatches->sortBy('created_at') as $batch)
-                                                    <tr>
+                                    <div class="panel-body">
+                                        <table class="table table-bordered table-striped table-hover table-condensed table-responsive">
+                                            <thead>
+                                            <tr>
+                                                <th>Import</th>
+                                                <th>Started at</th>
+                                                <th>Next step</th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            @forelse ($project->importBatches->sortBy('created_at') as $batch)
+                                                <tr>
                                                     <td>
                                                         @can('edit', $project)
                                                             {{ laravel_link_to('projects/' . $project->id . '/imports/' . $batch->id . '/'. $batch->next_step , $batch->run_description) }}
@@ -116,19 +119,17 @@
                                                         {{ $batch->created_at }}
                                                     </td>
                                                     <td>
-                                                        {{ $batch->next_step }}
-                                                    </td>
-                                                    <td>
-                                                        {{ '<link to results>' }}
+                                                        {{ laravel_link_to('projects/' . $project->id . '/imports/' . $batch->id . '/'. $batch->next_step , $batch->next_step) }}
                                                     </td>
                                                 </tr>
-                                                @empty
-                                                    <tr><td colspan="4">No Project Imports yet</td></tr>
-                                                @endforelse
-                                                </tbody>
-                                            </table>
-                                    </ul>
-                                </div><!--panel-body-->
+                                            @empty
+                                                <tr>
+                                                    <td colspan="4">No Project Imports yet</td>
+                                                </tr>
+                                            @endforelse
+                                            </tbody>
+                                        </table>
+                                    </div><!--panel-body-->
                             </div><!--panel-->
                         </div><!--col-md-6-->
                     </div><!--row-->
