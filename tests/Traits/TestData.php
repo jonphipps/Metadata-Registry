2058cb5b28b638cfb0fa69ccf7465e5b811eee3a
Jon Phipps <jphipps@madcreek.com>
2017-10-12 14:37:43 -0400

    adjust testdata to match test database

--- tests/Traits/TestData.php
+++ tests/Traits/TestData.php
@@ -12,17 +12,17 @@ trait TestData
         $testData['project']['adminId']          = 422;
         $testData['project']['id']               = 177;
         $testData['project']['title']            = 'ALA Publishing';
-        $testData['vocabulary']['id']            = 437;
-        $testData['vocabulary']['title']         = 'RDA Material';
-        $testData['vocabulary']['importId']      = 866;
-        $testData['vocabulary']['exportId']      = 625;
-        $testData['vocabulary']['maintainerId']  = 500;
-        $testData['concept']['id']               = 8455;
-        $testData['concept']['preferredLabelId'] = 33545;
-        $testData['concept']['title']            = 'acetate';
+        $testData['vocabulary']['id']            = 37;
+        $testData['vocabulary']['title']         = 'RDA Media Type';
+        $testData['vocabulary']['importId']      = 456;
+        $testData['vocabulary']['exportId']      = 571;
+        $testData['vocabulary']['maintainerId']  = 32;
+        $testData['concept']['id']               = 475;
+        $testData['concept']['preferredLabelId'] = 1281;
+        $testData['concept']['title']            = 'audio';
         $testData['elementSet']['id']            = 83;
         $testData['elementSet']['title']         = 'RDA Classes';
-        $testData['elementSet']['importId']      = 1053;
+        $testData['elementSet']['importId']      = 451;
         $testData['import']['validSpreadsheetUri']    ='https://docs.google.com/spreadsheets/d/1WTxiOvHHUurz76NZ0WU_2GjjY4SG8Gzbg0vH8xwNz_I/edit#gid=0';
 
         return $testData;
