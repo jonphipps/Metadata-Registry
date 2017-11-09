1954a72b5b6ec777b6c447d879e21802db17f405
Jon Phipps <jphipps@madcreek.com>
2017-11-09 14:31:53 -0500

    correct namespace for tests

--- tests/Feature/ProjectManagementTest.php
+++ tests/Feature/ProjectManagementTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Unit;
+namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\BrowserKitTestCase;
--- tests/Feature/ViewProjectListTest.php
+++ tests/Feature/ViewProjectListTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Unit;
+namespace Tests\Feature;
 
 use Tests\BrowserKitTestCase;
 use Tests\Feature\Traits\Authorize;
