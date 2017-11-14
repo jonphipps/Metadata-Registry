44b54768c289fa8d16b0a4b85b8dbc120f539eba
Jon Phipps <jphipps@madcreek.com>
2017-11-14 15:50:26 -0500

    add rdf generator test

--- database/seeds/Testing/sql/RDAPrefix.sql
+++ database/seeds/Testing/sql/RDAPrefix.sql
@@ -2,4 +2,7 @@ INSERT INTO `reg_prefix` (`prefix`, `uri`, `rank`) VALUES
 	('dc','http://purl.org/dc/elements/1.1/',5),
 	('foaf','http://xmlns.com/foaf/0.1/',3),
 	('rdf','http://www.w3.org/1999/02/22-rdf-syntax-ns#',2),
-	('skos','http://www.w3.org/2004/02/skos/core#',11);
+	('skos','http://www.w3.org/2004/02/skos/core#',11),
+	('rdakit','http://metadataregistry.org/uri/profile/rdakit/',1501),
+	('rdfs','http://www.w3.org/2000/01/rdf-schema#',7),
+	('reg','http://metadataregistry.org/uri/profile/regap/',1500);
---
+++ tests/Feature/OMR/RDFGeneratorTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Feature\OMR;
+
+use Google\Auth\HttpHandler\Guzzle6HttpHandler;
+use GuzzleHttp\Client;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Spatie\Snapshots\MatchesSnapshots;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class RDFGeneratorTest extends TestCase
+{
+    use MatchesSnapshots;
+
+    public function setUp(): void
+    {
+        $this->dontSetupDatabase();
+        parent::setUp();
+    }
+
+    /** @test */
+    public function it_creates_a_file(): void
+    {
+        Storage::fake('repos');
+        $file = UploadedFile::fake()->create('foobar.xml');
+        $upload = Storage::disk('repos')->putFileAs('', $file, $file->name);
+        Storage::disk('repos')->assertExists($upload);
+    }
+
+   /** @test */
+    public function it_creates_an_rdf_file_for_download(): void
+    {
+        $client = new Client();
+        $res = $client->get(url('vocabularies/37.rdf'));
+        $this->assertMatchesXmlSnapshot((string) $res->getBody());
+        $this->assertSame(['attachment; filename="RDAMediaType.xml"'], $res->getHeader('Content-Disposition'));
+
+        $res = $client->get(url('elementsets/83.rdf'));
+        $this->assertMatchesXmlSnapshot((string) $res->getBody());
+        $this->assertSame([ 'attachment; filename="c.xml"' ], $res->getHeader('Content-Disposition'));
+
+        $res = $client->get(url('vocabularies/37/concepts/475.rdf'));
+        $this->assertMatchesXmlSnapshot((string) $res->getBody());
+        $this->assertArrayNotHasKey('Content-Disposition', $res->getHeaders());
+
+        $res = $client->get(url('elementsets/83/elements/14329.rdf'));
+        $this->assertMatchesXmlSnapshot((string) $res->getBody());
+        $this->assertArrayNotHasKey('Content-Disposition', $res->getHeaders());
+
+        Storage::fake('repos');
+        $file = UploadedFile::fake()->create('foobar.xml');
+        $upload = Storage::disk('repos')->putFileAs('', $file, $file->name);
+        Storage::disk('repos')->assertExists($upload);
+    }
+
+}
