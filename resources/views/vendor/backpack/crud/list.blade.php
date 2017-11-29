48da5efd2f09242bbe5f7ca39941677d484de45a
Jon Phipps <jphipps@madcreek.com>
2017-11-29 16:48:46 -0500

    remove all non-changed views and revert to default backpack

--- resources/views/vendor/backpack/crud/list.blade.php
+++ resources/views/vendor/backpack/crud/list.blade.php
@@ -40,7 +40,7 @@
           @include('crud::inc.filters_navbar')
         @endif
 
-        <table id="crudTable" class="table table-bordered table-striped display">
+        <table id="crudTable" class="table table-striped table-hover display">
             <thead>
               <tr>
                 @if ($crud->details_row)
@@ -61,39 +61,7 @@
             </thead>
             <tbody>
 
-              @if (!$crud->ajaxTable())
-                @foreach ($entries as $k => $entry)
-                <tr data-entry-id="{{ $entry->getKey() }}">
-@cache($entry)
-                  @if ($crud->details_row)
-                    @include('crud::columns.details_row_button')
-                  @endif
 
-                  {{-- load the view from the application if it exists, otherwise load the one in the package --}}
-                  @foreach ($crud->columns as $column)
-                    @if (!isset($column['type']))
-                      @include('crud::columns.text')
-                    @else
-                      @if(view()->exists('vendor.backpack.crud.columns.'.$column['type']))
-                        @include('vendor.backpack.crud.columns.'.$column['type'])
-                      @else
-                        @if(view()->exists('crud::columns.'.$column['type']))
-                          @include('crud::columns.'.$column['type'])
-                        @else
-                          @include('crud::columns.text')
-                        @endif
-                      @endif
-                    @endif
-                  @endforeach
-@endcache
-                  @if ($crud->buttons->where('stack', 'line')->count())
-                    <td>
-                      @include('crud::inc.button_stack', ['stack' => 'line'])
-                    </td>
-                  @endif
-                </tr>
-                @endforeach
-              @endif
 
             </tbody>
             <tfoot>
@@ -208,17 +176,22 @@
               "aria": {
                   "sortAscending":  "{{ trans('backpack::crud.aria.sortAscending') }}",
                   "sortDescending": "{{ trans('backpack::crud.aria.sortDescending') }}"
-              }
+              },
+              "buttons": {
+                  "copy":   "{{ trans('backpack::crud.export.copy') }}",
+                  "excel":  "{{ trans('backpack::crud.export.excel') }}",
+                  "csv":    "{{ trans('backpack::crud.export.csv') }}",
+                  "pdf":    "{{ trans('backpack::crud.export.pdf') }}",
+                  "print":  "{{ trans('backpack::crud.export.print') }}",
+                  "colvis": "{{ trans('backpack::crud.export.column_visibility') }}"
           },
-
-          @if ($crud->ajaxTable())
+          },
           "processing": true,
           "serverSide": true,
           "ajax": {
               "url": "{!! url($crud->route.'/search').'?'.Request::getQueryString() !!}",
               "type": "POST"
           },
-          @endif
 
           @if ($crud->exportButtons())
           // show the export datatable buttons
