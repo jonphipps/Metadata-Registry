b90562a7f4830e2ec2c409f8bcc0ee31a9f57a56
Jon Phipps <jphipps@madcreek.com>
2017-12-04 14:19:01 -0500

    test correct storage path and content

--- tests/Feature/OMR/RDFGeneratorServiceTest.php
+++ tests/Feature/OMR/RDFGeneratorServiceTest.php
@@ -3,8 +3,6 @@
 namespace Tests\Feature\OMR;
 
 use App\Jobs\GenerateRdf;
-use GuzzleHttp\Client;
-use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 use Spatie\Snapshots\MatchesSnapshots;
 use Tests\TestCase;
@@ -20,10 +18,14 @@ class RDFGeneratorServiceTest extends TestCase
     }
 
     /** @test */
-    public function it_creates_a_new_job(): void
+    public function it_creates_a_new_job_and_stores_xml(): void
     {
         $job = new GenerateRdf(GenerateRdf::VOCABULARY, 37);
         dispatch($job);
+        $this->assertSame(storage_path('repos/projects/177/xml/termList/RDAMediaType.xml'),
+            $job->getStoragePath('xml'));
+        $file = Storage::get($job->getStoragePath('xml'));
+        $this->assertMatchesXmlSnapshot($file);
     }
 
 }
