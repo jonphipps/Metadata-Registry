57c08243b4b58f60e8e67cb8ba1f00f7e076341e
Jon Phipps <jphipps@madcreek.com>
2017-12-07 09:35:42 -0500

    Make sure we have a symfony environment setup for importupdate test suite

--- tests/importUpdate/_bootstrap.php
+++ tests/importUpdate/_bootstrap.php
@@ -1,4 +1,23 @@
 <?php
 // Here you can initialize variables that will be available to your tests
 use Codeception\Util\Fixtures;
-Fixtures::add('importFolder', '/var/www/registry/tests/_data/');
+Fixtures::add('importFolder', getcwd().'/tests/_data/');
+
+if ( ! defined('SF_APP')) {
+    define('SF_APP', 'frontend');
+    define('SF_ENVIRONMENT',  'dev');
+    define('SF_DEBUG', 'true');
+}
+if ( ! defined('SF_ROOT_DIR')) {
+    define('SF_ROOT_DIR', getcwd().'/');
+}
+/** @noinspection PhpIncludeInspection */
+require_once SF_ROOT_DIR . 'apps/frontend/config/config.php';
+
+//make sure we have a fresh instance since it's never an internal symfony forward
+if (sfContext::hasInstance()) {
+    sfContext::removeInstance();
+}
+
+return sfContext::getInstance();
+
