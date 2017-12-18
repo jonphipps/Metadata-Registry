ca4c94a0d6c24e99318c99f00cb666a258cb7f19
Jon Phipps <jphipps@madcreek.com>
2017-12-18 15:42:05 -0500

    reset the example vue component to only use frontend component

--- resources/assets/js/components/ExampleComponent.vue
+++ resources/assets/js/components/ExampleComponent.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-xs-12">
                 <div class="panel panel-default">
                     <div class="panel-heading">Example Component</div>
 
                     <div class="panel-body">
-                        I'm an example component!
+                        I'm an example component from ExampleComponent.vue!!
                     </div>
                 </div>
             </div>
@@ -17,7 +17,7 @@
 <script>
     export default {
         mounted() {
-            console.log('Component mounted.')
+            console.log('ExampleComponent mounted.')
         }
     }
 </script>
