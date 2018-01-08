1e3bf75ca913b8fe50ec611c06cd0ac0d1720290
Jon Phipps <jphipps@madcreek.com>
2018-01-08 12:38:59 -0500

    add upate_authorized to data fizes

--- routes/Backend/DataFixes.php
+++ routes/Backend/DataFixes.php
@@ -5,6 +5,7 @@
  */
 
 use App\Models\Project;
+use App\Models\ProjectUser;
 use App\Models\Vocabulary;
 
 Route::get('fixprojectlanguages', function(){
@@ -14,11 +15,6 @@ Route::get('fixprojectlanguages', function(){
     foreach ($agents as $agent) {
         $languages[$agent->id]['languages'] = $agent->languages;
         /** @var Vocabulary $vocabulary */
-        // foreach ($agent->vocabularies as $vocabulary) {
-        //     if ($vocabulary->languages) {
-        //         $languages[ $agent->id ] += $vocabulary->languages;
-        //     }
-        // }
         foreach ($agent->vocabularies as $vocabulary) {
             if ($vocabulary->languages) {
                 $languages[ $agent->id ]['languages'] += $vocabulary->languages;
@@ -36,4 +32,13 @@ Route::get('fixprojectlanguages', function(){
         // $languages[ $agent->id ]['count'] = count($languages[ $agent->id ]['languages']);
     }
     return $agents;
+});
+Route::get('update_authorized_as', function(){
+    //$agents = Project::with('members', 'vocabularies.members', 'elementsets.members')->find(177);
+    ProjectUser::where('is_maintainer_for', 1)->where('is_admin_for', 0)->update(['authorized_as' => 1]);
+    // $projectUsers = ProjectUser::with('user', 'project.vocabularies.members', 'project.elementsets.members')->where('agent_id',177)->first();
+    $projectUsers = ProjectUser::get();
+
+    return $projectUsers->where('authorized_as',1);
+
 });
