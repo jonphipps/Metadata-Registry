442bf7f310dd33cf50622542c51cf632a025ad80
Jon Phipps <jphipps@madcreek.com>
2018-01-30 18:50:12 -0500

    ask for scopes with github login

--- config/services.php
+++ config/services.php
@@ -68,7 +68,7 @@ return [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
         'redirect' => env('GITHUB_REDIRECT'),
-        'scopes' => [],
+        'scopes' => ['repo', 'admin:repo_hook', 'read:org', 'user:email'],
         'with' => [],
     ],
 
