b9a85bc77f737218869439d85215338c14c1b526
Jon Phipps <jphipps@madcreek.com>
2018-02-02 17:29:27 -0500

    add fav_icon reset

--- resources/views/vendor/backpack/base/inc/header.blade.php
+++ resources/views/vendor/backpack/base/inc/header.blade.php
@@ -7,6 +7,7 @@
     <title>
         {{ isset($title) ? ' The Registry! :: ' . $title: 'The Registry!'}}
     </title>
+    <link rel="shortcut icon" href="/favicon.ico?v=2"/>
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
