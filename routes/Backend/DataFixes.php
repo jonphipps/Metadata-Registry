132aa29bff0bfc1600c14980c8355954c7eda4af
Jon Phipps <jphipps@madcreek.com>
2018-02-12 09:29:01 -0500

    add rda prefix hack

--- routes/Backend/DataFixes.php
+++ routes/Backend/DataFixes.php
@@ -3,6 +3,7 @@
 /**
  * All route names are prefixed with 'admin.'.
  */
+
 use App\Jobs\SyncProduction;
 use App\Models\Elementset;
 use App\Models\Project;
@@ -122,3 +123,88 @@ Route::get('check_github', function () {
         return '<table>'.$e->xdebug_message. '</table>';
     }
 });
+Route::get('hack_rda_prefix',
+    function () {
+        $rdaPrefixes   = [
+            'rdaar'   => 'http://rdaregistry.info/termList/AspectRatio/',
+            'rdabf'   => 'http://rdaregistry.info/termList/bookFormat/',
+            'rdabs'   => 'http://rdaregistry.info/termList/broadcastStand/',
+            'rdacarx' => 'http://rdaregistry.info/termList/RDACarrierEU/',
+            'rdacc'   => 'http://rdaregistry.info/termList/RDAColourContent/',
+            'rdacct'  => 'http://rdaregistry.info/termList/CollTitle/',
+            'rdacdt'  => 'http://rdaregistry.info/termList/RDACartoDT/',
+            'rdaco'   => 'http://rdaregistry.info/termList/RDAContentType/',
+            'rdacpc'  => 'http://rdaregistry.info/termList/configPlayback/',
+            'rdact'   => 'http://rdaregistry.info/termList/RDACarrierType/',
+            'rdafmn'  => 'http://rdaregistry.info/termList/MusNotation/',
+            'rdafnm'  => 'http://rdaregistry.info/termList/formatNoteMus/',
+            'rdafnv'  => 'http://rdaregistry.info/termList/noteMove/',
+            'rdafr'   => 'http://rdaregistry.info/termList/frequency/',
+            'rdafs'   => 'http://rdaregistry.info/termList/fontSize/',
+            'rdaft'   => 'http://rdaregistry.info/termList/fileType/',
+            'rdaftn'  => 'http://rdaregistry.info/termList/TacNotation/',
+            'rdagen'  => 'http://rdaregistry.info/termList/RDAGeneration/',
+            'rdagrp'  => 'http://rdaregistry.info/termList/groovePitch/',
+            'rdagw'   => 'http://rdaregistry.info/termList/grooveWidth/',
+            'rdaill'  => 'http://rdaregistry.info/termList/IllusContent/',
+            'rdalay'  => 'http://rdaregistry.info/termList/layout/',
+            'rdamat'  => 'http://rdaregistry.info/termList/RDAMaterial/',
+            'rdami'   => 'http://rdaregistry.info/termList/ModeIssue/',
+            'rdamt'   => 'http://rdaregistry.info/termList/RDAMediaType/',
+            'rdapf'   => 'http://rdaregistry.info/termList/presFormat/',
+            'rdapm'   => 'http://rdaregistry.info/termList/RDAproductionMethod/',
+            'rdapo'   => 'http://rdaregistry.info/termList/RDAPolarity/',
+            'rdarm'   => 'http://rdaregistry.info/termList/recMedium/',
+            'rdarr'   => 'http://rdaregistry.info/termList/RDAReductionRatio/',
+            'rdasca'  => 'http://rdaregistry.info/termList/scale/',
+            'rdasco'  => 'http://rdaregistry.info/termList/soundCont/',
+            'rdasoi'  => 'http://rdaregistry.info/termList/statIdentification/',
+            'rdaspc'  => 'http://rdaregistry.info/termList/specPlayback/',
+            'rdatc'   => 'http://rdaregistry.info/termList/trackConfig/',
+            'rdaterm' => 'http://rdaregistry.info/termList/RDATerms/',
+            'rdatr'   => 'http://rdaregistry.info/termList/typeRec/',
+            'rdavf'   => 'http://rdaregistry.info/termList/videoFormat/',
+        //elementPrefixes
+            'rdac'  => 'http://rdaregistry.info/Elements/c/',
+            'rdaa'  => 'http://rdaregistry.info/Elements/a/',
+            'rdaad' => 'http://rdaregistry.info/Elements/a/datatype/',
+            'rdaao' => 'http://rdaregistry.info/Elements/a/object/',
+            'rdae'  => 'http://rdaregistry.info/Elements/e/',
+            'rdaed' => 'http://rdaregistry.info/Elements/e/datatype/',
+            'rdaeo' => 'http://rdaregistry.info/Elements/e/object/',
+            'rdai'  => 'http://rdaregistry.info/Elements/i/',
+            'rdaid' => 'http://rdaregistry.info/Elements/i/datatype/',
+            'rdaio' => 'http://rdaregistry.info/Elements/i/object/',
+            'rdam'  => 'http://rdaregistry.info/Elements/m/',
+            'rdamd' => 'http://rdaregistry.info/Elements/m/datatype/',
+            'rdamo' => 'http://rdaregistry.info/Elements/m/object/',
+            'rdan'  => 'http://rdaregistry.info/Elements/n/',
+            'rdand' => 'http://rdaregistry.info/Elements/n/datatype/',
+            'rdano' => 'http://rdaregistry.info/Elements/n/object/',
+            'rdap'  => 'http://rdaregistry.info/Elements/p/',
+            'rdapd' => 'http://rdaregistry.info/Elements/p/datatype/',
+            'rdapo' => 'http://rdaregistry.info/Elements/p/object/',
+            'rdat'  => 'http://rdaregistry.info/Elements/t/',
+            'rdatd' => 'http://rdaregistry.info/Elements/t/datatype/',
+            'rdato' => 'http://rdaregistry.info/Elements/t/object/',
+            'rdau'  => 'http://rdaregistry.info/Elements/u/',
+            'rdaw'  => 'http://rdaregistry.info/Elements/w/',
+            'rdawd' => 'http://rdaregistry.info/Elements/w/datatype/',
+            'rdawo' => 'http://rdaregistry.info/Elements/w/object/',
+            'rdax'  => 'http://rdaregistry.info/Elements/x/',
+            'rdaxd' => 'http://rdaregistry.info/Elements/x/datatype/',
+            'rdaxo' => 'http://rdaregistry.info/Elements/x/object/',
+            'rdaz'  => 'http://rdaregistry.info/Elements/z/',
+        ];
+        $project         = Project::find(177);
+        $vocabs          = $project->vocabularies;
+        $elements        = $project->elementsets;
+        foreach ($vocabs as $vocab) {
+            $vocab->prefixes = $rdaPrefixes;
+            $vocab->save();
+        }
+        foreach ($elements as $element) {
+            $element->prefixes = $rdaPrefixes;
+            $element->save();
+        }
+    });
