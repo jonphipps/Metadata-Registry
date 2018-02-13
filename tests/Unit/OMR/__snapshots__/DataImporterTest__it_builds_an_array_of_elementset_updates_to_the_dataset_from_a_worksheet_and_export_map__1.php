b1ba13009c9ece567aa696ef4c028cda8ed6f5c7
Laravel Shift <shift@laravelshift.com>
2018-02-13 16:13:29 +0000

    Adopt PHP short array syntax
    Laravel 5 adopted the short array syntax which became available in
    PHP 5.4.

--- tests/Unit/OMR/__snapshots__/DataImporterTest__it_builds_an_array_of_elementset_updates_to_the_dataset_from_a_worksheet_and_export_map__1.php
+++ tests/Unit/OMR/__snapshots__/DataImporterTest__it_builds_an_array_of_elementset_updates_to_the_dataset_from_a_worksheet_and_export_map__1.php
@@ -1,8 +1,8 @@
-<?php return array (
+<?php return  [
   14335 =>
-  array (
+   [
     '*name_fr' =>
-    array (
+     [
       'new value' => 'bingo',
       'old value' => null,
       'statement_id' => null,
@@ -10,36 +10,36 @@
       'property_id' => '1',
       'updated_at' => null,
       'required' => true,
-    ),
+    ],
     '*label_fr' =>
-    array (
+     [
       'new value' => 'foobar',
       'old value' => 'Famille',
       'statement_id' => 180596,
       'language' => 'fr',
       'property_id' => '2',
       'updated_at' =>
-      Illuminate\Support\Carbon::__set_state(array(
+      Illuminate\Support\Carbon::__set_state([
          'date' => '2016-02-17 10:14:07.000000',
          'timezone_type' => 3,
          'timezone' => 'UTC',
-      )),
+      ]),
       'required' => true,
-    ),
+    ],
     'lexicalAlias_en' =>
-    array (
+     [
       'new value' => '',
       'old value' => 'rdac:Family.en',
       'statement_id' => 128175,
       'language' => 'en',
       'property_id' => '27',
       'updated_at' =>
-      Illuminate\Support\Carbon::__set_state(array(
+      Illuminate\Support\Carbon::__set_state([
          'date' => '2015-06-20 19:37:46.000000',
          'timezone_type' => 3,
          'timezone' => 'UTC',
-      )),
+      ]),
       'required' => false,
-    ),
-  ),
-);
+    ],
+  ],
+];
