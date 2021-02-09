8e1507837b597da9a6cf4d6134466c780e442e1a
Jon Phipps <jphipps@madcreek.com>
2021-02-09 17:38:48 -0500

    adjust array maps to match actual returned array
    Hope this is correct

--- tests/Unit/OMR/ProfileTest.php
+++ tests/Unit/OMR/ProfileTest.php
@@ -24,35 +24,35 @@ class ProfileTest extends TestCase
 
         //repeatable with language
         $header  = 'alternative label[1]_fr';
-        $testMap = [ 'id' => '34', 'label' => $header, 'language' => 'fr' ];
+        $testMap = [ 'id' => '34', 'label' => $header, 'language' => 'fr', 'object' => 0, 'required'=> 0 ];
         $map     = $profile->getColumnMapFromHeader($header);
         //then I should get a column header map returned
         self::assertEquals($testMap, $map);
 
         //required repeatable with language
         $header  = '*preferred label[0]_en';
-        $testMap = [ 'id' => '45', 'label' => $header, 'language' => 'en' ];
+        $testMap = [ 'id' => '45', 'label' => $header, 'language' => 'en', 'object' => 0, 'required' => 1];
         $map     = $profile->getColumnMapFromHeader($header);
         //then I should get a column header map returned
         self::assertEquals($testMap, $map);
 
         //required not repeatable no language
         $header  = '*status';
-        $testMap = [ 'id' => '59', 'label' => $header, 'language' => '' ];
+        $testMap = [ 'id' => '59', 'label' => $header, 'language' => '', 'object' => 1, 'required' => 0];
         $map     = $profile->getColumnMapFromHeader($header);
         //then I should get a column header map returned
         self::assertEquals($testMap, $map);
 
         //not required not repeatable with language
         $header  = 'ToolkitLabel_en';
-        $testMap = [ 'id' => '61', 'label' => $header, 'language' => 'en' ];
+        $testMap = [ 'id' => '61', 'label' => $header, 'language' => 'en', 'object' => 0, 'required' => 0];
         $map     = $profile->getColumnMapFromHeader($header);
         //then I should get a column header map returned
         self::assertEquals($testMap, $map);
 
         //not required not repeatable no language
         $header  = 'ToolkitLabel_en';
-        $testMap = [ 'id' => '61', 'label' => $header, 'language' => 'en' ];
+        $testMap = [ 'id' => '61', 'label' => $header, 'language' => 'en', 'object' => 0, 'required' => 0];
         $map     = $profile->getColumnMapFromHeader($header);
         //then I should get a column header map returned
         self::assertEquals($testMap, $map);
@@ -62,14 +62,14 @@ class ProfileTest extends TestCase
 
         //not repeatable, not required, no language
         $header  = 'domain';
-        $testMap = [ 'id' => '11', 'label' => $header, 'language' => '' ];
+        $testMap = [ 'id' => '11', 'label' => $header, 'language' => '', 'object' => 1, 'required' => 0];
         $map     = $profile->getColumnMapFromHeader($header);
         //then I should get a column header map returned
         self::assertEquals($testMap, $map);
 
         //repeatable, not required, no language
         $header  = 'subPropertyOf[0]';
-        $testMap = [ 'id' => '6', 'label' => $header, 'language' => '' ];
+        $testMap = [ 'id' => '6', 'label' => $header, 'language' => '', 'object' => 1, 'required' => 0];
         $map     = $profile->getColumnMapFromHeader($header);
         //then I should get a column header map returned
         self::assertEquals($testMap, $map);
